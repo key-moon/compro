41bfa03eef604bf3832cf95344d5ac0f8476ad85
crawler <kymn0116+crawler@gmail.com>
2017-11-16 23:56:44 +0000

    add atcoder/abc077/abc077_b/1778626.cs

---
+++ atcoder/abc077/abc077_b/1778626.cs
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/abc077/submissions/1778626
+using System;
+class P
+{
+    static void Main(string[] args)
+    {
+        var i = (int)(Math.Sqrt(int.Parse(Console.ReadLine())));
+        Console.WriteLine(i * i);
+    }
+}
