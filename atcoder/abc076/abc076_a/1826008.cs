43af802a12b522431d2cb352ad969b42415db998
crawler <kymn0116+crawler@gmail.com>
2017-12-03 10:24:19 +0000

    add atcoder/abc076/abc076_a/1826008.cs

---
+++ atcoder/abc076/abc076_a/1826008.cs
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc076/submissions/1826008
+using System;
+class P
+{
+    static void Main()
+    {
+        int R = int.Parse(Console.ReadLine());
+        int G = int.Parse(Console.ReadLine());
+        Console.WriteLine(R + (G - R) * 2);
+    }
+}
