cc76ebb0258fb02f5f3200c07ebb3dc88e7a8d05
crawler <kymn0116+crawler@gmail.com>
2017-12-23 12:07:43 +0000

    add atcoder/arc088/arc088_a/1896554.cs

---
+++ atcoder/arc088/arc088_a/1896554.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/arc088/submissions/1896554
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        long[] XY = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray();
+        int count = 0;
+        long num = XY[0];
+        while (num <= XY[1])
+        {
+            num *= 2;
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+}
