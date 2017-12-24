2a5eec0c628066031bd8c7e2f995500db74a3017
crawler <kymn0116+crawler@gmail.com>
2017-12-24 10:58:10 +0000

    add atcoder/xmascon17/xmascon17_b/1905494.cs

---
+++ atcoder/xmascon17/xmascon17_b/1905494.cs
@@ -0,0 +1,31 @@
+// detail: https://atcoder.jp/contests/xmascon17/submissions/1905494
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+class P
+{
+    static void Main()
+    {
+        string res = "17 16\n";
+        res += "*...............\n";
+        res += "X7..............\n";
+        res += ".m1.............\n";
+        res += "..a0............\n";
+        res += "...s2...........\n";
+        res += "....Ct..........\n";
+        res += ".....os.........\n";
+        res += "......ne........\n";
+        res += ".......tt.......\n";
+        res += "........en......\n";
+        res += ".........so.....\n";
+        res += "..........tC....\n";
+        res += "...........2s...\n";
+        res += "............0a..\n";
+        res += ".............1m.\n";
+        res += "..............7X\n";
+        res += "...............*";
+        Console.WriteLine(res);
+    }
+}
