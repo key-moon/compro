5edb9c0a61cd914b5fde7dcc430f598f16ad3346
crawler <kymn0116+crawler@gmail.com>
2017-12-24 11:49:38 +0000

    add atcoder/xmascon17/xmascon17_g/1906111.cs

---
+++ atcoder/xmascon17/xmascon17_g/1906111.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/xmascon17/submissions/1906111
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine("ACEFGHNPQSYZ");
+    }
+}
