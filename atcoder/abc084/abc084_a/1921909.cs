486bb80094ac48de0afe6a4505a8e914965c8ba0
crawler <kymn0116+crawler@gmail.com>
2017-12-30 12:01:18 +0000

    add atcoder/abc084/abc084_a/1921909.cs

---
+++ atcoder/abc084/abc084_a/1921909.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc084/submissions/1921909
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        //Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(48 - N);
+    }
+}
