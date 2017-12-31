395e21fee5e629b210cbcd87510d3351991e26ab
crawler <kymn0116+crawler@gmail.com>
2017-12-31 17:37:28 +0000

    add atcoder/abc043/abc043_a/1931388.cs

---
+++ atcoder/abc043/abc043_a/1931388.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc043/submissions/1931388
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n * (n + 1) / 2);
+    }
+}
