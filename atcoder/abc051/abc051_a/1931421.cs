3980cab404f6dcdab0b363ffbc952fcd1ae3bd7e
crawler <kymn0116+crawler@gmail.com>
2017-12-31 17:59:49 +0000

    add atcoder/abc051/abc051_a/1931421.cs

---
+++ atcoder/abc051/abc051_a/1931421.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc051/submissions/1931421
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split(',');
+        Console.WriteLine(string.Join(" ",s));
+    }
+}
