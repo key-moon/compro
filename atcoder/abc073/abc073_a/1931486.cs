e4f330f2760536b98827dcb06bb289c7de0cb3cd
crawler <kymn0116+crawler@gmail.com>
2017-12-31 18:36:21 +0000

    add atcoder/abc073/abc073_a/1931486.cs

---
+++ atcoder/abc073/abc073_a/1931486.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc073/submissions/1931486
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Contains('9') ? "Yes" : "No");
+    }
+}
