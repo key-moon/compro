85fa0608fb4a5be9eac944e450c677c14f5555e3
crawler <kymn0116+crawler@gmail.com>
2017-12-31 17:41:58 +0000

    add atcoder/abc045/abc045_a/1931394.cs

---
+++ atcoder/abc045/abc045_a/1931394.cs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc045/submissions/1931394
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int h = int.Parse(Console.ReadLine());
+        Console.WriteLine((a + b) * h / 2);
+    }
+}
