926810fd9aead20ade0ff2ec2399817441827709
crawler <kymn0116+crawler@gmail.com>
2017-12-31 18:42:53 +0000

    add atcoder/abc081/abc081_a/1931499.cs

---
+++ atcoder/abc081/abc081_a/1931499.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc081/submissions/1931499
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Count(x => x == '1'));
+    }
+}
