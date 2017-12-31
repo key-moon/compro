ea05d6b560d0a9e2554e880365a403bc6d67543e
crawler <kymn0116+crawler@gmail.com>
2017-12-31 17:54:04 +0000

    add atcoder/abc048/abc048_a/1931410.cs

---
+++ atcoder/abc048/abc048_a/1931410.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc048/submissions/1931410
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string[] a = Console.ReadLine().Split();
+
+        Console.WriteLine(a[0].ToUpper()[0].ToString() + a[1].ToUpper()[0].ToString() + a[2].ToUpper()[0].ToString());
+    }
+}
