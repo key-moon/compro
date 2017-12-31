586b9778e383998ab55e8c1519e8a49cb7d00a65
crawler <kymn0116+crawler@gmail.com>
2017-12-31 19:06:30 +0000

    add atcoder/tenka1-2017-beginner/tenka1_2017_a/1931535.cs

---
+++ atcoder/tenka1-2017-beginner/tenka1_2017_a/1931535.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/tenka1-2017-beginner/submissions/1931535
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Count(x => x == '1'));
+    }
+}
