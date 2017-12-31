772751cfa1b6b6b5827d10540b25e2ba8a53c80e
crawler <kymn0116+crawler@gmail.com>
2017-12-31 18:29:02 +0000

    add atcoder/abc068/abc068_a/1931475.cs

---
+++ atcoder/abc068/abc068_a/1931475.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc068/submissions/1931475
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int i = int.Parse(Console.ReadLine());
+        Console.WriteLine($"ABC{i}");
+    }
+}
