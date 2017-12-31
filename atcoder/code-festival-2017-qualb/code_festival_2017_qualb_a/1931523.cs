a8519febf47ea1e29dbca85f6fffb45f6c7c6830
crawler <kymn0116+crawler@gmail.com>
2017-12-31 18:59:03 +0000

    add atcoder/code-festival-2017-qualb/code_festival_2017_qualb_a/1931523.cs

---
+++ atcoder/code-festival-2017-qualb/code_festival_2017_qualb_a/1931523.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/code-festival-2017-qualb/submissions/1931523
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Substring(0, s.Length - 8));
+    }
+}
