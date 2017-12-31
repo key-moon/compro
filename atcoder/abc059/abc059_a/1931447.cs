a3ab7ca1b0af7d926633c80cff1dcfd7462de6b0
crawler <kymn0116+crawler@gmail.com>
2017-12-31 18:11:58 +0000

    add atcoder/abc059/abc059_a/1931447.cs

---
+++ atcoder/abc059/abc059_a/1931447.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc059/submissions/1931447
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split();
+        Console.WriteLine(s[0].ToUpper()[0].ToString()+ s[1].ToUpper()[0].ToString()+ s[2].ToUpper()[0].ToString());
+    }
+}
