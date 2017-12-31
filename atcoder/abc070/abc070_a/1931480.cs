c8722128497c71d0869a3ba5dfa2b2c0a585e379
crawler <kymn0116+crawler@gmail.com>
2017-12-31 18:31:26 +0000

    add atcoder/abc070/abc070_a/1931480.cs

---
+++ atcoder/abc070/abc070_a/1931480.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc070/submissions/1931480
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s[0] == s[2] ? "Yes" : "No");
+    }
+}
