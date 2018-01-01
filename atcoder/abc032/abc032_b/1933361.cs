9d4de32e6d03fbe90d3a75c4d770ba8175a208c7
crawler <kymn0116+crawler@gmail.com>
2018-01-01 13:27:32 +0000

    add atcoder/abc032/abc032_b/1933361.cs

---
+++ atcoder/abc032/abc032_b/1933361.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/abc032/submissions/1933361
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+        //int[] NQ = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        //string[][] s = new string[n][].Select(_ => Console.ReadLine().Split()).ToArray();
+        //int sum = 0;
+        List<string> list = new List<string>();
+        for (int i = 0; i <= s.Length - n; i++)
+        {
+            string substr = s.Substring(i,n);
+            if(!list.Contains(substr)) list.Add(substr);
+        }
+        Console.WriteLine(list.Count);
+    }
+}
