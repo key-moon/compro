1eb1bb60596908c9392f3c7a3cad3c9d62b1a70d
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:18:48 +0000

    add atcoder/abc027/abc027_a/1932325.cs

---
+++ atcoder/abc027/abc027_a/1932325.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc027/submissions/1932325
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        //int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(a.Count(x => x == a.Max()) == 1 ? a.Max() : a.Min());
+    }
+}
