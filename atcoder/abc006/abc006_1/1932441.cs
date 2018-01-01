c93ee0a3bd57c332aea162a7fca2359f9d364937
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:56:10 +0000

    add atcoder/abc006/abc006_1/1932441.cs

---
+++ atcoder/abc006/abc006_1/1932441.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc006/submissions/1932441
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(a % 3 == 0?"YES":"NO");
+    }
+}
