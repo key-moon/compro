03b0c82c99665461dd8ba7460feccc2f95214795
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:17:05 +0000

    add atcoder/abc028/abc028_a/1932320.cs

---
+++ atcoder/abc028/abc028_a/1932320.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc028/submissions/1932320
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(a == 100 ? "Perfect" : (a >= 90 ? "Great" : (a >= 60 ? "Good" : "Bad")));
+    }
+}
