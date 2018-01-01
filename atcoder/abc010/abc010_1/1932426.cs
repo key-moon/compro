75372ef1409e50253c4a99945be532ba839c6e28
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:52:23 +0000

    add atcoder/abc010/abc010_1/1932426.cs

---
+++ atcoder/abc010/abc010_1/1932426.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc010/submissions/1932426
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(Console.ReadLine() + "pp");
+    }
+}
