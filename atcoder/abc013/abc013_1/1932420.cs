0295ce239d425f376e4f2c7abeb13c941634115e
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:50:24 +0000

    add atcoder/abc013/abc013_1/1932420.cs

---
+++ atcoder/abc013/abc013_1/1932420.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc013/submissions/1932420
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine()[0] - 'A' + 1);
+    }
+}
