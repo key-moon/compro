7f4b061b4bd0b22d80de7070cb592b7b594718aa
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:49:43 +0000

    add atcoder/abc014/abc014_1/1932414.cs

---
+++ atcoder/abc014/abc014_1/1932414.cs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc014/submissions/1932414
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine((b - a % b) % b);
+    }
+}
