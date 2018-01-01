ebd540d48666944474a19d8351cc1638d202c678
crawler <kymn0116+crawler@gmail.com>
2018-01-01 06:55:04 +0000

    add atcoder/abc041/abc041_a/1932259.cs

---
+++ atcoder/abc041/abc041_a/1932259.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc041/submissions/1932259
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s[int.Parse(Console.ReadLine()) - 1]);
+    }
+}
