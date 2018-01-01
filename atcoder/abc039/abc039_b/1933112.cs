9b373844ca52ac8d78da9c6cd27a7d36ce3d30ec
crawler <kymn0116+crawler@gmail.com>
2018-01-01 12:16:25 +0000

    add atcoder/abc039/abc039_b/1933112.cs

---
+++ atcoder/abc039/abc039_b/1933112.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc039/submissions/1933112
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine((int)Math.Sqrt(Math.Sqrt(n)));
+    }
+}
