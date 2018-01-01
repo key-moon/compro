eaa10e8e1b8c367e9458eb655800557fd2488e6e
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:28:41 +0000

    add atcoder/abc025/abc025_a/1932353.cs

---
+++ atcoder/abc025/abc025_a/1932353.cs
@@ -0,0 +1,17 @@
+// detail: https://atcoder.jp/contests/abc025/submissions/1932353
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        string[] str = { "aa", "ab", "ac", "ad", "ae", "ba", "bb", "bc", "bd", "be", "ca", "cb", "cc", "cd", "ce", "da", "db", "dc", "dd", "de", "ea", "eb", "ec", "ed", "ee" };
+        string s = Console.ReadLine();
+        int a = int.Parse(Console.ReadLine());
+        string res = str[a - 1].Replace('e', s[4]).Replace('d', s[3]).Replace('c', s[2]).Replace('b', s[1]).Replace('a', s[0]);
+        Console.WriteLine(res);
+    }
+}
