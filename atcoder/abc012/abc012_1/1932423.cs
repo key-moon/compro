b62e5888ff293e59279c450bff84570d2ed99266
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:51:12 +0000

    add atcoder/abc012/abc012_1/1932423.cs

---
+++ atcoder/abc012/abc012_1/1932423.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc012/submissions/1932423
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine($"{a[1]} {a[0]}");
+    }
+}
