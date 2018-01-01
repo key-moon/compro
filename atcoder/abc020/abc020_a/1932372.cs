b7f5d27b2689972d1c40f453b775b14bbccf1585
crawler <kymn0116+crawler@gmail.com>
2018-01-01 07:36:53 +0000

    add atcoder/abc020/abc020_a/1932372.cs

---
+++ atcoder/abc020/abc020_a/1932372.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc020/submissions/1932372
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int w = int.Parse(Console.ReadLine());
+        Console.WriteLine(w == 1 ? "ABC" : "chokudai");
+    }
+}
