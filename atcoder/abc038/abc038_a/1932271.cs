96c0753a305e8ec535c06ee27d97728eb30d0570
crawler <kymn0116+crawler@gmail.com>
2018-01-01 06:58:59 +0000

    add atcoder/abc038/abc038_a/1932271.cs

---
+++ atcoder/abc038/abc038_a/1932271.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc038/submissions/1932271
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s[s.Length - 1] == 'T' ? "YES" : "NO");
+    }
+}
