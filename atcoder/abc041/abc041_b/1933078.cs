bfc2e89a0abdfc4d0352d21d221cdca4fdcc398a
crawler <kymn0116+crawler@gmail.com>
2018-01-01 12:09:12 +0000

    add atcoder/abc041/abc041_b/1933078.cs

---
+++ atcoder/abc041/abc041_b/1933078.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc041/submissions/1933078
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        long[] ABC = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        Console.WriteLine((((ABC[0] * ABC[1]) % 1000000007) * ABC[2]) % 1000000007);
+    }
+}
