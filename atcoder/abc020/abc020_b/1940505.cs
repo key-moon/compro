86433b9710175eeb3092e261a3ee97daa503783b
crawler <kymn0116+crawler@gmail.com>
2018-01-04 15:05:19 +0000

    add atcoder/abc020/abc020_b/1940505.cs

---
+++ atcoder/abc020/abc020_b/1940505.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc020/submissions/1940505
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+         Console.WriteLine(int.Parse(string.Join("",Console.ReadLine().Split())) * 2);
+    }
+}
