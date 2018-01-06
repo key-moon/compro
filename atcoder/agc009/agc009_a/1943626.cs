693656809d41adb013962d1665a5f2885ef2baf0
crawler <kymn0116+crawler@gmail.com>
2018-01-06 07:38:21 +0000

    add atcoder/agc009/agc009_a/1943626.cs

---
+++ atcoder/agc009/agc009_a/1943626.cs
@@ -0,0 +1,25 @@
+// detail: https://atcoder.jp/contests/agc009/submissions/1943626
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long res = 0;
+        List<long[]> abs = new List<long[]>();
+        for (int i = 0; i < n; i++)
+        {
+            abs.Add(Console.ReadLine().Split().Select(long.Parse).ToArray());
+        }
+        abs.Reverse();
+        foreach (var ab in abs)
+        {
+            long amari = (ab[0] + res) % ab[1];
+            res += (ab[1] - amari) % ab[1];
+        }
+        Console.WriteLine(res);
+    }
+}
