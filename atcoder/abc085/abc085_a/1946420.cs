b7d4dde719f227ce8b2f700c4ca637c8266add72
crawler <kymn0116+crawler@gmail.com>
2018-01-07 12:01:01 +0000

    add atcoder/abc085/abc085_a/1946420.cs

---
+++ atcoder/abc085/abc085_a/1946420.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc085/submissions/1946420
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+       // int n = int.Parse(Console.ReadLine());
+        //int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(Console.ReadLine().Replace("2017", "2018"));
+    }
+}
