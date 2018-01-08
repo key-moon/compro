f69c8b0758cc078d64e1df7a0b2e80c84b506dd8
crawler <kymn0116+crawler@gmail.com>
2018-01-08 21:38:59 +0000

    add atcoder/arc035/arc035_a/1957533.cs

---
+++ atcoder/arc035/arc035_a/1957533.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/arc035/submissions/1957533
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] point = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        //int n = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+        string res = "YES";
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != '*' && s[s.Length - 1 - i] != '*' && s[i] != s[s.Length - 1 - i]) res = "NO";
+        }
+        Console.WriteLine(res);
+    }
+}
