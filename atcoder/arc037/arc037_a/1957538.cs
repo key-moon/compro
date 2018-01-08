814d2628c80259d081e8d201a47e3b9a0147995a
crawler <kymn0116+crawler@gmail.com>
2018-01-08 21:50:48 +0000

    add atcoder/arc037/arc037_a/1957538.cs

---
+++ atcoder/arc037/arc037_a/1957538.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/arc037/submissions/1957538
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.ReadLine();
+        int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        //int n = int.Parse(Console.ReadLine());
+        //string s = Console.ReadLine();
+        int res = 0;
+        for (int i = 0; i < n.Length; i++)
+        {
+            res += Math.Max(80 - n[i], 0);
+        }
+        Console.WriteLine(res);
+
+    }
+}
