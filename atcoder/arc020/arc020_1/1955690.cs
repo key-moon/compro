4017d7d2168036b1aa1b9fa8b8685cc99283dc90
crawler <kymn0116+crawler@gmail.com>
2018-01-08 06:56:29 +0000

    add atcoder/arc020/arc020_1/1955690.cs

---
+++ atcoder/arc020/arc020_1/1955690.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/arc020/submissions/1955690
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] ab = Console.ReadLine().Split().Select(x => Math.Abs(int.Parse(x))).ToArray();
+        Console.WriteLine(ab[0] == ab[1] ? "Draw" :(ab[0] < ab[1] ? "Ant" : "Bug"));
+    }
+}
