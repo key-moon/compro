807e8f21b4995f73c69951a9b98ec2902ee7d3f9
crawler <kymn0116+crawler@gmail.com>
2018-01-08 14:09:01 +0000

    add atcoder/arc026/arc026_1/1957122.cs

---
+++ atcoder/arc026/arc026_1/1957122.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/arc026/submissions/1957122
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(a[2] * Math.Min(a[0], 5) + a[1] * Math.Max(a[0] - 5, 0));
+    }
+}
