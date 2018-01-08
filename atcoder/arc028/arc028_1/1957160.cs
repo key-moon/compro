c82f91f8c2bf164817df80875c7cc64be43e5881
crawler <kymn0116+crawler@gmail.com>
2018-01-08 14:17:58 +0000

    add atcoder/arc028/arc028_1/1957160.cs

---
+++ atcoder/arc028/arc028_1/1957160.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/arc028/submissions/1957160
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] nab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(nab[0] % (nab[1] + nab[2]) <= nab[1] && nab[0] % (nab[1] + nab[2]) != 0? "Ant" : "Bug");
+    }
+}
