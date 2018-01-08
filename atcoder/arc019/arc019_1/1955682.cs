19248b7bde83e87af2170859db43ea2432c203cb
crawler <kymn0116+crawler@gmail.com>
2018-01-08 06:54:18 +0000

    add atcoder/arc019/arc019_1/1955682.cs

---
+++ atcoder/arc019/arc019_1/1955682.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/arc019/submissions/1955682
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Replace('O','0').Replace('D', '0').Replace('I', '1').Replace('Z', '2').Replace('S', '5').Replace('B', '8'));
+    }
+}
