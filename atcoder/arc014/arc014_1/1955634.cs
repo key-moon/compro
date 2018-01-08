c20979a64498f0fea768f09457195a1feaa10710
crawler <kymn0116+crawler@gmail.com>
2018-01-08 06:38:36 +0000

    add atcoder/arc014/arc014_1/1955634.cs

---
+++ atcoder/arc014/arc014_1/1955634.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/arc014/submissions/1955634
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(int.Parse(Console.ReadLine()) % 2 == 0 ? "Blue" : "Red");
+    }
+}
