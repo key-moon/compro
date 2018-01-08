afbff8df0f0e2316d38510f7a49e7dd67c2a8a1a
crawler <kymn0116+crawler@gmail.com>
2018-01-08 14:59:02 +0000

    add atcoder/arc030/arc030_1/1957276.cs

---
+++ atcoder/arc030/arc030_1/1957276.cs
@@ -0,0 +1,17 @@
+// detail: https://atcoder.jp/contests/arc030/submissions/1957276
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] hm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(n / m >= 2 ? "YES" : "NO");
+
+    }
+}
