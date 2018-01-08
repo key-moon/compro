396afe380337a24232cc74d0f7ca571d9cbfc40d
crawler <kymn0116+crawler@gmail.com>
2018-01-08 06:40:15 +0000

    add atcoder/arc015/arc015_1/1955641.cs

---
+++ atcoder/arc015/arc015_1/1955641.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/arc015/submissions/1955641
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine((double.Parse(Console.ReadLine()) * 1.8) + 32);
+    }
+}
