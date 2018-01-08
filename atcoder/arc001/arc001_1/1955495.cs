03319e0d719503c30c63460e05f499a3e706b2d2
crawler <kymn0116+crawler@gmail.com>
2018-01-08 05:32:24 +0000

    add atcoder/arc001/arc001_1/1955495.cs

---
+++ atcoder/arc001/arc001_1/1955495.cs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/arc001/submissions/1955495
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.ReadLine();
+        string s = Console.ReadLine();
+        int[] a = { s.Count(x => x == '1'), s.Count(x => x == '2'), s.Count(x => x == '3'), s.Count(x => x == '4') };
+        Console.WriteLine($"{a.Max()} {a.Min()}");
+    }
+}
