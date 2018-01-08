b44fa839097fdc297907c05089dd81dcb6048b1e
crawler <kymn0116+crawler@gmail.com>
2018-01-08 21:30:13 +0000

    add atcoder/arc033/arc033_1/1957528.cs

---
+++ atcoder/arc033/arc033_1/1957528.cs
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/arc033/submissions/1957528
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] hm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+        //string s = Console.ReadLine();
+        Console.WriteLine(n * (n + 1) / 2);
+
+    }
+}
