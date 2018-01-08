6872452a27bea744455958595b0fd4b763cc74d8
crawler <kymn0116+crawler@gmail.com>
2018-01-08 21:24:16 +0000

    add atcoder/arc031/arc031_1/1957520.cs

---
+++ atcoder/arc031/arc031_1/1957520.cs
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/arc031/submissions/1957520
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] hm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        //int n = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+        Console.WriteLine(s == string.Join("", s.Reverse().ToArray()) ? "YES" : "NO");
+
+    }
+}
