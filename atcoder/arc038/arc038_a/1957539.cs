ef57da55f46955c795b5e6fac7670935c16322e8
crawler <kymn0116+crawler@gmail.com>
2018-01-08 21:52:18 +0000

    add atcoder/arc038/arc038_a/1957539.cs

---
+++ atcoder/arc038/arc038_a/1957539.cs
@@ -0,0 +1,17 @@
+// detail: https://atcoder.jp/contests/arc038/submissions/1957539
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.ReadLine();
+        //int[] n = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Where((_, ind) => ind % 2 == 0).Sum();
+        //int n = int.Parse(Console.ReadLine());
+        //string s = Console.ReadLine();
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Where((_, ind) => ind % 2 == 0).Sum());
+
+    }
+}
