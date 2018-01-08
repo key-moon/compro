db4f23000d900b6fd164d5fcb85361405e38cc2e
crawler <kymn0116+crawler@gmail.com>
2018-01-08 23:05:01 +0000

    add atcoder/arc055/arc055_a/1957575.cs

---
+++ atcoder/arc055/arc055_a/1957575.cs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/arc055/submissions/1957575
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        //int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        //int a = int.Parse(Console.ReadLine());
+        //string s = Console.ReadLine();
+        Console.WriteLine($"1{"".PadLeft(int.Parse(Console.ReadLine()) - 1,'0')}7");
+    }
+}
