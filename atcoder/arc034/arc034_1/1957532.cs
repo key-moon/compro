2a1a7d94300cf26481aed75a5c4497ecd6283663
crawler <kymn0116+crawler@gmail.com>
2018-01-08 21:35:33 +0000

    add atcoder/arc034/arc034_1/1957532.cs

---
+++ atcoder/arc034/arc034_1/1957532.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/arc034/submissions/1957532
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        //string s = Console.ReadLine();
+        double max = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int[] point = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            max = Math.Max(point.Sum() - point[4] * 0.8777777777777777777777777,max);
+        }
+        Console.WriteLine(max);
+
+    }
+}
