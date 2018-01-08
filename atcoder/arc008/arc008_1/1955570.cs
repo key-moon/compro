93711509f43a2186aeae9aa3dcbb38706aba1c43
crawler <kymn0116+crawler@gmail.com>
2018-01-08 06:12:00 +0000

    add atcoder/arc008/arc008_1/1955570.cs

---
+++ atcoder/arc008/arc008_1/1955570.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/arc008/submissions/1955570
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int money = (n / 10) * 100;
+        n = n % 10;
+        if (n < 7)
+        {
+            money += 15 * n;
+        }
+        else
+        {
+            money += 100;
+        }
+        Console.WriteLine(money);
+    }
+}
