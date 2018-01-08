7840ffcb8154386717a130ce11b0905de2b4c07b
crawler <kymn0116+crawler@gmail.com>
2018-01-08 22:08:39 +0000

    add atcoder/arc041/arc041_a/1957545.cs

---
+++ atcoder/arc041/arc041_a/1957545.cs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/arc041/submissions/1957545
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = int.Parse(Console.ReadLine());
+        //string s = Console.ReadLine();
+        Console.WriteLine(n[0] + Math.Min(n[1],a) - Math.Max(a - n[1],0));
+    }
+}
