ed3af42a6f43c19a8064c164c4b15e473291ffdf
crawler <kymn0116+crawler@gmail.com>
2018-01-08 05:07:59 +0000

    add atcoder/abc016/abc016_3/1955455.cs

---
+++ atcoder/abc016/abc016_3/1955455.cs
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/abc016/submissions/1955455
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        List<int>[] user = Enumerable.Range(1,nm[0]).Select(_ => new List<int>()).ToArray();
+        for (int i = 0; i < nm[1]; i++)
+        {
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            user[a[0] - 1].Add(a[1] - 1);
+            user[a[1] - 1].Add(a[0] - 1);
+        }
+        for (int i = 0; i < nm[0]; i++)
+        {
+            List<int> friendfriend = new List<int>();
+            foreach (var u in user[i])
+            {
+                friendfriend.AddRange(user[u]);
+            }
+            friendfriend = friendfriend.Distinct().Where(x => !user[i].Contains(x) && x != i).ToList();
+            Console.WriteLine(friendfriend.Count());
+        }
+    }
+}
