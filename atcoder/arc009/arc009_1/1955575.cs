276744dcaef06e469bd00eed3a5f18f8a9393b59
crawler <kymn0116+crawler@gmail.com>
2018-01-08 06:14:33 +0000

    add atcoder/arc009/arc009_1/1955575.cs

---
+++ atcoder/arc009/arc009_1/1955575.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/arc009/submissions/1955575
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int money = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            money += a[0] * a[1];
+        }
+        Console.WriteLine((int)Math.Floor(money * 1.05));
+    }
+}
