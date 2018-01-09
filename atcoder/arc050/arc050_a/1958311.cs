5e3077ba604d03eb596db4d1bf121ebc26b54c73
crawler <kymn0116+crawler@gmail.com>
2018-01-09 07:07:07 +0000

    add atcoder/arc050/arc050_a/1958311.cs

---
+++ atcoder/arc050/arc050_a/1958311.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/arc050/submissions/1958311
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s[0] - s[2] == -32 ? "Yes" : "No");
+    }
+}
