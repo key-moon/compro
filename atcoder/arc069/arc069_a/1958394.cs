646864df072fa685b5ca537ae067f2e22c1ecab8
crawler <kymn0116+crawler@gmail.com>
2018-01-09 07:31:43 +0000

    add atcoder/arc069/arc069_a/1958394.cs

---
+++ atcoder/arc069/arc069_a/1958394.cs
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/arc069/submissions/1958394
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        long[] a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        long res = Math.Min(a[0], a[1] / 2);
+        a[1] -= res * 2;
+        res += a[1] / 4;
+        Console.WriteLine(res);
+    }
+}
