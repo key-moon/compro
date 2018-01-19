f47e30da6c3d56f115b52a01d5235921bcc22e51
crawler <kymn0116+crawler@gmail.com>
2018-01-19 14:57:57 +0000

    add atcoder/arc014/arc014_2/1992630.cs

---
+++ atcoder/arc014/arc014_2/1992630.cs
@@ -0,0 +1,27 @@
+// detail: https://atcoder.jp/contests/arc014/submissions/1992630
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> wordList = new List<string>();
+        char lastChar = ' ';
+        for (int i = 0; i < n; i++)
+        {
+            string s = Console.ReadLine();
+            if (wordList.Contains(s) || (lastChar != s[0] && i != 0))
+            {
+                Console.WriteLine(i % 2 == 1 ? "WIN" : "LOSE");
+                return;
+            }
+            wordList.Add(s);
+            lastChar = s.Last();
+        }
+        Console.WriteLine("DRAW");
+
+    }
+}
