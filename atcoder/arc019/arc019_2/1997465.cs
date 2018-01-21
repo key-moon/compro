6ab52a2a4785ca75e23fd14161855bac751af9bd
crawler <kymn0116+crawler@gmail.com>
2018-01-21 08:03:45 +0000

    add atcoder/arc019/arc019_2/1997465.cs

---
+++ atcoder/arc019/arc019_2/1997465.cs
@@ -0,0 +1,31 @@
+// detail: https://atcoder.jp/contests/arc019/submissions/1997465
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        int diff = 0;
+        int res = 0;
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != s[s.Length - i - 1]) diff++;
+        }
+        if (diff == 0)
+        {
+            res = (s.Length / 2) * 2 * 25;
+        }
+        else if(diff == 1)
+        {
+            res = (s.Length - 2) * 25 + 2 * 24;
+        }
+        else
+        {
+            res = s.Length * 25;
+        }
+        Console.WriteLine(res);
+    }
+}
