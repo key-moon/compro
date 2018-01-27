c18343df31f95c443cd1fd09f39160c465d70ace
crawler <kymn0116+crawler@gmail.com>
2018-01-27 11:21:23 +0000

    add atcoder/abc053/abc053_a/2021078.cs

---
+++ atcoder/abc053/abc053_a/2021078.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc053/submissions/2021078
+using System;
+using System.Linq;
+using System.Collections.Generic;
+ 
+class P
+{
+    static void Main()
+    {
+        int a =  int.Parse(Console.ReadLine());
+        Console.WriteLine(a < 1200 ? "ABC" : "ARC");
+    }
+}
