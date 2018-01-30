ff576b8f52c9c10684f737671392796dc7a92731
crawler <kymn0116+crawler@gmail.com>
2018-01-30 03:36:06 +0000

    add atcoder/abc087/abc087_a/2040191.cs

---
+++ atcoder/abc087/abc087_a/2040191.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc087/submissions/2040191
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine((int.Parse(Console.ReadLine()) - int.Parse(Console.ReadLine())) % int.Parse(Console.ReadLine()));
+    }
+}
