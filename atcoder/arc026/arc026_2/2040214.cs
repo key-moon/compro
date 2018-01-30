79d6783b3a4f10b62cccedc465c2373a00abc68d
crawler <kymn0116+crawler@gmail.com>
2018-01-30 03:57:43 +0000

    add atcoder/arc026/arc026_2/2040214.cs

---
+++ atcoder/arc026/arc026_2/2040214.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/arc026/submissions/2040214
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        long a = long.Parse(Console.ReadLine());
+        long res = -a;
+        int sqrta = (int)Math.Ceiling(Math.Sqrt(a));
+        for (int i = 1; i < sqrta; i++)
+        {
+            if (a % i == 0)
+            {
+                if (a / i == i) res += i;
+                else res += (a / i) + i;
+            }
+        }
+        Console.WriteLine(a == res ? "Perfect" : (a > res ? "Deficient" : "Abundant"));
+    }
+
+}
