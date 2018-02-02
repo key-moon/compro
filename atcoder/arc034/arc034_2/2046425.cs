2684d8b73aae7ba7849813f16942d453067fa50e
crawler <kymn0116+crawler@gmail.com>
2018-02-02 00:49:31 +0000

    add atcoder/arc034/arc034_2/2046425.cs

---
+++ atcoder/arc034/arc034_2/2046425.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/arc034/submissions/2046425
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        List<long> res = new List<long>();
+        for (long i = Math.Max(0, n - 9 * 18); i < n; i++)
+        {
+            if (fx(i) + i == n) res.Add(i);
+        }
+        Console.WriteLine(res.Count);
+        if(res.Count != 0) Console.WriteLine(string.Join("\n", res));
+    }
+
+    static long fx(long i) => i.ToString().Sum(x => x - '0');
+}
