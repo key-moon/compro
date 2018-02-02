b3af9f08d12a597cedb6b4614fab8e18254dba1e
crawler <kymn0116+crawler@gmail.com>
2018-02-02 15:12:59 +0000

    add atcoder/arc033/arc033_2/2048475.cs

---
+++ atcoder/arc033/arc033_2/2048475.cs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/arc033/submissions/2048475
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        Console.ReadLine();
+        string[] a = Console.ReadLine().Split();
+        string[] b = Console.ReadLine().Split();
+        Console.WriteLine((double)a.Intersect(b).Count() / a.Union(b).Count());
+    }
+}
