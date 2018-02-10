5dbe5448902d97cf72b373ed814ed2ac684c15e8
crawler <kymn0116+crawler@gmail.com>
2018-02-10 12:02:01 +0000

    add atcoder/yahoo-procon2018-qual/yahoo_procon2018_qual_a/2079736.cs

---
+++ atcoder/yahoo-procon2018-qual/yahoo_procon2018_qual_a/2079736.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/yahoo-procon2018-qual/submissions/2079736
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s[0] == 'y' && s[1] == 'a' && s[2] == 'h' && s[3] == s[4] ? "YES" : "NO");
+    }
+}
