8cb964ca3aab0d07898aeff5dd1264881f268210
crawler <kymn0116+crawler@gmail.com>
2018-02-11 18:03:46 +0000

    add atcoder/arc069/arc069_b/2089496.cs

---
+++ atcoder/arc069/arc069_b/2089496.cs
@@ -0,0 +1,51 @@
+// detail: https://atcoder.jp/contests/arc069/submissions/2089496
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        //[マルバツ][自分][一つ前]
+        int[][][] q =
+        {
+            //◯
+            new int[][]
+            {
+                //羊
+                new int[]{0,1},
+                //狼
+                new int[]{1,0}
+            },
+            //×
+            new int[][]
+            {
+                //羊
+                new int[]{1,0},
+                //狼
+                new int[]{0,1}
+            }
+        };
+        int n = int.Parse(Console.ReadLine());
+        int[] s = Console.ReadLine().Select(x => x == 'o' ? 0 : 1).ToArray();
+        for (int i = 0; i <= 1; i++)
+        {
+            for (int j = 0; j <= 1; j++)
+            {
+                int[] a = new int[n];
+                a[0] = i;
+                a[1] = j;
+                for (int k = 2; k < n; k++)
+                {
+                    a[k] = q[s[k - 1]][a[k - 1]][a[k - 2]];
+                }
+                if (q[s[n - 1]][a[n - 1]][a[n - 2]] == a[0] && q[s[0]][a[0]][a[n - 1]] == a[1])
+                {
+                    Console.WriteLine(string.Join("", a.Select(x => x == 0 ? "S" : "W").ToArray()));
+                    return;
+                }
+            }
+        }
+        Console.WriteLine(-1);
+    }
+}
