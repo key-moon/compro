b6153de9b0e37fdffc516a9e541b4718230b01c3
crawler <kymn0116+crawler@gmail.com>
2018-02-11 17:14:44 +0000

    add atcoder/arc072/arc072_b/2089418.cs

---
+++ atcoder/arc072/arc072_b/2089418.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/arc072/submissions/2089418
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        long[] xy = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        Console.WriteLine(Math.Abs(xy[0] - xy[1]) > 1 ? "Alice" : "Brown");
+    }
+}
