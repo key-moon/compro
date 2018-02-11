6f1d10692e3a84defc19fd3df5edee12ac098542
crawler <kymn0116+crawler@gmail.com>
2018-02-11 18:21:15 +0000

    add atcoder/arc068/arc068_b/2089523.cs

---
+++ atcoder/arc068/arc068_b/2089523.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/arc068/submissions/2089523
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+        Console.WriteLine(a.Length - ((n - a.Length) % 2 == 0 ? 0 : 1));
+    }
+}
