e92f712316a584df81714a8ada8572e42a2a9fb6
crawler <kymn0116+crawler@gmail.com>
2018-02-14 16:29:59 +0000

    add atcoder/arc057/arc057_a/2095484.cs

---
+++ atcoder/arc057/arc057_a/2095484.cs
@@ -0,0 +1,28 @@
+// detail: https://atcoder.jp/contests/arc057/submissions/2095484
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        long[] a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        long count = 0;
+        long nicho = 2000000000000;
+        if (a[1] == 0)
+        {
+            count = nicho - a[0];
+        }
+        else
+        {
+            long t = a[0];
+            while (t < nicho)
+            {
+                t += (t * a[1] + 1);
+                //Console.WriteLine(t);
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
