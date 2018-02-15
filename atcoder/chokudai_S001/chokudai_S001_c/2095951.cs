0dc90107d9edeed03c3bb436eaf52a17c73da2d9
crawler <kymn0116+crawler@gmail.com>
2018-02-15 03:04:35 +0000

    add atcoder/chokudai_S001/chokudai_S001_c/2095951.cs

---
+++ atcoder/chokudai_S001/chokudai_S001_c/2095951.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/chokudai_S001/submissions/2095951
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(string.Join(",",a));
+    }
+}
