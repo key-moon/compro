f7e133ebaebdf727c0baf166f3c20b3c84e81adc
crawler <kymn0116+crawler@gmail.com>
2018-02-17 05:11:26 +0000

    add atcoder/future-contest-2018-qual/future_contest_2018_qual_a/2100108.cs

---
+++ atcoder/future-contest-2018-qual/future_contest_2018_qual_a/2100108.cs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/future-contest-2018-qual/submissions/2100108
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        //すべてが0が初期マス目
+        //中心と高さhを決め、中心からﾚｰﾍﾞﾝｼｭﾀｲﾝ距離aのところは+h-a
+        //
+        //マス目を最小化
+        Console.WriteLine(0);
+    }
+}
