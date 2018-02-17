cf144159190bd6d4876d778b2e01f2ee2de6c6e4
crawler <kymn0116+crawler@gmail.com>
2018-02-17 09:44:02 +0000

    add atcoder/future-contest-2018-qual/future_contest_2018_qual_a/2102947.cs

---
+++ atcoder/future-contest-2018-qual/future_contest_2018_qual_a/2102947.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/future-contest-2018-qual/submissions/2102947
+using System;
+class P
+{
+    const string ans = 
+@"1
+1 1 1";
+    static void Main()
+    {
+        Console.WriteLine(ans);
+    }
+}
