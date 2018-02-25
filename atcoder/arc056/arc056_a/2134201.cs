8901bae524181720e5f2d7512970fbb712c6fafe
crawler <kymn0116+crawler@gmail.com>
2018-02-25 02:05:24 +0000

    add atcoder/arc056/arc056_a/2134201.cs

---
+++ atcoder/arc056/arc056_a/2134201.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/arc056/submissions/2134201
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        long[] abkl = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        Console.WriteLine(abkl[1] * (abkl[2] / abkl[3]) + Math.Min(abkl[0] * (abkl[2] % abkl[3]), abkl[1]));
+    }
+}
