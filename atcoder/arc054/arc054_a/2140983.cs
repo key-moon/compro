b51db9e28abf987de5ea031dab2e04dce7a26204
crawler <kymn0116+crawler@gmail.com>
2018-02-27 13:04:57 +0000

    add atcoder/arc054/arc054_a/2140983.cs

---
+++ atcoder/arc054/arc054_a/2140983.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/arc054/submissions/2140983
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class P
+{
+    static void Main()
+    {
+        int[] lxysd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        double d = (lxysd[4] + (lxysd[0] - lxysd[3])) % lxysd[0];
+        Console.WriteLine(Math.Min(d / (lxysd[1] + lxysd[2]), (lxysd[2] - lxysd[1] > 0 ? (lxysd[0] - d) / (lxysd[2] - lxysd[1]) : double.MaxValue)));
+    }
+}
