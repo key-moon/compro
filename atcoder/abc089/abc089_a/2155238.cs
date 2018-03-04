8ab3267070037a9924c8d3428fbbfd34d3bb0d87
crawler <kymn0116+crawler@gmail.com>
2018-03-04 12:12:25 +0000

    add atcoder/abc089/abc089_a/2155238.cs

---
+++ atcoder/abc089/abc089_a/2155238.cs
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc089/submissions/2155238
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n / 3);
+    }
+}
