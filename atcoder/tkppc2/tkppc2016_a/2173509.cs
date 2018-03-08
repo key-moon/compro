11d7f5a13c519f200c75593718321fe259b8dc59
crawler <kymn0116+crawler@gmail.com>
2018-03-08 14:41:58 +0000

    add atcoder/tkppc2/tkppc2016_a/2173509.cs

---
+++ atcoder/tkppc2/tkppc2016_a/2173509.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/tkppc2/submissions/2173509
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine($"{Console.ReadLine()}{Console.ReadLine()}");
+    }
+}
