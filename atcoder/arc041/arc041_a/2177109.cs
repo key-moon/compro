4bc45f53200837a96c3f161c1a9f5cb14d2fe48e
crawler <kymn0116+crawler@gmail.com>
2018-03-10 02:07:31 +0000

    add atcoder/arc041/arc041_a/2177109.cs

---
+++ atcoder/arc041/arc041_a/2177109.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/arc041/submissions/2177109
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int k = int.Parse(Console.ReadLine());
+        Console.WriteLine(a.Sum() - Math.Abs(a[1] - k));
+    }
+}
