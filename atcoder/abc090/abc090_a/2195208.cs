e401e4e97f23f01e868726227d0f357a50a3fa5d
crawler <kymn0116+crawler@gmail.com>
2018-03-12 05:57:47 +0000

    add atcoder/abc090/abc090_a/2195208.cs

---
+++ atcoder/abc090/abc090_a/2195208.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc090/submissions/2195208
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine($"{Console.ReadLine()[0]}{Console.ReadLine()[1]}{Console.ReadLine()[2]}");
+    }
+}
