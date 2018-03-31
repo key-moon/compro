09e04067dc20ebdd2f1468a3b0f2364ab9c99004
crawler <kymn0116+crawler@gmail.com>
2018-03-31 14:21:31 +0000

    add atcoder/chokudai002/chokudai002_a/2284396.cs

---
+++ atcoder/chokudai002/chokudai002_a/2284396.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/chokudai002/submissions/2284396
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(string.Join("\n",Enumerable.Range(1737,100)));
+    }
+}
