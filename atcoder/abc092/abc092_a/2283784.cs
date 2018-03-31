a8d5efbd51fde0df5c1505ce640006adc33e2c25
crawler <kymn0116+crawler@gmail.com>
2018-03-31 11:40:07 +0000

    add atcoder/abc092/abc092_a/2283784.cs

---
+++ atcoder/abc092/abc092_a/2283784.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc092/submissions/2283784
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Min(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())) + Min(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+    }
+}
