304f61e3fad68f289dc21eb4646eee22b3dca464
crawler <kymn0116+crawler@gmail.com>
2018-03-31 14:35:37 +0000

    add atcoder/indeednow-quala/indeednow_2015_quala_1/2284447.cs

---
+++ atcoder/indeednow-quala/indeednow_2015_quala_1/2284447.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/indeednow-quala/submissions/2284447
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Length * Console.ReadLine().Length);
+    }
+}
