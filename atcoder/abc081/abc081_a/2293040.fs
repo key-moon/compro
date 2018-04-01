d18279086414dcc1c3c1aeac133ae7819aca079e
crawler <kymn0116+crawler@gmail.com>
2018-04-01 14:31:57 +0000

    add atcoder/abc081/abc081_a/2293040.fs

---
+++ atcoder/abc081/abc081_a/2293040.fs
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc081/submissions/2293040
+open System
+open System.Text;
+open System.Linq
+open System.Collections.Generic
+
+[<EntryPoint>]
+let main argv = 
+    Console.WriteLine(Console.ReadLine().Count(fun x -> x = '1'))
+    
+    0
