ee018ec71e9d7d3b6d67c318c14cd24ca281ec15
crawler <kymn0116+crawler@gmail.com>
2018-04-07 13:45:47 +0000

    add atcoder/arc094/arc094_a/2321743.cs

---
+++ atcoder/arc094/arc094_a/2321743.cs
@@ -0,0 +1,26 @@
+// detail: https://atcoder.jp/contests/arc094/submissions/2321743
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        int[] abc = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+        //0 0 0 -> 0
+        //1 0 0 -> 1
+        //1 1 0 -> 2
+        int count = 0;
+        int diff = abc.Max() * 3 - abc.Sum();
+        if (diff % 2 == 1)
+        {
+            diff -= 1;
+            count += 2;
+        }
+        count += diff / 2;
+        Console.WriteLine(count);
+    }
+}
