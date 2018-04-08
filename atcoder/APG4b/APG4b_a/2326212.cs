7db751f9d9bccd0200aad69965acaf18a88f6296
crawler <kymn0116+crawler@gmail.com>
2018-04-08 12:21:44 +0000

    add atcoder/APG4b/APG4b_a/2326212.cs

---
+++ atcoder/APG4b/APG4b_a/2326212.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/APG4b/submissions/2326212
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
