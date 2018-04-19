f13540cf1a3af06125407e3b1f8fe8db76ac8ddf
crawler <kymn0116+crawler@gmail.com>
2018-04-19 07:33:02 +0000

    add atcoder/APG4b/APG4b_cv/2378165.cpp

---
+++ atcoder/APG4b/APG4b_cv/2378165.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/APG4b/submissions/2378165
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
+}
