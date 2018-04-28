d99b10af0a0be1a069f40e8da11a723747261013
crawler <kymn0116+crawler@gmail.com>
2018-04-28 13:57:54 +0000

    add atcoder/agc023/agc023_b/2431190.cs

---
+++ atcoder/agc023/agc023_b/2431190.cs
@@ -0,0 +1,55 @@
+// detail: https://atcoder.jp/contests/agc023/submissions/2431190
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[][] map = Enumerable.Repeat(0, n).Select(_ => Console.ReadLine().ToArray()).ToArray();
+        //[x][y][深さ]
+        bool[][][] isSame = Enumerable.Repeat(0, n).Select(_ => Enumerable.Repeat(0, n).Select(__ => new bool[n]).ToArray()).ToArray();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    isSame[i][j][k] = map[i][j] == map[(i + k) % n][(n + j - k) % n];
+                }
+            }
+        }
+        bool[][] isAllSame = Enumerable.Repeat(0, n).Select(_ => Enumerable.Repeat(true, n).ToArray()).ToArray();
+        //スタート
+        for (int i = 0; i < n; i++)
+        {
+            //串のインデックス
+            for (int j = 0; j < n; j++)
+            {
+                //ズラす
+                for (int k = 0; k < n; k++)
+                {
+                    if (!isSame[(i + k) % n][k][j])
+                    {
+                        isAllSame[i][j] = false;
+                        break;
+                    }
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (!isAllSame[(i + j) % n][n - (j + 1)]) goto end;
+            }
+            count++;
+            end:;
+        }
+        Console.WriteLine(count * n);
+    }
+}
