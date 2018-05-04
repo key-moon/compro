1c4f0c88a0557fc51533fe2be5f60508e25c9b6d
crawler <kymn0116+crawler@gmail.com>
2018-05-04 15:38:40 +0000

    add atcoder/abc012/abc012_2/2455559.cs

---
+++ atcoder/abc012/abc012_2/2455559.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc012/submissions/2455559
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(new TimeSpan(long.Parse(Console.ReadLine()) * 10000000).ToString(@"hh\:mm\:ss"));
+
+    }
+}
