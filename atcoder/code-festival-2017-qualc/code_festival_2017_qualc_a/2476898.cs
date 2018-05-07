d61db74afa363cff3521e251f0454a5a9b92ab54
crawler <kymn0116+crawler@gmail.com>
2018-05-07 09:35:39 +0000

    add atcoder/code-festival-2017-qualc/code_festival_2017_qualc_a/2476898.cs

---
+++ atcoder/code-festival-2017-qualc/code_festival_2017_qualc_a/2476898.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/code-festival-2017-qualc/submissions/2476898
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Contains("AC") ? "Yes" : "No");
+    }
+}
