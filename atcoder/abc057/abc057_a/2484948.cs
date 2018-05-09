7182ac7950640b1c722627cddfefd98b76881f36
crawler <kymn0116+crawler@gmail.com>
2018-05-09 12:10:01 +0000

    add atcoder/abc057/abc057_a/2484948.cs

---
+++ atcoder/abc057/abc057_a/2484948.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc057/submissions/2484948
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Sum() % 24);
+    }
+}
