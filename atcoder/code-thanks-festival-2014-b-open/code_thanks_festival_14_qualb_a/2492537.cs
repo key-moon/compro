0dd5c2bb44ba30a7fea71e12abe627b31623db99
crawler <kymn0116+crawler@gmail.com>
2018-05-11 21:30:57 +0000

    add atcoder/code-thanks-festival-2014-b-open/code_thanks_festival_14_qualb_a/2492537.cs

---
+++ atcoder/code-thanks-festival-2014-b-open/code_thanks_festival_14_qualb_a/2492537.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/code-thanks-festival-2014-b-open/submissions/2492537
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Max());
+    }
+}
