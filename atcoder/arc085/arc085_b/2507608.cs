ca6c08ef4199cd5c1e7b111c371f73ff52a2a7da
crawler <kymn0116+crawler@gmail.com>
2018-05-13 03:40:44 +0000

    add atcoder/arc085/arc085_b/2507608.cs

---
+++ atcoder/arc085/arc085_b/2507608.cs
@@ -0,0 +1,17 @@
+// detail: https://atcoder.jp/contests/arc085/submissions/2507608
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        int[] nzw = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        if(a.Length != 1) Console.WriteLine(Max(Abs(nzw[2] - a.Last()), Abs(a[a.Length - 2] - a[a.Length - 1])));
+        else Console.WriteLine(Abs(nzw[2] - a.Last()));
+    }
+}
