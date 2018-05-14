2334dde37e91812cfe500c6ccee16746da671206
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:33:33 +0000

    add atcoder/abc013/abc013_1/2513755.cpp

---
+++ atcoder/abc013/abc013_1/2513755.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc013/submissions/2513755
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  char x;
+  cin >> x;
+  cout << (x - 'A' + 1) << endl;
+}
