aae96cbcf4670777a9cc0cf5940a8b3ae52ea2ee
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:22:40 +0000

    add atcoder/abc005/abc005_1/2513712.cpp

---
+++ atcoder/abc005/abc005_1/2513712.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc005/submissions/2513712
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b;
+  cin >> a >> b;
+  cout << (b / a) << endl;
+}
