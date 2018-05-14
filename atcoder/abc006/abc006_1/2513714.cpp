805926c2c47dd2ca90063b90d35307788dfba761
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:24:19 +0000

    add atcoder/abc006/abc006_1/2513714.cpp

---
+++ atcoder/abc006/abc006_1/2513714.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc006/submissions/2513714
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b;
+  cin >> a;
+  cout << (a % 3 == 0 ? "YES" : "NO") << endl;
+}
