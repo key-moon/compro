7f6c2f23f31c2221833497f837422f9180012ab9
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:26:31 +0000

    add atcoder/abc009/abc009_1/2513723.cpp

---
+++ atcoder/abc009/abc009_1/2513723.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc009/submissions/2513723
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b;
+  cin >> a;
+  cout << a / 2 + (a % 2 != 0) << endl;
+}
