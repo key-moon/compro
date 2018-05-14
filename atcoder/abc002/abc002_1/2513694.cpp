e48a4bb5694d70d1b01bbb4b53c874ce21334e6b
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:19:50 +0000

    add atcoder/abc002/abc002_1/2513694.cpp

---
+++ atcoder/abc002/abc002_1/2513694.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc002/submissions/2513694
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b;
+  cin >> a >> b;
+  cout << max(a,b) << endl;
+}
