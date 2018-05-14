e12609252c9968b27760532a533326d473891f47
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:30:05 +0000

    add atcoder/abc012/abc012_1/2513738.cpp

---
+++ atcoder/abc012/abc012_1/2513738.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc012/submissions/2513738
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b;
+  cin >> a >> b;
+  cout << b << " " << a << endl;
+}
