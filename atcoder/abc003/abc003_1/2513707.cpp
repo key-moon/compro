95b2bb16619ef373cdbf0bdfc4abac000ffd2cc3
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:21:40 +0000

    add atcoder/abc003/abc003_1/2513707.cpp

---
+++ atcoder/abc003/abc003_1/2513707.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc003/submissions/2513707
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a;
+  cin >> a;
+  cout << ((a + 1) * 5000) << endl;
+}
