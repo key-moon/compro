4361194ea7c0e4d5614b7961c13ed870a2c30858
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:25:21 +0000

    add atcoder/abc008/abc008_1/2513720.cpp

---
+++ atcoder/abc008/abc008_1/2513720.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc008/submissions/2513720
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b;
+  cin >> a >> b;
+  cout << (b - a + 1) << endl;
+}
