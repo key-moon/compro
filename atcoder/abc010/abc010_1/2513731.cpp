2f29fb116bad2d11b50fbf934f6fe4d5d8c9b938
crawler <kymn0116+crawler@gmail.com>
2018-05-14 14:28:03 +0000

    add atcoder/abc010/abc010_1/2513731.cpp

---
+++ atcoder/abc010/abc010_1/2513731.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc010/submissions/2513731
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string s;
+  cin >> s;
+  cout << s + "pp" << endl;
+}
