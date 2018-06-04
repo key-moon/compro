ded661ccd2cae1310f389cf5d6f0a3fd826b649b
crawler <kymn0116+crawler@gmail.com>
2018-06-04 07:36:17 +0000

    add atcoder/abc073/abc073_a/2616609.cs

---
+++ atcoder/abc073/abc073_a/2616609.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc073/submissions/2616609
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().ToArray().Contains('9') ? "Yes" : "No");
+    }
+}
