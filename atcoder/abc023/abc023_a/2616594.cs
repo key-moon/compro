bde456a08b9a29c5cff0bd90100225c9742a3021
crawler <kymn0116+crawler@gmail.com>
2018-06-04 07:33:56 +0000

    add atcoder/abc023/abc023_a/2616594.cs

---
+++ atcoder/abc023/abc023_a/2616594.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc023/submissions/2616594
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().ToArray().Sum(x => x - '0'));
+    }
+}
