1296e0789bae75c54f645890fb73069ddb0de1b1
crawler <kymn0116+crawler@gmail.com>
2018-06-04 07:35:44 +0000

    add atcoder/abc051/abc051_a/2616607.cs

---
+++ atcoder/abc051/abc051_a/2616607.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc051/submissions/2616607
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Replace(',',' '));
+    }
+}
