cde99dba8034f295beeaa3660126e53539c8aa1f
crawler <kymn0116+crawler@gmail.com>
2018-06-10 13:36:50 +0000

    add atcoder/abc099/abc099_a/2651848.cs

---
+++ atcoder/abc099/abc099_a/2651848.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc099/submissions/2651848
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"AB{char.ConvertFromUtf32('C'+n/1000)}");
+    }
+}
