1d95b3bad32c9d2824e9ff757966760506a6bbcf
crawler <kymn0116+crawler@gmail.com>
2018-07-22 07:02:28 +0000

    add atcoder/abc066/abc066_b/2892173.cs

---
+++ atcoder/abc066/abc066_b/2892173.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc066/submissions/2892173
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        for (int i = 1; i < s.Length; i++)
+        {
+            string t =  s.Substring(0, s.Length - i);
+            if (t.Length % 2 != 0) continue;
+            if (t.Substring(0, t.Length / 2) != t.Substring(t.Length / 2)) continue;
+            Console.WriteLine(t.Length);
+            return;
+        }
+    }
+}
