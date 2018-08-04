f49fbbbdbc1199d4651e6dd6a09dc6ea848574a2
crawler <kymn0116+crawler@gmail.com>
2018-08-04 12:01:31 +0000

    add atcoder/mujin-pc-2018/mujin_pc_2018_a/2941877.cs

---
+++ atcoder/mujin-pc-2018/mujin_pc_2018_a/2941877.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/mujin-pc-2018/submissions/2941877
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().PadRight(10).Substring(0, 5) == "MUJIN" ? "Yes" : "No");
+    }
+}
