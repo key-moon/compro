bb807a8f6972b26affce1f7a11f924bf401e67b0
crawler <kymn0116+crawler@gmail.com>
2018-09-14 13:00:45 +0000

    add aoj/0335/3135749.cpp

---
+++ aoj/0335/3135749.cpp
@@ -0,0 +1,10 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/0335/judge/3135749/C++
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long n;
+	cin >> n;
+	cout << (n * 32) << endl;
+	return 0;
+}
