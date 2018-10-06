19b819f9974e641216d4091ad16575c934e849c3
crawler <kymn0116+crawler@gmail.com>
2018-10-06 12:01:42 +0000

    add atcoder/abc112/abc112_a/3341725.cs

---
+++ atcoder/abc112/abc112_a/3341725.cs
@@ -0,0 +1,26 @@
+// detail: https://atcoder.jp/contests/abc112/submissions/3341725
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        int i = int.Parse(Console.ReadLine());
+        if (i == 1)
+        {
+            Console.WriteLine("Hello World");
+            return;
+        }
+        Console.WriteLine(int.Parse(Console.ReadLine()) + int.Parse(Console.ReadLine()));
+    }
+}
