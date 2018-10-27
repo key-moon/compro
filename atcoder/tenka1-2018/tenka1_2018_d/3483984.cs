7a638b3adf34c71df10fb3814d46458a1b8c645b
crawler <kymn0116+crawler@gmail.com>
2018-10-27 12:54:10 +0000

    add atcoder/tenka1-2018/tenka1_2018_d/3483984.cs

---
+++ atcoder/tenka1-2018/tenka1_2018_d/3483984.cs
@@ -0,0 +1,45 @@
+// detail: https://atcoder.jp/contests/tenka1-2018/submissions/3483984
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        //2つずつ
+        List<int> hoge = new List<int>();
+        for (int i = 1; i < 1000; i++)
+        {
+            if(n == (i * (i - 1)) / 2)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine(i);
+                List<int>[] res = Enumerable.Repeat(0, i).Select(_ => new List<int>()).ToArray();
+                int p = 1;
+                for (int j = 0; j < i - 1; j++)
+                {
+                    for (int k = j + 1; k < i; k++)
+                    {
+                        res[j].Add(p);
+                        res[k].Add(p);
+                        p++;
+                    }
+                }
+                Console.WriteLine(string.Join("\n", res.Select(x => $"{res[0].Count} {string.Join(" ", x)}")));
+                return;
+            }
+        }
+        Console.WriteLine("No");
+    }
+}
