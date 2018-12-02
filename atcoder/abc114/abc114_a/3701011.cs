ac62fc424e0fb7bbadbc54f6841c1bc578c756b9
crawler <kymn0116+crawler@gmail.com>
2018-12-02 12:01:05 +0000

    add atcoder/abc114/abc114_a/3701011.cs

---
+++ atcoder/abc114/abc114_a/3701011.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc114/submissions/3701011
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(new int[] { 7, 5, 3 }.Contains(int.Parse(Console.ReadLine())) ? "YES" : "NO");
+    }
+}
