131b7fb743ee344b4e7895ea4d89ddc6fd230463
crawler <kymn0116+crawler@gmail.com>
2018-12-29 11:20:13 +0000

    add atcoder/abc066/abc066_b/3891560.cs

---
+++ atcoder/abc066/abc066_b/3891560.cs
@@ -0,0 +1,28 @@
+// detail: https://atcoder.jp/contests/abc066/submissions/3891560
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using static System.Math;
+
+
+class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        for (int i = s.Length - 2; i >= 0; i--)
+        {
+            if(s.Substring(0, i) == s.Substring(0, i / 2) + s.Substring(0, i / 2))
+            {
+                Console.WriteLine(i);
+                return;
+            }
+        }
+    }
+}
