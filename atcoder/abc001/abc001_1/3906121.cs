9732e99cb42bcdf7bdce43a7f24af57706d29149
crawler <kymn0116+crawler@gmail.com>
2018-12-31 12:00:37 +0000

    add atcoder/abc001/abc001_1/3906121.cs

---
+++ atcoder/abc001/abc001_1/3906121.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc001/submissions/3906121
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using static System.Math;
+
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(int.Parse(Console.ReadLine()) - int.Parse(Console.ReadLine()));
+    }
+}
