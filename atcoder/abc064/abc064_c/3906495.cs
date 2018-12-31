47e1bc125f336756dc4b353a1cfda526a33c289c
crawler <kymn0116+crawler@gmail.com>
2018-12-31 12:12:49 +0000

    add atcoder/abc064/abc064_c/3906495.cs

---
+++ atcoder/abc064/abc064_c/3906495.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/abc064/submissions/3906495
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using static System.Math;
+
+
+class P
+{
+    static void Main()
+    {
+        int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int min = a.Where(x => x < 3200).Select(x => x / 400).Distinct().Count();
+        Console.WriteLine($"{Max(1, min)} {min + a.Count(x => x >= 3200)}");
+    }
+}
