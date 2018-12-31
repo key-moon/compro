baa0a9b0a81879acf4ce7d8c4092a8710a51de79
crawler <kymn0116+crawler@gmail.com>
2018-12-31 12:01:01 +0000

    add atcoder/abc002/abc002_1/3906135.cs

---
+++ atcoder/abc002/abc002_1/3906135.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc002/submissions/3906135
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using static System.Math;
+
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).ToArray().Max());
+    }
+}
