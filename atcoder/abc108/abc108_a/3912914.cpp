cb9f5b33402d3d072b3d7c8426b6cef6ebb219c1
crawler <kymn0116+crawler@gmail.com>
2019-01-01 16:42:57 +0000

    add atcoder/abc108/abc108_a/3912914.cpp

---
+++ atcoder/abc108/abc108_a/3912914.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc108/submissions/3912914
+#include <iostream>
+#include <cstdio>
+#include <cstdint>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <utility>
+using namespace std;
+typedef	long long ll;
+
+int main() {
+	ll a;
+	cin >> a;
+	cout << (a / 2) * (a - a / 2) << endl;
+}
