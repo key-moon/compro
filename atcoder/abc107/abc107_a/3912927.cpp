77622e70fae0047221953b730a216662df69fa5c
crawler <kymn0116+crawler@gmail.com>
2019-01-01 16:46:43 +0000

    add atcoder/abc107/abc107_a/3912927.cpp

---
+++ atcoder/abc107/abc107_a/3912927.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc107/submissions/3912927
+#include <iostream>
+#include <cstdio>
+#include <cstdint>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <utility>
+using namespace std;
+typedef	long long ll;
+
+int main() {
+	ll n,i;
+	cin >> n >> i;
+	cout << n - i + 1 << endl;
+}
