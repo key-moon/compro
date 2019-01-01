d099e75a459f5f22ac48485f576155d4fabc9596
crawler <kymn0116+crawler@gmail.com>
2019-01-01 14:45:36 +0000

    add atcoder/abc111/abc111_a/3912406.cs

---
+++ atcoder/abc111/abc111_a/3912406.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc111/submissions/3912406
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using static System.Math;
+
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Replace('1', 'x').Replace('9', '1').Replace('x', '9'));
+    }
+}
