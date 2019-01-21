6568b5c529cf1477a8194814372bc396f4b5ef43
crawler <kymn0116+crawler@gmail.com>
2019-01-21 14:54:34 +0000

    add atcoder/abc098/abc098_b/4065219.cs

---
+++ atcoder/abc098/abc098_b/4065219.cs
@@ -0,0 +1,26 @@
+// detail: https://atcoder.jp/contests/abc098/submissions/4065219
+using System;
+using System.Web;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static System.Math;
+ 
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+        Console.WriteLine(Enumerable.Range(1, n - 1).Max(x => s.Substring(0, x).Intersect(s.Substring(x)).Count()));
+    }
+}
