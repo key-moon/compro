8787db88d85fc64b1b7e5741977fe43a20896c92
crawler <kymn0116+crawler@gmail.com>
2019-01-29 14:38:36 +0000

    add atcoder/abc104/abc104_b/4123227.cs

---
+++ atcoder/abc104/abc104_b/4123227.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/abc104/submissions/4123227
+using System;
+using System.Web;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static System.Math;
+ 
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Regex.IsMatch(Console.ReadLine(), "^A[a-z]+C[a-z]+$") ? "AC" : "WA");
+    }
+}
