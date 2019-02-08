ab46ab8b75035b6a9f3ec6b9c3eccddfe9c7c5b8
crawler <kymn0116+crawler@gmail.com>
2019-02-08 09:09:57 +0000

    add atcoder/abc059/abc059_a/4195828.cpp

---
+++ atcoder/abc059/abc059_a/4195828.cpp
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc059/submissions/4195828
+//============================================================================
+#include<bits/stdc++.h>
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (n); i++)
+using namespace std;
+const ll inf = 1ll << 60;
+
+int main(){
+	string a,b,c;
+	cin >> a >> b >> c;
+	cout << (char)(a[0]+'A'-'a') << (char)(b[0]+'A'-'a') << (char)(c[0]+'A'-'a') << endl;
+}
+
+
