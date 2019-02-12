0925259496bf898d0f74c0de72de08bfefa7b77b
crawler <kymn0116+crawler@gmail.com>
2019-02-12 19:05:17 +0000

    add atcoder/abc117/abc117_a/4248338.cs

---
+++ atcoder/abc117/abc117_a/4248338.cs
@@ -0,0 +1,25 @@
+// detail: https://atcoder.jp/contests/abc117/submissions/4248338
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using static System.Math;
+
+
+class P
+{
+    static void Main()
+    {
+        int[] tx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine((double)tx[0] / (double)tx[1]);
+        Console.ReadLine();
+    }
+}
