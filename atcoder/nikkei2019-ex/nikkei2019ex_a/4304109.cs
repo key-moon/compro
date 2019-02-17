2fb958da53b8e128fdc4921b5b1f1918fdabe2b8
crawler <kymn0116+crawler@gmail.com>
2019-02-17 12:20:02 +0000

    add atcoder/nikkei2019-ex/nikkei2019ex_a/4304109.cs

---
+++ atcoder/nikkei2019-ex/nikkei2019ex_a/4304109.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/nikkei2019-ex/submissions/4304109
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(string.Join("\n", Enumerable.Range(1, Console.ReadLine().Length)));
+    }
+}
