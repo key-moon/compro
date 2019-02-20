1e0eea4f450dab9062ad869ea517a2aa17ea3e01
crawler <kymn0116+crawler@gmail.com>
2019-02-20 14:55:55 +0000

    add atcoder/nikkei2019-ex/nikkei2019ex_b/4333093.cs

---
+++ atcoder/nikkei2019-ex/nikkei2019ex_b/4333093.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/nikkei2019-ex/submissions/4333093
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+      Console.WriteLine(Sqrt(double.Parse(Console.ReadLine())).ToString().Split('.')[0]);
+    }
+}
