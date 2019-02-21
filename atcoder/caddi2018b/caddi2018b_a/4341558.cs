e7271138dd4048900fe96166cae81897b7f80f13
crawler <kymn0116+crawler@gmail.com>
2019-02-21 14:54:58 +0000

    add atcoder/caddi2018b/caddi2018b_a/4341558.cs

---
+++ atcoder/caddi2018b/caddi2018b_a/4341558.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/caddi2018b/submissions/4341558
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Count(x => x == '2'));
+    }
+}
