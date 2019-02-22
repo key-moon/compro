6d71cead4a5b9cffe22924061854912b43553347
crawler <kymn0116+crawler@gmail.com>
2019-02-22 15:22:39 +0000

    add yukicoder/721/318874.cs

---
+++ yukicoder/721/318874.cs
@@ -0,0 +1,20 @@
+// detail: https://yukicoder.me/submissions/318874
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine($"{DateTime.Parse(Console.ReadLine()).AddDays(2):yyyy/MM/dd}");
+    }
+}
