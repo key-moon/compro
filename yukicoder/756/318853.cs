70d11086736d869803145e8d8daa59a6ba41b770
crawler <kymn0116+crawler@gmail.com>
2019-02-22 15:08:55 +0000

    add yukicoder/756/318853.cs

---
+++ yukicoder/756/318853.cs
@@ -0,0 +1,20 @@
+// detail: https://yukicoder.me/submissions/318853
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(".123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960"[int.Parse(Console.ReadLine())]);
+    }
+}
