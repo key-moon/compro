d8d0419bb10a6896f83963032843820afbaa40d3
crawler <kymn0116+crawler@gmail.com>
2019-02-23 09:22:27 +0000

    add yukicoder/485/319325.cs

---
+++ yukicoder/485/319325.cs
@@ -0,0 +1,22 @@
+// detail: https://yukicoder.me/submissions/319325
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Int = System.Int64;
+using Debug = System.Diagnostics.Debug;
+
+class P
+{
+    static void Main()
+    {
+        var a = Console.ReadLine().Split().Select(int.Parse).ToList();
+        Console.WriteLine(a[1] % a[0] == 0 ? (a[1] / a[0]).ToString() : "NO");
+    }
+}
