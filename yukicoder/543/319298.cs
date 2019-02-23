3f9c1c9ba6aeab40c7943271858286b85b79b372
crawler <kymn0116+crawler@gmail.com>
2019-02-23 09:03:41 +0000

    add yukicoder/543/319298.cs

---
+++ yukicoder/543/319298.cs
@@ -0,0 +1,21 @@
+// detail: https://yukicoder.me/submissions/319298
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Int = System.Int64;
+using Debug = System.Diagnostics.Debug;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(string.Join(" ", Console.ReadLine().Reverse()));
+    }
+}
