a27ddcbba6fe994b42532874506f36ab0901dfde
crawler <kymn0116+crawler@gmail.com>
2019-02-23 08:57:15 +0000

    add yukicoder/558/319290.cs

---
+++ yukicoder/558/319290.cs
@@ -0,0 +1,21 @@
+// detail: https://yukicoder.me/submissions/319290
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Int = System.Int64;
+using Debug = System.Diagnostics.Debug;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Contains("575") ? "YES" : "NO");
+    }
+}
