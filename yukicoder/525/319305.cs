52ad94e441fab1fc3e1f9eace6b01efce45e2b16
crawler <kymn0116+crawler@gmail.com>
2019-02-23 09:08:56 +0000

    add yukicoder/525/319305.cs

---
+++ yukicoder/525/319305.cs
@@ -0,0 +1,21 @@
+// detail: https://yukicoder.me/submissions/319305
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Int = System.Int64;
+using Debug = System.Diagnostics.Debug;
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(DateTime.Parse(Console.ReadLine()).AddMinutes(5).ToString("HH:mm"));
+    }
+}
