a4e2764202ce3be3113b2bdb97be788bb2885a4d
crawler <kymn0116+crawler@gmail.com>
2019-02-23 07:55:02 +0000

    add yukicoder/63/319222.cs

---
+++ yukicoder/63/319222.cs
@@ -0,0 +1,22 @@
+// detail: https://yukicoder.me/submissions/319222
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Int = System.Int64;
+using Debug = System.Diagnostics.Debug;
+
+class P
+{
+    static void Main()
+    {
+        var lk = Console.ReadLine().Split().Select(int.Parse).ToList();
+        Console.WriteLine((lk[0] - 1) / (lk[1] * 2) * lk[1]);
+    }
+}
