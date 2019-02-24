d88395f151876dd869ffb7bf31abb08793b4a6f3
crawler <kymn0116+crawler@gmail.com>
2019-02-24 12:37:57 +0000

    add atcoder/abc119/abc119_a/4373513.cs

---
+++ atcoder/abc119/abc119_a/4373513.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc119/submissions/4373513
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+
+
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(DateTime.Parse(Console.ReadLine()) <= new DateTime(2019, 4, 30) ? "Heisei" : "TBD");
+    }
+}
