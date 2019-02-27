ce2566435da8b334de8ca8bc38258f2d92abad47
crawler <kymn0116+crawler@gmail.com>
2019-02-27 14:58:19 +0000

    add atcoder/code-festival-2018-qualb/code_festival_2018_qualb_a/4406638.cs

---
+++ atcoder/code-festival-2018-qualb/code_festival_2018_qualb_a/4406638.cs
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/code-festival-2018-qualb/submissions/4406638
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using static System.Math;
+using System.Collections;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(100 - 100 / n);
+    }
+}
