e743f35419bfa2fb2c5b67a657918ddade956333
crawler <kymn0116+crawler@gmail.com>
2019-03-04 17:19:54 +0000

    add atcoder/atc001/unionfind_a/4472948.cs

---
+++ atcoder/atc001/unionfind_a/4472948.cs
@@ -0,0 +1,136 @@
+// detail: https://atcoder.jp/contests/atc001/submissions/4472948
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+class P
+{
+    static void Main()
+    {
+        int[] nq = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        UnionFind uf = new UnionFind(nq[0]);
+        for (int i = 0; i < nq[1]; i++)
+        {
+            int[] pab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (pab[0] == 0) uf.TryUnite(pab[1], pab[2]);
+            else Console.WriteLine(uf.Find(pab[1]) == uf.Find(pab[2]) ? "Yes" : "No");       
+        }
+    }
+}
+
+
+class UnionFind
+{
+    public int Size;
+    public int GroupCount;
+    List<int> Sizes;
+    List<int> Parent;
+    public UnionFind(int count)
+    {
+        Size = 0;
+        GroupCount = 0;
+        Parent = new List<int>(count);
+        Sizes = new List<int>(count);
+        ExtendSize(count);
+    }
+    public bool TryUnite(int x, int y)
+    {
+        int xp = Find(x);
+        int yp = Find(y);
+        if (yp == xp) return false;
+        if (Sizes[xp] < Sizes[yp])
+        {
+            var tmp = xp;
+            xp = yp;
+            yp = tmp;
+        }
+        GroupCount--;
+        Parent[yp] = xp;
+        Sizes[xp] += Sizes[yp];
+        return true;
+    }
+    public IEnumerable<int> AllRepresents => Parent.Where(x => x == Parent[x]);
+    public int GetSize(int x) => Sizes[Find(x)];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Find(int x)
+    {
+        while (x != Parent[x])
+        {
+            Parent[x] = Parent[Parent[x]];
+            x = Parent[x];
+        }
+        return x;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void ExtendSize(int treeSize)
+    {
+        if (treeSize <= Size) return;
+        Parent.Capacity = treeSize;
+        Sizes.Capacity = treeSize;
+        while (Size < treeSize)
+        {
+            Parent.Add(Size);
+            Sizes.Add(1);
+            Size++;
+            GroupCount++;
+        }
+    }
+}
+
+class SegmentTree<T>
+{
+    public int Size { get; private set; }
+    T IdentityElement;
+    T[] Data;
+    Func<T, T, T> Merge;
+    int LeafCount;
+
+    public SegmentTree(int size, T identityElement, Func<T, T, T> merge) : this(Enumerable.Repeat(identityElement, size).ToArray(), identityElement, merge) { }
+
+    public SegmentTree(T[] elems, T identityElement, Func<T, T, T> merge)
+    {
+        Size = elems.Length;
+        Merge = merge;
+        IdentityElement = identityElement;
+        LeafCount = 1;
+        while (LeafCount < elems.Length) LeafCount <<= 1;
+
+        Data = new T[LeafCount * 2];
+        elems.CopyTo(Data, LeafCount);
+        for (int i = LeafCount - 1; i >= 1; i--) Data[i] = Merge(Data[i * 2], Data[i * 2 + 1]);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Update(int i, T x)
+    {
+        i += LeafCount;
+        Data[i] = x;
+        while (0 < (i >>= 1)) Data[i] = Merge(Data[i * 2], Data[i * 2 + 1]);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Query(int l, int r)
+    {
+        l += LeafCount;
+        T lRes = IdentityElement;
+        r += LeafCount - 1;
+        T rRes = IdentityElement;
+        while (l <= r)
+        {
+            if (l == r)
+            {
+                lRes = Merge(lRes, Data[l]);
+                break;
+            }
+            if (l % 2 == 1) lRes = Merge(lRes, Data[l]);
+            if (r % 2 == 0) rRes = Merge(Data[r], rRes);
+            l = (l + 1) / 2;
+            r = (r - 1) / 2;
+        }
+        return Merge(lRes, rRes);
+    }
+}
