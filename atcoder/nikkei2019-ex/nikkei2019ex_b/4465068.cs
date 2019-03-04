2b867f9ae3182b5b1a4d880f60fefd42564504e5
crawler <kymn0116+crawler@gmail.com>
2019-03-04 04:31:40 +0000

    add atcoder/nikkei2019-ex/nikkei2019ex_b/4465068.cs

---
+++ atcoder/nikkei2019-ex/nikkei2019ex_b/4465068.cs
@@ -0,0 +1,17 @@
+// detail: https://atcoder.jp/contests/nikkei2019-ex/submissions/4465068
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using System.Runtime.CompilerServices;
+
+class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine((int)Floor(Sqrt(n)));
+    }
+}
