a0fd5d5767ee09207a4e7ad730248ad7772dc6fb
crawler <kymn0116+crawler@gmail.com>
2019-03-04 13:04:52 +0000

    add aoj/DSL_2_A/3409967.cs

---
+++ aoj/DSL_2_A/3409967.cs
@@ -0,0 +1,83 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/DSL_2_A/judge/3409967/C#
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+class P
+{
+    static void Main()
+    {
+        var nq = Console.ReadLine().Split().Select(int.Parse).ToList();
+        SegmentTree<int> RMQ = new SegmentTree<int>(Enumerable.Repeat(int.MaxValue, nq[0]).ToArray(), int.MaxValue, (x, y) => Min(x, y));
+        for (int i = 0; i < nq[1]; i++)
+        {
+            var a = Console.ReadLine().Split().Select(int.Parse).ToList();
+            if (a[0] == 0) RMQ.Update(a[1], a[2]);
+            if (a[0] == 1) Console.WriteLine(RMQ.Query(a[1], a[2] + 1));
+        }
+    }
+}
+
+class SegmentTree<T>
+{
+    public int Size { get; private set; }
+    T IdentityElement;
+    T[] Data;
+    Func<T, T, T> Merge;
+    int LeafCount;
+
+    public SegmentTree(int size, T identityElement, Func<T, T, T> merge) : this(Enumerable.Repeat(identityElement,size).ToArray(), identityElement, merge) { }
+
+    public SegmentTree(T[] elems, T identityElement, Func<T, T, T> merge)
+    {
+        Size = elems.Length;
+        Merge = merge;
+        IdentityElement = identityElement;
+        LeafCount = 1;
+        while (LeafCount < elems.Length) LeafCount <<= 1;
+
+        Data = new T[LeafCount * 2];
+        elems.CopyTo(Data, LeafCount);
+        for (int i = LeafCount - 1; i >= 0; i--) Data[i] = Merge(Data[i * 2], Data[i * 2 + 1]);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Update(int i, T x)
+    {
+        i += LeafCount;
+        Data[i] = x;
+        while (0 < (i >>= 1)) Data[i] = Merge(Data[i * 2], Data[i * 2 + 1]);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Query(int l, int r)
+    {
+        l += LeafCount;
+        T lRes = IdentityElement;
+        r += LeafCount - 1;
+        T rRes = IdentityElement;
+        while (l <= r)
+        {
+            if (l == r)
+            {
+                lRes = Merge(lRes, Data[l]);
+                break;
+            }
+            if (l % 2 == 1) lRes = Merge(lRes, Data[l]);
+            if (r % 2 == 0) rRes = Merge(Data[r], rRes);
+            l = (l + 1) / 2;
+            r = (r - 1) / 2;
+        }
+        return Merge(lRes, rRes);
+    }
+}
