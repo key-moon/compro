e546e2fd3aca30236cb0810ea71d946943c3fe69
crawler <kymn0116+crawler@gmail.com>
2019-03-08 01:31:58 +0000

    add aoj/DSL_2_E/3417410.cs

---
+++ aoj/DSL_2_E/3417410.cs
@@ -0,0 +1,98 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/DSL_2_E/judge/3417410/C#
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+class P
+{
+    static void Main()
+    {
+        var nq = Console.ReadLine().Split().Select(int.Parse).ToList();
+        SegmentTree<int> RAQ = new SegmentTree<int>(nq[0], 0, (x, y) => x + y);
+        for (int i = 0; i < nq[1]; i++)
+        {
+            int[] query = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (query[0] == 0) RAQ.Operate(query[1] - 1, query[2], query[3]);
+            else Console.WriteLine(RAQ.Query(query[1] - 1));
+        }
+    }
+}
+
+
+class SegmentTree<T>
+{
+    public int Size { get; private set; }
+    T IdentityElement;
+    Func<T, T, T> Merge;
+    int LeafCount;
+    int TreeHeight;
+    T[] Datas;
+
+    public SegmentTree(int size, T identityElement, Func<T, T, T> merge) : this(Enumerable.Repeat(identityElement, size).ToArray(), identityElement, merge) { }
+
+    public SegmentTree(T[] elems, T identityElement, Func<T, T, T> merge)
+    {
+        Size = elems.Length;
+        IdentityElement = identityElement;
+        Merge = merge;
+        LeafCount = 1;
+        TreeHeight = 1;
+        while (LeafCount < elems.Length)
+        {
+            LeafCount <<= 1;
+            TreeHeight++;
+        }
+
+        Datas = Enumerable.Repeat(identityElement, LeafCount).Concat(elems).Concat(Enumerable.Repeat(identityElement, LeafCount - Size)).ToArray();
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Operate(int l, int r, T x)
+    {
+        l += LeafCount;
+        r += LeafCount - 1;
+        Propagate(l);
+        Propagate(r);
+        while (l <= r)
+        {
+            if (l % 2 == 1) Datas[l] = Merge(Datas[l], x);
+            if (r % 2 == 0) Datas[r] = Merge(Datas[r], x);
+            l = (l + 1) / 2;
+            r = (r - 1) / 2;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Query(int index)
+    {
+        index += LeafCount;
+        Propagate(index);
+        return Datas[index];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Propagate(int sectionIndex)
+    {
+        for (int i = TreeHeight - 1; i >= 1; i--) Eval(sectionIndex >> i);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Eval(int sectionIndex)
+    {
+        Datas[sectionIndex * 2] = Merge(Datas[sectionIndex * 2], Datas[sectionIndex]);
+        Datas[sectionIndex * 2 + 1] = Merge(Datas[sectionIndex * 2 + 1], Datas[sectionIndex]);
+        Datas[sectionIndex] = IdentityElement;
+    }
+}
+
