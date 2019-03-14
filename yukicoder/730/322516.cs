6e15cbd3d65eb146c5630d4f32823ce57b520e68
crawler <kymn0116+crawler@gmail.com>
2019-03-14 03:54:55 +0000

    add yukicoder/730/322516.cs

---
+++ yukicoder/730/322516.cs
@@ -0,0 +1,24 @@
+// detail: https://yukicoder.me/submissions/322516
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Length == s.Distinct().Count() ? "YES" : "NO");
+    }
+}
