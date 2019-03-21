9f478980fae3ae2e73258f478b82c021c7c2b327
crawler <kymn0116+crawler@gmail.com>
2019-03-21 09:01:07 +0000

    add atcoder/chokudai_S001/chokudai_S001_a/4648336.cs

---
+++ atcoder/chokudai_S001/chokudai_S001_a/4648336.cs
@@ -0,0 +1,40 @@
+// detail: https://atcoder.jp/contests/chokudai_S001/submissions/4648336
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        int n = Read();
+        int max = 0;
+        for (int i = 0; i < n; i++) max = Max(max, Read());
+        Console.WriteLine(max);
+    }
+
+    static readonly TextReader In = Console.In;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static int Read()
+    {
+        int res = 0;
+        int next = In.Read();
+        while (48 > next || next > 57) next = In.Read();
+        while (48 <= next && next <= 57)
+        {
+            res = res * 10 + next - 48;
+            next = In.Read();
+        }
+        return res;
+    }
+}
