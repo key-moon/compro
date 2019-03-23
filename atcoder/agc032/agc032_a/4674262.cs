3f4ec2aca7c3b754f77b48b9c2c5e8bcdb974bdb
crawler <kymn0116+crawler@gmail.com>
2019-03-23 14:24:36 +0000

    add atcoder/agc032/agc032_a/4674262.cs

---
+++ atcoder/agc032/agc032_a/4674262.cs
@@ -0,0 +1,46 @@
+// detail: https://atcoder.jp/contests/agc032/submissions/4674262
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> operations = new List<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            //操作列のn個目に挿入する場合は
+            int currentBack = 0;
+            int insertOps = 0;
+            for (int j = a.Count - 1; j >= 0; j--)
+            {
+                if (a[j] < i) currentBack++;
+                if (a[j] == i)
+                {
+                    if (operations.Count < currentBack + insertOps + i - 1)
+                    {
+                        Console.WriteLine(-1);
+                        return;
+                    }
+                    else
+                    {
+                        operations.Insert(currentBack + insertOps + i - 1, i);
+                        insertOps++;
+                    }
+                }
+            }
+        }
+        Console.WriteLine(string.Join("\n", operations));
+    }
+}
