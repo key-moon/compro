3b89a60a7f18227d30f68ce95187ddd16fca1844
crawler <kymn0116+crawler@gmail.com>
2019-03-24 13:38:31 +0000

    add atcoder/abc122/abc122_a/4701388.cs

---
+++ atcoder/abc122/abc122_a/4701388.cs
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/abc122/submissions/4701388
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        if (s == "A") Console.WriteLine("T");
+        if (s == "T") Console.WriteLine("A");
+        if (s == "G") Console.WriteLine("C");
+        if (s == "C") Console.WriteLine("G");
+    }
+}
