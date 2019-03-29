4b22e9fe85bf3458c3ca6fcb5c771f78c09e4328
crawler <kymn0116+crawler@gmail.com>
2019-03-29 11:44:16 +0000

    add yukicoder/3040/330009.cs

---
+++ yukicoder/3040/330009.cs
@@ -0,0 +1,26 @@
+// detail: https://yukicoder.me/submissions/330009
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Debug.WriteLine(ab.Sum());
+        Console.WriteLine("A + B");
+    }
+}
