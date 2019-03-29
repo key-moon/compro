e7741f6ce90c46a61cb895be49008faa3dccf199
crawler <kymn0116+crawler@gmail.com>
2019-03-29 08:40:20 +0000

    add yukicoder/508/329960.cs

---
+++ yukicoder/508/329960.cs
@@ -0,0 +1,21 @@
+// detail: https://yukicoder.me/submissions/329960
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Round(Sqrt(long.Parse(Console.ReadLine()) / PI)));
+    }
+}
