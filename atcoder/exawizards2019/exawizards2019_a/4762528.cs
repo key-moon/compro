109236687331110aa3d681bd2f3145b20cb8fc48
crawler <kymn0116+crawler@gmail.com>
2019-03-30 12:00:42 +0000

    add atcoder/exawizards2019/exawizards2019_a/4762528.cs

---
+++ atcoder/exawizards2019/exawizards2019_a/4762528.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/exawizards2019/submissions/4762528
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Distinct().Count() == 1 ? "Yes" : "No");
+    }
+}
