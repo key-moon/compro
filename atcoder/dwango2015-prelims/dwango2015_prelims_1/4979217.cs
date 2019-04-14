4bf66145ecdd8a48ee6b50a13ebcb0ba0d990165
crawler <kymn0116+crawler@gmail.com>
2019-04-14 14:21:02 +0000

    add atcoder/dwango2015-prelims/dwango2015_prelims_1/4979217.cs

---
+++ atcoder/dwango2015-prelims/dwango2015_prelims_1/4979217.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/dwango2015-prelims/submissions/4979217
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(int.Parse(Console.ReadLine()) * 525 + int.Parse(Console.ReadLine()) * 15);
+    }
+}
+
