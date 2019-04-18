f81274cae1da0e4f594864c25a8a2da81a1b6631
crawler <kymn0116+crawler@gmail.com>
2019-04-18 16:03:16 +0000

    add yukicoder/441/339857.cs

---
+++ yukicoder/441/339857.cs
@@ -0,0 +1,24 @@
+// detail: https://yukicoder.me/submissions/339857
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        var a = Console.ReadLine().Split().Select(BigInteger.Parse).ToArray();
+        Console.WriteLine(a[0] * a[1] > a[0] + a[1] ? "P" : a[0] * a[1] < a[0] + a[1] ? "S" : "E");
+    }
+}
