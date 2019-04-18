ccaeed1247af502bcb91bd15b3000c718e2803c3
crawler <kymn0116+crawler@gmail.com>
2019-04-18 15:39:00 +0000

    add yukicoder/350/339843.cs

---
+++ yukicoder/350/339843.cs
@@ -0,0 +1,24 @@
+// detail: https://yukicoder.me/submissions/339843
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        var a = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+        Console.WriteLine(Floor(a[0] * a[1]));
+    }
+}
