7b929dd1ea868590748d438c2d5e8e7af3162cfb
crawler <kymn0116+crawler@gmail.com>
2019-04-18 10:15:58 +0000

    add yukicoder/57/339757.cs

---
+++ yukicoder/57/339757.cs
@@ -0,0 +1,23 @@
+// detail: https://yukicoder.me/submissions/339757
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(3.5 * int.Parse(Console.ReadLine()));
+    }
+}
