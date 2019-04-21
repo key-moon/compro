a6ffa7bc907990e03bc0250f2ea29f027b1b9954
crawler <kymn0116+crawler@gmail.com>
2019-04-21 09:12:59 +0000

    add atcoder/utpc2012/utpc2012_02/5075313.cs

---
+++ atcoder/utpc2012/utpc2012_02/5075313.cs
@@ -0,0 +1,25 @@
+// detail: https://atcoder.jp/contests/utpc2012/submissions/5075313
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(string.Join("", s.Reverse().Distinct().Concat("ABCDEFGH".Except(s)).Reverse()));
+    }
+}
