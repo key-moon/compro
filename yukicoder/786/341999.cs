f7cf8b68e3c7c05b74179cf1e08b8a2ad5a6d33f
crawler <kymn0116+crawler@gmail.com>
2019-04-25 05:07:43 +0000

    add yukicoder/786/341999.cs

---
+++ yukicoder/786/341999.cs
@@ -0,0 +1,31 @@
+// detail: https://yukicoder.me/submissions/341999
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using Debug = System.Diagnostics.Debug;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using System.Runtime.CompilerServices;
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) dp[i] = dp[i - 2] + dp[i - 1];
+        Console.WriteLine(dp.Last());
+    }
+}
