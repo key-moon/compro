342927de2148ab086be8b8c3a0488c8f3001c795
crawler <kymn0116+crawler@gmail.com>
2019-04-25 05:03:59 +0000

    add yukicoder/790/341998.cs

---
+++ yukicoder/790/341998.cs
@@ -0,0 +1,27 @@
+// detail: https://yukicoder.me/submissions/341998
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using Debug = System.Diagnostics.Debug;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using System.Runtime.CompilerServices;
+
+static class P
+{
+    static void Main()
+    {
+        var catalan = new int[] { 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012, 742900, 2674440, 9694845 };
+        Console.WriteLine(catalan[int.Parse(Console.ReadLine())]);
+    }
+}
