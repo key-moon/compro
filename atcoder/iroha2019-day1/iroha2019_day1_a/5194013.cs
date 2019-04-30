f9ee734f0ef8080a5a69f7617298034f4a0f3827
crawler <kymn0116+crawler@gmail.com>
2019-04-30 04:00:19 +0000

    add atcoder/iroha2019-day1/iroha2019_day1_a/5194013.cs

---
+++ atcoder/iroha2019-day1/iroha2019_day1_a/5194013.cs
@@ -0,0 +1,18 @@
+// detail: https://atcoder.jp/contests/iroha2019-day1/submissions/5194013
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine()[0]);
+    }
+}
