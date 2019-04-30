3db4ca66361bc653d7e965359e96b3aa6ebc7ac3
crawler <kymn0116+crawler@gmail.com>
2019-04-30 04:02:45 +0000

    add atcoder/iroha2019-day1/iroha2019_day1_c/5194357.cs

---
+++ atcoder/iroha2019-day1/iroha2019_day1_c/5194357.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/iroha2019-day1/submissions/5194357
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(string.Join("\n", Enumerable.Range(n - 7, 8)));
+    }
+}
