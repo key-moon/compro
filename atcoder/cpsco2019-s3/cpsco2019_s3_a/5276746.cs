64bc40ea3282c33a333122a453932dcae66379b1
crawler <kymn0116+crawler@gmail.com>
2019-05-05 05:44:12 +0000

    add atcoder/cpsco2019-s3/cpsco2019_s3_a/5276746.cs

---
+++ atcoder/cpsco2019-s3/cpsco2019_s3_a/5276746.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/cpsco2019-s3/submissions/5276746
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Replace('O','x').Replace('A','O').Replace('x','A'));
+    }
+}
