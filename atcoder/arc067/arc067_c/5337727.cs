7120d9c0aa321eabce969d0ca4c3e5e2f295feae
crawler <kymn0116+crawler@gmail.com>
2019-05-11 07:04:36 +0000

    add atcoder/arc067/arc067_c/5337727.cs

---
+++ atcoder/arc067/arc067_c/5337727.cs
@@ -0,0 +1,123 @@
+// detail: https://atcoder.jp/contests/arc067/submissions/5337727
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Debug = System.Diagnostics.Debug;
+using System.Runtime.CompilerServices;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        var nabcd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nabcd[0];
+        var a = nabcd[1];
+        var b = nabcd[2];
+        var c = nabcd[3];
+        var d = nabcd[4];
+        ModInt[] table = new ModInt[n + 1];
+        table[0] = 1;
+        for (int memberCount = a; memberCount <= b; memberCount++)
+        {
+            ModInt[] newTable = table.ToArray();
+            for (int groupCount = c; groupCount <= d; groupCount++)
+            {
+                var duplicate = Power(Factorial(memberCount), groupCount) * Factorial(groupCount);
+                //選択したものをi個にする→選択する前はi - memberCount * groupCount個残りのn - (i - memberCount * groupCount)個の中からmemberCount * groupCount個選ぶ
+                for (int i = table.Length - 1; i >= memberCount * groupCount; i--)
+                    newTable[i] += table[i - memberCount * groupCount] * (Factorial(n - i + memberCount * groupCount) * FactorialInverse(n - i)) / duplicate;
+            }
+            table = newTable;
+        }
+        Console.WriteLine(table[n]);
+    }
+
+    static ModInt Power(ModInt n, long m)
+    {
+        ModInt pow = n;
+        ModInt res = 1;
+        while (m > 0)
+        {
+            if ((m & 1) == 1) res *= pow;
+            pow *= pow;
+            m >>= 1;
+        }
+        return res;
+    }
+    static List<ModInt> factorialMemo = new List<ModInt>() { 1 };
+    static ModInt Factorial(int x)
+    {
+        for (int i = factorialMemo.Count; i <= x; i++) factorialMemo.Add(factorialMemo.Last() * i);
+        return factorialMemo[x];
+    }
+    static List<ModInt> factorialInverseMemo = new List<ModInt>() { 1 };
+    static ModInt FactorialInverse(int x)
+    {
+        for (int i = factorialInverseMemo.Count; i <= x; i++) factorialInverseMemo.Add(factorialInverseMemo.Last() / i);
+        return factorialInverseMemo[x];
+    }
+}
+
+struct ModInt
+{
+    const int MOD = 1000000007;
+    long Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ModInt(long data) { if ((Data = data % MOD) < 0) Data += MOD; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, ModInt b)
+    {
+        long res = a.Data + b.Data;
+        return new ModInt() { Data = res >= MOD ? res - MOD : res };
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, long b) => a.Data + b;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, ModInt b)
+    {
+        long res = a.Data - b.Data;
+        return new ModInt() { Data = res < 0 ? res + MOD : res };
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, long b) => a.Data - b;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator /(ModInt a, ModInt b) => a.Data * GetInverse(b);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => Data.ToString();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static long GetInverse(long a)
+    {
+        long div;
+        long p = MOD;
+        long x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2;
+            div = a / p;
+            x1 -= x2 * div;
+            y1 -= y2 * div;
+            a %= p;
+            if (a == 1) return x1;
+            div = p / a;
+            x2 -= x1 * div;
+            y2 -= y1 * div;
+            p %= a;
+        }
+    }
+}
