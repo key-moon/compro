6cf81a39e5f2cb439079ede83adb24e715bd122c
crawler <kymn0116+crawler@gmail.com>
2019-05-19 12:35:51 +0000

    add atcoder/abc126/abc126_e/5464416.cs

---
+++ atcoder/abc126/abc126_e/5464416.cs
@@ -0,0 +1,88 @@
+// detail: https://atcoder.jp/contests/abc126/submissions/5464416
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        UnionFind uf = new UnionFind(nm[0]);
+        for (int i = 0; i < nm[1]; i++)
+        {
+            var xyz = Console.ReadLine().Split().Select(x => int.Parse(x) - 1).ToArray();
+            uf.TryUnite(xyz[0], xyz[1]);
+        }
+        Console.WriteLine(uf.GroupCount);
+    }
+}
+
+
+class UnionFind
+{
+    public int Size { get; private set; }
+    public int GroupCount { get; private set; }
+    List<int> Sizes;
+    List<int> Parent;
+    public UnionFind(int count)
+    {
+        Size = 0;
+        GroupCount = 0;
+        Parent = new List<int>(count);
+        Sizes = new List<int>(count);
+        ExtendSize(count);
+    }
+    public bool TryUnite(int x, int y)
+    {
+        int xp = Find(x);
+        int yp = Find(y);
+        if (yp == xp) return false;
+        if (Sizes[xp] < Sizes[yp])
+        {
+            var tmp = xp;
+            xp = yp;
+            yp = tmp;
+        }
+        GroupCount--;
+        Parent[yp] = xp;
+        Sizes[xp] += Sizes[yp];
+        return true;
+    }
+    public IEnumerable<int> AllRepresents => Parent.Where(x => x == Parent[x]);
+    public int GetSize(int x) => Sizes[Find(x)];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Find(int x)
+    {
+        while (x != Parent[x])
+        {
+            Parent[x] = Parent[Parent[x]];
+            x = Parent[x];
+        }
+        return x;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void ExtendSize(int treeSize)
+    {
+        if (treeSize <= Size) return;
+        Parent.Capacity = treeSize;
+        Sizes.Capacity = treeSize;
+        while (Size < treeSize)
+        {
+            Parent.Add(Size);
+            Sizes.Add(1);
+            Size++;
+            GroupCount++;
+        }
+    }
+}
