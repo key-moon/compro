9871243593773ae553763c33615da8504164a9fd
crawler <kymn0116+crawler@gmail.com>
2019-05-20 11:26:23 +0000

    add atcoder/caddi2019/caddi2019_a/5492555.cs

---
+++ atcoder/caddi2019/caddi2019_a/5492555.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/caddi2019/submissions/5492555
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        for (int i = 0; i < 1000; i++)
+            Console.WriteLine("-1 -1 -1");
+    }
+}
