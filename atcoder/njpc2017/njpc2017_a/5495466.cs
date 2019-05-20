1c9cf8c79b1bf12ae2063d774bc45569c10ae62e
crawler <kymn0116+crawler@gmail.com>
2019-05-20 15:29:39 +0000

    add atcoder/njpc2017/njpc2017_a/5495466.cs

---
+++ atcoder/njpc2017/njpc2017_a/5495466.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/njpc2017/submissions/5495466
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        int l = int.Parse(Console.ReadLine());
+        Console.WriteLine(string.Join("", Console.ReadLine().Take(l)));
+    }
+}
