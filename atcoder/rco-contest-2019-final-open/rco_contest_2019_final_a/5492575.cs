26e32efce701a60b0637bf9c3a0310fd96c4a9d2
crawler <kymn0116+crawler@gmail.com>
2019-05-20 11:28:02 +0000

    add atcoder/rco-contest-2019-final-open/rco_contest_2019_final_a/5492575.cs

---
+++ atcoder/rco-contest-2019-final-open/rco_contest_2019_final_a/5492575.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/rco-contest-2019-final-open/submissions/5492575
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(-1);
+    }
+}
