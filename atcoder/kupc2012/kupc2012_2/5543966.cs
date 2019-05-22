c7f588067379f2253df8640f4eea5e272028016a
crawler <kymn0116+crawler@gmail.com>
2019-05-22 07:58:29 +0000

    add atcoder/kupc2012/kupc2012_2/5543966.cs

---
+++ atcoder/kupc2012/kupc2012_2/5543966.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/kupc2012/submissions/5543966
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.First() == 'x' && s.Last() == 'x' ? "x" : "o");
+    }
+}
