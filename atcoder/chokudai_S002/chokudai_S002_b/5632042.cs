bcbe38d18d3905103b1759f5c1d71e1e56708ee2
crawler <kymn0116+crawler@gmail.com>
2019-05-26 11:45:10 +0000

    add atcoder/chokudai_S002/chokudai_S002_b/5632042.cs

---
+++ atcoder/chokudai_S002/chokudai_S002_b/5632042.cs
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/chokudai_S002/submissions/5632042
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(ab[0] % ab[1]);
+        }
+    }
+}
