c1d64bc9f3e54fcce63c0f36dd2ac3dacaab8074
crawler <kymn0116+crawler@gmail.com>
2019-05-26 11:56:26 +0000

    add atcoder/chokudai_S002/chokudai_S002_g/5632280.cs

---
+++ atcoder/chokudai_S002/chokudai_S002_g/5632280.cs
@@ -0,0 +1,42 @@
+// detail: https://atcoder.jp/contests/chokudai_S002/submissions/5632280
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var a = ab[0];
+            var b = ab[1];
+            Console.WriteLine(GCD(a, b));
+        }
+    }
+
+    static long GCD(long a, long b)
+    {
+        while (true)
+        {
+            if (b == 0) return a;
+            a %= b;
+            if (a == 0) return b;
+            b %= a;
+        }
+    }
+}
