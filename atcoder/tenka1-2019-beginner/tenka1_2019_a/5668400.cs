0904aebc463545f01499b7a28452ef0caa867b26
crawler <kymn0116+crawler@gmail.com>
2019-05-27 17:15:36 +0000

    add atcoder/tenka1-2019-beginner/tenka1_2019_a/5668400.cs

---
+++ atcoder/tenka1-2019-beginner/tenka1_2019_a/5668400.cs
@@ -0,0 +1,39 @@
+// detail: https://atcoder.jp/contests/tenka1-2019-beginner/submissions/5668400
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        var a = Read();
+        var b = Read();   
+        var c = Read();
+        Console.WriteLine((a - c) * (c - b) > 0 ? "Yes" : "No");
+    }
+
+    static readonly TextReader In = Console.In;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static int Read()
+    {
+        int res = 0;
+        int next = In.Read();
+        while (48 > next || next > 57) next = In.Read();
+        while (48 <= next && next <= 57)
+        {
+            res = res * 10 + next - 48;
+            next = In.Read();
+        }
+        return res;
+    }
+}
