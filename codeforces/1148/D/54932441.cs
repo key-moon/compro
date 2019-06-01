43bc40446fd25a3c930a198955656635b17bd275
crawler <kymn0116+crawler@gmail.com>
2019-06-01 15:35:39 +0000

    add codeforces/1148/D/54932441.cs

---
+++ codeforces/1148/D/54932441.cs
@@ -0,0 +1,80 @@
+// detail: https://codeforces.com/contest/1148/submission/54932441
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        Random rng = new Random();
+        int n = int.Parse(Console.ReadLine());
+        var a = Enumerable.Range(1, n).Select(x => new Tuple<int[], int>(Console.ReadLine().Split().Select(int.Parse).ToArray(), x)).ToArray();
+        var ascend = a.Where(x => x.Item1[0] > x.Item1[1]).OrderBy(_ => rng.Next()).OrderBy(x => x.Item1[0]).Select(x => x.Item2).ToArray();
+        var descend = a.Where(x => x.Item1[0] < x.Item1[1]).OrderBy(_ => rng.Next()).OrderByDescending(x => x.Item1[0]).Select(x => x.Item2).ToArray();
+        var res = ascend.Length < descend.Length ? descend : ascend;
+        Console.WriteLine(res.Length);
+        Console.WriteLine(string.Join(" ", res));
+    }
+}
+
+
+[StructLayout(LayoutKind.Explicit)]
+class Random
+{
+    [FieldOffset(0)]
+    private byte __byte;
+    [FieldOffset(0)]
+    private sbyte __sbyte;
+    [FieldOffset(0)]
+    private char __char;
+    [FieldOffset(0)]
+    private short __short;
+    [FieldOffset(0)]
+    private ushort __ushort;
+    [FieldOffset(0)]
+    private int __int;
+    [FieldOffset(0)]
+    private uint __uint;
+    [FieldOffset(0)]
+    private long __long;
+    [FieldOffset(0)]
+    private ulong __ulong;
+
+    public byte Byte { get { Update(); return __byte; } }
+    public sbyte SByte { get { Update(); return __sbyte; } }
+    public char Char { get { Update(); return __char; } }
+    public short Short { get { Update(); return __short; } }
+    public ushort UShort { get { Update(); return __ushort; } }
+    public int Int { get { Update(); return __int; } }
+    public uint UInt { get { Update(); return __uint; } }
+    public long Long { get { Update(); return __long; } }
+    public ulong ULong { get { Update(); return __ulong; } }
+    public double Double { get { return (double)ULong / ulong.MaxValue; } }
+
+    [FieldOffset(0)]
+    private ulong _xorshift;
+
+    public Random() : this((ulong)DateTime.Now.Ticks) { }
+    public Random(ulong seed) { SetSeed(seed); }
+    public void SetSeed(ulong seed) => _xorshift = seed * 0x3141592c0ffeeul;
+
+    public int Next() => Int & 2147483647;
+    public void Update()
+    {
+        _xorshift ^= _xorshift << 7;
+        _xorshift ^= _xorshift >> 9;
+    }
+}
