dc2af230bcb31293e98b1a72f793de787a8fad71
crawler <kymn0116+crawler@gmail.com>
2019-06-11 04:58:29 +0000

    add atcoder/xmascon16/xmascon16_a/5875969.cs

---
+++ atcoder/xmascon16/xmascon16_a/5875969.cs
@@ -0,0 +1,32 @@
+// detail: https://atcoder.jp/contests/xmascon16/submissions/5875969
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long sum = 0;
+        for (int i = 1; n != 0; i <<= 1)
+        {
+            if ((n & i) == 0) continue;
+            sum += Query(n - i, n);
+            n -= i;
+        }
+        Console.WriteLine($"! {sum}");
+    }
+    static long Query(int l, int r)
+    {
+        Console.WriteLine($"? {l} {r}");
+        return int.Parse(Console.ReadLine());
+    }
+}
