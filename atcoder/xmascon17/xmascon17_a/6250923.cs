c19a8e772a753dbb6ed670b65b775e5c1291128f
crawler <kymn0116+crawler@gmail.com>
2019-07-06 06:12:30 +0000

    add atcoder/xmascon17/xmascon17_a/6250923.cs

---
+++ atcoder/xmascon17/xmascon17_a/6250923.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/xmascon17/submissions/6250923
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine("AABAAAABABABBAB");
+    }
+}
