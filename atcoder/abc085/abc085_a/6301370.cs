215cf06e6fc712889c7b348536bf5210706ee2f2
crawler <kymn0116+crawler@gmail.com>
2019-07-07 14:40:25 +0000

    add atcoder/abc085/abc085_a/6301370.cs

---
+++ atcoder/abc085/abc085_a/6301370.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc085/submissions/6301370
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+
+
+static class P
+{
+    static void Main()
+    {
+        var s = Console.ReadLine();
+        Console.WriteLine(s.Replace("2017", "2018"));
+    }
+}
