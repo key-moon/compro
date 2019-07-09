27ddcca1feff5a2669c1bcce1304d12b20ea6b6e
crawler <kymn0116+crawler@gmail.com>
2019-07-09 16:28:46 +0000

    add atcoder/tricky/tricky_1/6325993.cpp

---
+++ atcoder/tricky/tricky_1/6325993.cpp
@@ -0,0 +1,199 @@
+// detail: https://atcoder.jp/contests/tricky/submissions/6325993
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef _MSC_VER
+#define getchar_unlocked getchar
+#define putchar_unlocked putchar
+#define fseek_unlocked fseek
+#endif
+
+
+int main()
+{
+    uint32_t n = 0;
+    uint64_t a, b, x;
+    char f[19]{ };
+    int k = getchar_unlocked();
+    while ('0' <= k)
+    {
+        n = n * 10 + k - '0';
+        k = getchar_unlocked();
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        bool sign = false;
+
+        k = getchar_unlocked();
+        if (k == '-') { sign ^= true; k = getchar_unlocked(); }
+
+        //1
+        a = k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //2
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //3
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //4
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //5
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //6
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //7
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //8
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //9
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //10
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //11
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //12
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //13
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //14
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //15
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //16
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //17
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //18
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == ' ') goto readAEnd;
+        //19
+        a = a * 10 + k - 48;
+        k = getchar_unlocked();
+    readAEnd:;
+
+        k = getchar_unlocked();
+        if (k == '-') { sign ^= true; k = getchar_unlocked(); }
+        //1
+        b = k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //2
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //3
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //4
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //5
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //6
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //7
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //8
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //9
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //10
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //11
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //12
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //13
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //14
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //15
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //16
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //17
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //18
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+        if (k == '\n') goto readBEnd;
+        //19
+        b = b * 10 + k - 48;
+        k = getchar_unlocked();
+    readBEnd:;
+
+        x = a / b;
+        if ((x != 0) && sign) putchar_unlocked('-');
+        int s = 0;
+        do
+        {
+            f[s++] = x % 10;
+            x /= 10;
+        } while (x);
+        while (s--)
+            putchar_unlocked(f[s] + '0');
+        putchar_unlocked('\n');
+    }
+    return 0;
+}
