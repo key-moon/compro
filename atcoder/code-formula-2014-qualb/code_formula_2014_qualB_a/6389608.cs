d82e3beee15fa65419a0fb347820e01447cff68a
crawler <kymn0116+crawler@gmail.com>
2019-07-14 18:49:31 +0000

    add atcoder/code-formula-2014-qualb/code_formula_2014_qualB_a/6389608.cs

---
+++ atcoder/code-formula-2014-qualb/code_formula_2014_qualB_a/6389608.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/code-formula-2014-qualb/submissions/6389608
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(7 - int.Parse(Console.ReadLine()));
+    }
+}
+
