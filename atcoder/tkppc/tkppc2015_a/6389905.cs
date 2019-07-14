a2c98011819c00b69ffdb83a5aac9f12d35ba62c
crawler <kymn0116+crawler@gmail.com>
2019-07-14 19:53:52 +0000

    add atcoder/tkppc/tkppc2015_a/6389905.cs

---
+++ atcoder/tkppc/tkppc2015_a/6389905.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/tkppc/submissions/6389905
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+            Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Sum());
+    }
+}
