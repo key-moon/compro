e2fcb4214e0ab393172cd411450d64e69bbf41aa
crawler <kymn0116+crawler@gmail.com>
2019-07-14 19:23:37 +0000

    add atcoder/k2pc-hard/k2pc001_h1/6389756.cs

---
+++ atcoder/k2pc-hard/k2pc001_h1/6389756.cs
@@ -0,0 +1,42 @@
+// detail: https://atcoder.jp/contests/k2pc-hard/submissions/6389756
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        var n = GetElem(nm[0]);
+        var m = GetElem(nm[1]);
+
+        Console.WriteLine(GetInd(n.Item1 + m.Item1, n.Item2 + m.Item2));
+    }
+    static Tuple<int,int> GetElem(int index)
+    {
+        long sqrt = (int)Floor(Sqrt((index - 1) * 2));
+        while (index > sqrt * (sqrt + 1) / 2) sqrt++;
+        var curRowLastInd = sqrt * (sqrt + 1) / 2;
+        var first = curRowLastInd - index + 1;
+        var second =  (sqrt + 1) - first;
+        return new Tuple<int, int>((int)first, (int)second);
+    }
+    static long GetInd(long a, long b)
+    {
+        long sqrt = (a + b - 1);
+        var curRowLastInd = sqrt * (sqrt + 1) / 2;
+        var Ind = curRowLastInd - a + 1;
+        return Ind;
+    }
+}
