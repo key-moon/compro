af55d9ad7e44cb181081844b5529405284af7120
crawler <kymn0116+crawler@gmail.com>
2019-07-14 20:46:35 +0000

    add atcoder/code-festival-2015-relay/cf_2015_relay_a/6390099.cs

---
+++ atcoder/code-festival-2015-relay/cf_2015_relay_a/6390099.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/code-festival-2015-relay/submissions/6390099
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(1005);
+    }
+}
