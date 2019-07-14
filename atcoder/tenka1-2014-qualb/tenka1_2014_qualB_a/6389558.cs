ce9a6ce2b1df5a37a9c52e863bc1b3779cb4b268
crawler <kymn0116+crawler@gmail.com>
2019-07-14 18:39:09 +0000

    add atcoder/tenka1-2014-qualb/tenka1_2014_qualB_a/6389558.cs

---
+++ atcoder/tenka1-2014-qualb/tenka1_2014_qualB_a/6389558.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/tenka1-2014-qualb/submissions/6389558
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+
+static class P
+{
+    static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Replace("HAGIYA", "HAGIXILE"));
+    }
+}
