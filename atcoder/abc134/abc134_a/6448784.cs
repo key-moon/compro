225d8c6cb4a29290d362c4f43d2ba1739776ac65
crawler <kymn0116+crawler@gmail.com>
2019-07-20 12:02:34 +0000

    add atcoder/abc134/abc134_a/6448784.cs

---
+++ atcoder/abc134/abc134_a/6448784.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/abc134/submissions/6448784
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+
+static class P
+{
+    static void Main()
+    {
+        var r = int.Parse(Console.ReadLine());
+        Console.WriteLine(3 * r * r);
+    }
+}
