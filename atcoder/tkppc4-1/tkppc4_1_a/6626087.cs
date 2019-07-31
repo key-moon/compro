4a2add7b664e37f654f1781fe6f5fb8b9289785c
crawler <kymn0116+crawler@gmail.com>
2019-07-31 12:20:58 +0000

    add atcoder/tkppc4-1/tkppc4_1_a/6626087.cs

---
+++ atcoder/tkppc4-1/tkppc4_1_a/6626087.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/tkppc4-1/submissions/6626087
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        var nw = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(nw[0] - 1);
+    }
+}
