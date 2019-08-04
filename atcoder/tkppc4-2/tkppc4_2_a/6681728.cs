c25b34af01b9d0a40df426dc37b93c0edf1999ef
crawler <kymn0116+crawler@gmail.com>
2019-08-04 11:48:28 +0000

    add atcoder/tkppc4-2/tkppc4_2_a/6681728.cs

---
+++ atcoder/tkppc4-2/tkppc4_2_a/6681728.cs
@@ -0,0 +1,46 @@
+// detail: https://atcoder.jp/contests/tkppc4-2/submissions/6681728
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using Debug = System.Diagnostics.Debug;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        int x = NextInt, y = NextInt;
+        if (y % 2 == 0 && Abs(x) <= y / 2 && Abs(x) % 2 == (y / 2) % 2 )
+            Console.WriteLine(y / 2);
+        else
+            Console.WriteLine(-1);
+    }
+
+
+    static readonly TextReader In = Console.In;
+    static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0;
+            int next = In.Read();
+            int rev = 1;
+            while (45 > next || next > 57) next = In.Read();
+            if (next == 45) { next = In.Read(); rev = -1; }
+            while (48 <= next && next <= 57)
+            {
+                res = res * 10 + next - 48;
+                next = In.Read();
+            }
+            return res * rev;
+        }
+    }
+}
