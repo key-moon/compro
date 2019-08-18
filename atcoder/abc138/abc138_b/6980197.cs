0c775a81d7dbe53ce56d52b40482ede585d14566
crawler <kymn0116+crawler@gmail.com>
2019-08-18 12:00:43 +0000

    add atcoder/abc138/abc138_b/6980197.cs

---
+++ atcoder/abc138/abc138_b/6980197.cs
@@ -0,0 +1,44 @@
+// detail: https://atcoder.jp/contests/abc138/submissions/6980197
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Debug = System.Diagnostics.Debug;
+using static System.Math;
+using System.Runtime.CompilerServices;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(1.0 / a.Sum(x => 1 / (double)x));
+    }
+
+    static readonly TextReader In = Console.In;
+    static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0;
+            int next = In.Read();
+            int rev = 1;
+            while (45 > next || next > 57) next = In.Read();
+            if (next == 45) { next = In.Read(); rev = -1; }
+            while (48 <= next && next <= 57)
+            {
+                res = res * 10 + next - 48;
+                next = In.Read();
+            }
+            return res * rev;
+        }
+    }
+}
