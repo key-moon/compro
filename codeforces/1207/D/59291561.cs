a082a19564e9d2a36870c92c61a7a49c6789bf5a
crawler <kymn0116+crawler@gmail.com>
2019-08-22 15:13:59 +0000

    add codeforces/1207/D/59291561.cs

---
+++ codeforces/1207/D/59291561.cs
@@ -0,0 +1,163 @@
+// detail: https://codeforces.com/contest/1207/submission/59291561
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Debug = System.Diagnostics.Debug;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+using StructLayoutAttribute = System.Runtime.InteropServices.StructLayoutAttribute;
+using FieldOffsetAttribute = System.Runtime.InteropServices.FieldOffsetAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Enumerable.Repeat(0, n).Select(_ =>
+        {
+            var s = Console.ReadLine().Split();
+            return new IntTuple(int.Parse(s[0]), int.Parse(s[1]));
+        }).ToArray();
+
+        var total = Factorial(n);
+        var abad = a.GroupBy(x => x.Item1).Select(x => x.Count()).Select(x => Factorial(x)).Aggregate((ModInt)1, (x, y) => x * y);
+        var bbad = a.GroupBy(x => x.Item2).Select(x => x.Count()).Select(x => Factorial(x)).Aggregate((ModInt)1, (x, y) => x * y);
+
+        var sorted = a.OrderBy(x => x.Item1).ThenBy(x => x.Item2).ToArray();
+        bool IsSorted = true;
+        for (int i = 1; i < sorted.Length; i++)
+            if (sorted[i - 1].Item1 > sorted[i].Item1 || sorted[i - 1].Item2 > sorted[i].Item2)
+                IsSorted = false;
+
+        var abbad = IsSorted ? a.GroupBy(x => x).Select(x => x.Count()).Select(x => Factorial(x)).Aggregate((ModInt)1, (x, y) => x * y) : 0;
+
+        Console.WriteLine(total - abad - bbad + abbad);
+    }
+
+    static List<ModInt> factorialMemo = new List<ModInt>() { 1 };
+    static ModInt Factorial(int x)
+    {
+        for (int i = factorialMemo.Count; i <= x; i++) factorialMemo.Add(factorialMemo.Last() * i);
+        return factorialMemo[x];
+    }
+}
+
+struct IntTuple : IEquatable<IntTuple>
+{
+    static int seed = new Random().Next();
+    public int Item1;
+    public int Item2;
+    public IntTuple(int item1, int item2)
+    {
+        Item1 = item1;
+        Item2 = item2;
+    }
+
+    public bool Equals(IntTuple other) => other.Item1 == Item1 && other.Item2 == Item2;
+    public override int GetHashCode()
+    {
+        return Item1 * seed + Item2;
+    }
+}
+
+
+struct ModInt
+{
+    const int MOD = 998244353;
+    const long POSITIVIZER = ((long)MOD) << 31;
+    long Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ModInt(long data) { if ((Data = data % MOD) < 0) Data += MOD; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= MOD ? res - MOD : res }; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + MOD : res }; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => Data.ToString();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static long GetInverse(long a)
+    {
+        long div, p = MOD, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + MOD; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + MOD; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
+
+
+[StructLayout(LayoutKind.Explicit)]
+class Random
+{
+    [FieldOffset(0)]
+    private byte __byte;
+    [FieldOffset(0)]
+    private sbyte __sbyte;
+    [FieldOffset(0)]
+    private char __char;
+    [FieldOffset(0)]
+    private short __short;
+    [FieldOffset(0)]
+    private ushort __ushort;
+    [FieldOffset(0)]
+    private int __int;
+    [FieldOffset(0)]
+    private uint __uint;
+    [FieldOffset(0)]
+    private long __long;
+    [FieldOffset(0)]
+    private ulong __ulong;
+
+    public byte Byte { get { Update(); return __byte; } }
+    public sbyte SByte { get { Update(); return __sbyte; } }
+    public char Char { get { Update(); return __char; } }
+    public short Short { get { Update(); return __short; } }
+    public ushort UShort { get { Update(); return __ushort; } }
+    public int Int { get { Update(); return __int; } }
+    public uint UInt { get { Update(); return __uint; } }
+    public long Long { get { Update(); return __long; } }
+    public ulong ULong { get { Update(); return __ulong; } }
+    public double Double { get { return (double)ULong / ulong.MaxValue; } }
+
+    [FieldOffset(0)]
+    private ulong _xorshift;
+
+    public Random() : this((ulong)DateTime.Now.Ticks) { }
+    public Random(ulong seed) { SetSeed(seed); }
+    public void SetSeed(ulong seed) => _xorshift = seed * 0x3141592c0ffeeul;
+
+    public int Next() => Int & 2147483647;
+    public void Update()
+    {
+        _xorshift ^= _xorshift << 7;
+        _xorshift ^= _xorshift >> 9;
+    }
+}
