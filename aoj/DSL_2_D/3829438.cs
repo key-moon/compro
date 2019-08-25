51244f5bce58b1fee48d3eaf7218740efe1d6d7d
crawler <kymn0116+crawler@gmail.com>
2019-08-25 18:06:44 +0000

    add aoj/DSL_2_D/3829438.cs

---
+++ aoj/DSL_2_D/3829438.cs
@@ -0,0 +1,125 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/DSL_2_D/judge/3829438/C#
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Debug = System.Diagnostics.Debug;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+static class P
+{
+    static void Main()
+    {
+        SegmentTree<int> segTree = new SegmentTree<int>(NextInt, -1, (x, y) => y == -1 ? x : y);
+        segTree.Operate(0, segTree.Size - 1, int.MaxValue);
+        int q = NextInt;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < q; i++)
+        {
+            if (NextInt == 0)
+                segTree.Operate(NextInt, NextInt, NextInt);
+            else
+                builder.AppendLine(segTree[NextInt].ToString());
+        }
+        Console.Write(builder.ToString());
+    }
+
+
+    static readonly TextReader In = Console.In;
+    static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0;
+            int next = In.Read();
+            while (45 > next || next > 57) next = In.Read();
+            while (48 <= next && next <= 57)
+            {
+                res = res * 10 + next - 48;
+                next = In.Read();
+            }
+            return res;
+        }
+    }
+}
+
+
+class SegmentTree<T>
+{
+    public readonly int Size;
+    T Identity;
+    Func<T, T, T> Merge;
+    int LeafCount;
+    int Height;
+    T[] Operators;
+
+    public SegmentTree(int size, T identity, Func<T, T, T> merge)
+    {
+        Size = size;
+        Identity = identity;
+        Merge = merge;
+        Height = 1;
+        LeafCount = 1;
+        while (LeafCount < size) { Height++; LeafCount <<= 1; }
+        Operators = new T[LeafCount << 1];
+        for (int i = 0; i < Operators.Length; i++) Operators[i] = identity;
+    }
+
+    public T this[int index]
+    {
+        get { return Query(index); }
+        set { Propagate(index += LeafCount); Operators[index] = value; }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Operate(int l, int r, T x)
+    {
+        l += LeafCount;
+        r += LeafCount;
+        Propagate(l);
+        Propagate(r);
+        while (l <= r)
+        {
+            if ((l & 1) == 1) Operators[l] = Merge(Operators[l++], x);
+            if ((r & 1) == 0) Operators[r] = Merge(Operators[r--], x);
+            l >>= 1;
+            r >>= 1;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Query(int index)
+    {
+        index += LeafCount;
+        Propagate(index);
+        return Operators[index];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Eval(int ind)
+    {
+        Operators[(ind << 1)] = Merge(Operators[(ind << 1)], Operators[ind]);
+        Operators[(ind << 1) | 1] = Merge(Operators[(ind << 1) | 1], Operators[ind]);
+        Operators[ind] = Identity;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Propagate(int ind) { for (int i = Height - 1; i >= 1; i--) { Eval(ind >> i); } return; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Propagate(int l, int r)
+    {
+        if (l == r) { Propagate(l); return; }
+        int xor = l ^ r, i = Height - 1;
+        for (; (xor >> i) == 0; i--) { Eval(l >> i); }
+        for (; i >= 1; i--) { Eval(l >> i); Eval(r >> i); }
+    }
+}
+
