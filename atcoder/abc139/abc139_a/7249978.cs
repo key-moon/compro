8abbae0431f2fbc2a149752f573a3c041181abce
crawler <kymn0116+crawler@gmail.com>
2019-09-01 12:04:25 +0000

    add atcoder/abc139/abc139_a/7249978.cs

---
+++ atcoder/abc139/abc139_a/7249978.cs
@@ -0,0 +1,45 @@
+// detail: https://atcoder.jp/contests/abc139/submissions/7249978
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        var s = Console.ReadLine();
+        var t = Console.ReadLine();
+        Console.WriteLine(s.Zip(t, (x, y) => x == y).Count(x => x));
+    }
+
+    static readonly TextReader In = Console.In;
+    static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0;
+            int next = In.Read();
+            int rev = 1;
+            while (45 > next || next > 57) next = In.Read();
+            if (next == 45) { next = In.Read(); rev = -1; }
+            while (48 <= next && next <= 57)
+            {
+                res = res * 10 + next - 48;
+                next = In.Read();
+            }
+            return res * rev;
+        }
+    }
+}
