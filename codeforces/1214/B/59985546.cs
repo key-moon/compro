8d42fb739dbd5481e68ed519e114f1e47193b9c4
crawler <kymn0116+crawler@gmail.com>
2019-09-04 09:17:38 +0000

    add codeforces/1214/B/59985546.cs

---
+++ codeforces/1214/B/59985546.cs
@@ -0,0 +1,49 @@
+// detail: https://codeforces.com/contest/1214/submission/59985546
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        int b = int.Parse(Console.ReadLine());
+        var g = int.Parse(Console.ReadLine());
+        var n = int.Parse(Console.ReadLine());
+        var gMin = Max(n - b, 0);
+        var gMax = Min(n, g);
+        Console.WriteLine(gMax - gMin + 1);
+    }
+
+
+    static readonly TextReader In = Console.In;
+    static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0;
+            int next = In.Read();
+            int rev = 1;
+            while (45 > next || next > 57) next = In.Read();
+            if (next == 45) { next = In.Read(); rev = -1; }
+            while (48 <= next && next <= 57)
+            {
+                res = res * 10 + next - 48;
+                next = In.Read();
+            }
+            return res * rev;
+        }
+    }
+}
