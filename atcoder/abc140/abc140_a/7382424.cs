091075d2cd59a1a2167a9ec4f073924ae3583da2
crawler <kymn0116+crawler@gmail.com>
2019-09-07 12:02:38 +0000

    add atcoder/abc140/abc140_a/7382424.cs

---
+++ atcoder/abc140/abc140_a/7382424.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/abc140/submissions/7382424
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine((int)Pow(n, 3));
+    }
+}
