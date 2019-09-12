f781e42ee9cbf910fc97dc321ba8c8e267db31c4
crawler <kymn0116+crawler@gmail.com>
2019-09-12 12:28:43 +0000

    add aoj/0335/3863058.cpp

---
+++ aoj/0335/3863058.cpp
@@ -0,0 +1,10 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/0335/judge/3863058/C++14
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n * 32 << endl;
+}
+
