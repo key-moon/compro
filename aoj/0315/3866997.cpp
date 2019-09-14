41e5c83cc9ccbb8d88647c4a24942fe2c033db57
crawler <kymn0116+crawler@gmail.com>
2019-09-14 04:10:18 +0000

    add aoj/0315/3866997.cpp

---
+++ aoj/0315/3866997.cpp
@@ -0,0 +1,19 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/0315/judge/3866997/C++14
+#include <bits/stdc++.h>
+
+using namespace std;
+#define REP(i,m) for(int i=0; i<m; i++)
+#define FOR(i,n,m) for(int i=n; i<m; i++)
+#define INF 1000000007
+#define INFL (1LL << 60)
+#define MOD 998244353
+#define ALL(v) v.begin(),v.end()
+#define pb push_back
+#define ll long long int
+#define P pair<ll, ll>
+
+int main() {
+	int p, m, c;
+	cin >> p >> m >> c;
+	cout << p + m + c << endl;
+}
