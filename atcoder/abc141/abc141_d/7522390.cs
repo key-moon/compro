e2f051645392164a2d5c0cc7dc975dc8671c4e36
crawler <kymn0116+crawler@gmail.com>
2019-09-15 12:10:31 +0000

    add atcoder/abc141/abc141_d/7522390.cs

---
+++ atcoder/abc141/abc141_d/7522390.cs
@@ -0,0 +1,96 @@
+// detail: https://atcoder.jp/contests/abc141/submissions/7522390
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        PriorityQueue<int> pqueue = new PriorityQueue<int>(true);
+        foreach (var item in Console.ReadLine().Split().Select(int.Parse))
+        {
+            pqueue.Push(item);
+        }
+        for (int i = 0; i < nm[1]; i++)
+        {
+            pqueue.Push(pqueue.Pop() >> 1);
+        }
+        long sum = 0;
+        while (pqueue.Count > 0)
+        {
+            sum += pqueue.Pop();
+        }
+        Console.WriteLine(sum);
+    }
+}
+
+
+class PriorityQueue<T> where T : IComparable<T>
+{
+    public int Count { get; private set; }
+    private bool Descendance;
+    private T[] data = new T[65536];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public PriorityQueue(bool descendance = false) { Descendance = descendance; }
+    public T Top
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get { ValidateNonEmpty(); return data[1]; }
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Pop()
+    {
+        var top = Top;
+        var elem = data[Count--];
+        int index = 1;
+        while (true)
+        {
+            if ((index << 1) >= Count)
+            {
+                if (index << 1 > Count) break;
+                if (elem.CompareTo(data[index << 1]) > 0 ^ Descendance) data[index] = data[index <<= 1];
+                else break;
+            }
+            else
+            {
+                var nextIndex = data[index << 1].CompareTo(data[(index << 1) + 1]) <= 0 ^ Descendance ? (index << 1) : (index << 1) + 1;
+                if (elem.CompareTo(data[nextIndex]) > 0 ^ Descendance) data[index] = data[index = nextIndex];
+                else break;
+            }
+        }
+        data[index] = elem;
+        return top;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Push(T value)
+    {
+        int index = ++Count;
+        if (data.Length == Count) Extend(data.Length * 2);
+        while ((index >> 1) != 0)
+        {
+            if (data[index >> 1].CompareTo(value) > 0 ^ Descendance) data[index] = data[index >>= 1];
+            else break;
+        }
+        data[index] = value;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Extend(int newSize)
+    {
+        T[] newDatas = new T[newSize];
+        data.CopyTo(newDatas, 0);
+        data = newDatas;
+    }
+    private void ValidateNonEmpty() { if (Count == 0) throw new Exception(); }
+}
