0f02e7813e42057cbcc2c9c84268257769e0520d
crawler <kymn0116+crawler@gmail.com>
2019-09-30 17:48:53 +0000

    add atcoder/abc139/abc139_c/7804772.cs

---
+++ atcoder/abc139/abc139_c/7804772.cs
@@ -0,0 +1,89 @@
+// detail: https://atcoder.jp/contests/abc139/submissions/7804772
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = Reader.NextInt;
+        int last = 0;
+        int cur = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++)
+        {
+            var h = Reader.NextInt;
+            if (last >= h) max = Max(max, ++cur);
+            else cur = 0;
+            last = h;
+        }
+        Console.WriteLine(max);
+    }
+}
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = 0;
+
+    private static void Read() { Stream.Read(Buffer, 0, BUF_SIZE); }
+
+    private static byte Next { get { if (ptr >= Buffer.Length) { Read(); ptr = 0; } return Buffer[ptr++]; } }
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; byte next; while ((next = Next) < 48) ;
+            do { res = res * 10 + (next ^ 48); } while (48 <= (next = Next));
+            return res;
+        }
+    }
+
+    public static int NextSInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; byte next; int sign = 1; do { next = Next; } while (next < 45); if (next == 45) { next = Next; sign = -1; }
+            do { res = res * 10 + (next ^ 48); next = Next; } while (48 <= next);
+            return res * sign;
+        }
+    }
+
+    public static long NextLong
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            long res = 0; byte next; do { next = Next; } while (next < 48);
+            do { res = res * 10 + (next ^ 48); next = Next; } while (48 <= next);
+            return res;
+        }
+    }
+
+    public static long NextSLong
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            long res = 0; byte next; int sign = 1; do { next = Next; } while (next < 45); if (next == 45) { next = Next; sign = -1; }
+            do { res = res * 10 + (next ^ 48); next = Next; } while (48 <= next);
+            return res * sign;
+        }
+    }
+}
