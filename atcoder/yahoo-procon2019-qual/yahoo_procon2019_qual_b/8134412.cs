72015e99aa40857d63bb70729c3b75c7dea49462
crawler <kymn0116+crawler@gmail.com>
2019-10-26 21:01:20 +0000

    add atcoder/yahoo-procon2019-qual/yahoo_procon2019_qual_b/8134412.cs

---
+++ atcoder/yahoo-procon2019-qual/yahoo_procon2019_qual_b/8134412.cs
@@ -0,0 +1,43 @@
+// detail: https://atcoder.jp/contests/yahoo-procon2019-qual/submissions/8134412
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Enumerable.Repeat(0, 6).Select(_ => Reader.NextInt).GroupBy(x => x).Max(x => x.Count()) == 3 ? "NO" : "YES");
+    }
+}
+
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = 0;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
