c2d687dacbe26e46e90ccf7038fe5fcb5f283c41
crawler <kymn0116+crawler@gmail.com>
2019-10-26 20:39:56 +0000

    add atcoder/agc027/agc027_a/8134376.cs

---
+++ atcoder/agc027/agc027_a/8134376.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/agc027/submissions/8134376
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        var X = Console.ReadLine().Split().Select(int.Parse).Last();
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).TakeWhile(x => 0 <= (X -= x)).Count() - (0 < X ? 1 : 0));
+    }
+}
