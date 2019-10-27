4130a17df0c839cccf23e16949e725ba7e39045a
crawler <kymn0116+crawler@gmail.com>
2019-10-27 20:24:30 +0000

    add atcoder/abc038/abc038_d/8183814.cs

---
+++ atcoder/abc038/abc038_d/8183814.cs
@@ -0,0 +1,87 @@
+// detail: https://atcoder.jp/contests/abc038/submissions/8183814
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Debug = System.Diagnostics.Debug;
+using static System.Math;
+using static Reader;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        var n = NextInt;
+        BIT<int> bit = new BIT<int>(100001, 0, Max);
+        foreach (var boxes in Enumerable.Repeat(0, n).Select(_ => new Box() { X = NextInt, Y = NextInt }).GroupBy(x => x.X).OrderBy(x => x.Key))
+        {
+            foreach (var item in boxes.Select(x => new { Pos = x.Y, Res = bit.Query(x.Y) }).ToArray())
+            {
+                bit.Operate(item.Pos, item.Res + 1);
+            }
+        }
+        var res = bit.Query(bit.Size);
+        Console.WriteLine(res);
+    }
+}
+
+struct Box
+{
+    public int X;
+    public int Y;
+}
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = 0;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
+
+class BIT<T>
+{
+    public readonly int Size;
+    T Identity;
+    T[] Elements;
+    Func<T, T, T> Merge;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public BIT(int size, T identity, Func<T, T, T> merge)
+    {
+        Size = size;
+        Identity = identity;
+        Merge = merge;
+        Elements = new T[Size + 1];
+        for (int i = 0; i < Elements.Length; i++) Elements[i] = identity;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Query(int r)
+    {
+        T res = Identity;
+        for (; r > 0; r -= r & -r) res = Merge(res, Elements[r]);
+        return res;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Operate(int i, T x)
+    {
+        for (i++; i < Elements.Length; i += i & -i) Elements[i] = Merge(Elements[i], x);
+    }
+}
