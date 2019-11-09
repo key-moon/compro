02b64476df80ee282a4ac212376afd8a56cc8a3e
crawler <kymn0116+crawler@gmail.com>
2019-11-09 09:11:05 +0000

    add atcoder/arc056/arc056_b/8345317.cs

---
+++ atcoder/arc056/arc056_b/8345317.cs
@@ -0,0 +1,98 @@
+// detail: https://atcoder.jp/contests/arc056/submissions/8345317
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using static Reader;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = NextInt, m = NextInt, s = NextInt - 1;
+        List<int>[] neighbours = Enumerable.Repeat(0, n).Select(_ => new List<int>()).ToArray();
+        for (int i = 0; i < m; i++)
+        {
+            int u = NextInt - 1, v = NextInt - 1;
+            neighbours[u].Add(v);
+            neighbours[v].Add(u);
+        }
+        UnionFind uf = new UnionFind(n);
+        List<int> res = new List<int>();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            foreach (var neighbour in neighbours[i])
+                if (i < neighbour)
+                    uf.TryUnite(i, neighbour);
+            if (uf.Find(i) == uf.Find(s)) res.Add(i + 1);
+        }
+        Console.WriteLine(string.Join("\n", res.Reverse<int>()));
+    }
+}
+
+
+class UnionFind
+{
+    public int Size { get; private set; }
+    public int GroupCount { get; private set; }
+    public IEnumerable<int> AllRepresents => Parent.Where((x, y) => x == y);
+    int[] Sizes;
+    int[] Parent;
+    public UnionFind(int count)
+    {
+        Size = count;
+        GroupCount = count;
+        Parent = new int[count];
+        Sizes = new int[count];
+        for (int i = 0; i < count; i++) Sizes[Parent[i] = i] = 1;
+    }
+    public bool TryUnite(int x, int y)
+    {
+        int xp = Find(x);
+        int yp = Find(y);
+        if (yp == xp) return false;
+        if (Sizes[xp] < Sizes[yp]) { var tmp = xp; xp = yp; yp = tmp; }
+        GroupCount--;
+        Parent[yp] = xp;
+        Sizes[xp] += Sizes[yp];
+        return true;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int GetSize(int x) => Sizes[Find(x)];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Find(int x)
+    {
+        while (x != Parent[x]) x = (Parent[x] = Parent[Parent[x]]);
+        return x;
+    }
+}
+
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = 0;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (int)(Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
