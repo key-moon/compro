157d2e6c687c6c90dcaf059421c3a50d49a58bf4
crawler <kymn0116+crawler@gmail.com>
2019-11-16 12:24:08 +0000

    add atcoder/abc145/abc145_d/8474822.cs

---
+++ atcoder/abc145/abc145_d/8474822.cs
@@ -0,0 +1,90 @@
+// detail: https://atcoder.jp/contests/abc145/submissions/8474822
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+public static class P
+{
+    public static void Main()
+    {
+        var xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var x = xy[0];
+        var y = xy[1];
+        if ((x + y) % 3 != 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+        var moves = (x + y) / 3;
+        if (x < moves || y < moves)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+        var xCount = x - moves;
+        var yCount = y - moves;
+        Console.WriteLine(Factorial(moves) / (Factorial(xCount) * Factorial(yCount)));
+    }
+
+    static List<ModInt> factorialMemo = new List<ModInt>() { 1 };
+    static ModInt Factorial(int x)
+    {
+        for (int i = factorialMemo.Count; i <= x; i++) factorialMemo.Add(factorialMemo.Last() * i);
+        return factorialMemo[x];
+    }
+}
+
+
+struct ModInt
+{
+    const int MOD = 1000000007;
+    const long POSITIVIZER = ((long)MOD) << 31;
+    long Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ModInt(long data) { if ((Data = data % MOD) < 0) Data += MOD; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= MOD ? res - MOD : res }; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + MOD : res }; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => Data.ToString();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static long GetInverse(long a)
+    {
+        long div, p = MOD, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + MOD; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + MOD; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
