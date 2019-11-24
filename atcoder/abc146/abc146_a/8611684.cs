8d4230034457e3677549a33e4a07a57e9063ba8a
crawler <kymn0116+crawler@gmail.com>
2019-11-24 12:13:42 +0000

    add atcoder/abc146/abc146_a/8611684.cs

---
+++ atcoder/abc146/abc146_a/8611684.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc146/submissions/8611684
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using LayoutKind = System.Runtime.InteropServices.LayoutKind;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(7 - Array.IndexOf(new string[] { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" }, Console.ReadLine()));
+    }
+}
