d8a963212335658f18ce0bf392496b16e3ae609b
crawler <kymn0116+crawler@gmail.com>
2019-12-04 07:00:30 +0000

    add atcoder/abc140/abc140_a/8784269.fs

---
+++ atcoder/abc140/abc140_a/8784269.fs
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc140/submissions/8784269
+open System
+
+[<EntryPoint>]
+let main argv =
+    let n = Console.ReadLine() |> int
+    n * n * n |> Console.WriteLine
+    0
