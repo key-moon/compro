9f3fb86fbe9dc0b2db025b3c03a5303b3d9edd47
crawler <kymn0116+crawler@gmail.com>
2019-12-08 12:08:16 +0000

    add atcoder/abc147/abc147_a/8852281.cs

---
+++ atcoder/abc147/abc147_a/8852281.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc147/submissions/8852281
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Sum() <= 21 ? "win" : "bust");
+    }
+}
