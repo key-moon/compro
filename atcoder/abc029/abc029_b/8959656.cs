0edeb90a1f0a4ec25b0e8661f627dd6bb1b3ec1b
crawler <kymn0116+crawler@gmail.com>
2019-12-14 23:10:58 +0000

    add atcoder/abc029/abc029_b/8959656.cs

---
+++ atcoder/abc029/abc029_b/8959656.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc029/submissions/8959656
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Enumerable.Repeat(0, 12).Select(_ => Console.ReadLine()).Count(x => x.Contains("r")));
+    }
+}
