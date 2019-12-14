693bedaedf18dbd4ea54bdc7c604ed89d6617e97
crawler <kymn0116+crawler@gmail.com>
2019-12-14 23:47:55 +0000

    add atcoder/abc066/abc066_a/8959752.cs

---
+++ atcoder/abc066/abc066_a/8959752.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc066/submissions/8959752
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).Take(2).Sum());
+    }
+}
