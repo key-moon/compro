43aa9786425a0281a8aabe9b73bd4bf6128b5f6b
crawler <kymn0116+crawler@gmail.com>
2019-12-17 09:06:30 +0000

    add atcoder/abc001/abc001_1/8991497.fs

---
+++ atcoder/abc001/abc001_1/8991497.fs
@@ -0,0 +1,7 @@
+// detail: https://atcoder.jp/contests/abc001/submissions/8991497
+[<EntryPoint>]
+let main argv =
+    let rint () = stdin.ReadLine() |> int
+    (rint ()) - (rint ())
+    |> stdout.WriteLine 
+    0
