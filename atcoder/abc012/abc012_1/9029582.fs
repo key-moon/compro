20ab434ba403290db569efdf022e1b2aec326004
crawler <kymn0116+crawler@gmail.com>
2019-12-20 12:12:51 +0000

    add atcoder/abc012/abc012_1/9029582.fs

---
+++ atcoder/abc012/abc012_1/9029582.fs
@@ -0,0 +1,5 @@
+// detail: https://atcoder.jp/contests/abc012/submissions/9029582
+[<EntryPoint>]
+let main argv =
+    stdin.ReadLine().Split() |> Array.rev |> String.concat " " |> stdout.WriteLine
+    0
