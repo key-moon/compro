304a27ea1c47e098ce4ace85ff50a6ecac5abe92
crawler <kymn0116+crawler@gmail.com>
2019-12-20 16:50:05 +0000

    add atcoder/abc020/abc020_a/9032774.fs

---
+++ atcoder/abc020/abc020_a/9032774.fs
@@ -0,0 +1,6 @@
+// detail: https://atcoder.jp/contests/abc020/submissions/9032774
+[<EntryPoint>]
+let main argv =
+    if stdin.ReadLine() = "1" then "ABC" else "chokudai"
+    |> stdout.WriteLine
+    0
