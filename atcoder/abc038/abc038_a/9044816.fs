d7920b22c398c1e2db0b61093a1c88c35f811510
crawler <kymn0116+crawler@gmail.com>
2019-12-21 16:37:51 +0000

    add atcoder/abc038/abc038_a/9044816.fs

---
+++ atcoder/abc038/abc038_a/9044816.fs
@@ -0,0 +1,6 @@
+// detail: https://atcoder.jp/contests/abc038/submissions/9044816
+[<EntryPoint>]
+let main argv =
+    if (stdin.ReadLine() |> Seq.last) = 'T' then "YES" else "NO"
+    |> stdout.WriteLine
+    0
