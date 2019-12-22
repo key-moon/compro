3d5d33d421cf79f2a7be6f636c1e44a78171384f
crawler <kymn0116+crawler@gmail.com>
2019-12-22 02:08:20 +0000

    add atcoder/abc044/abc044_a/9046894.fs

---
+++ atcoder/abc044/abc044_a/9046894.fs
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc044/submissions/9046894
+[<EntryPoint>]
+let main argv =
+    let rint () = stdin.ReadLine() |> int
+    let n = rint ()
+    let k = rint ()
+    let a = rint ()
+    let b = rint ()
+    n * a - (a - b) * (max (n - k) 0)
+    |> stdout.WriteLine
+    0
