315b3f08d18d001893ce4721bce8a9b56914b621
crawler <kymn0116+crawler@gmail.com>
2019-12-22 12:01:24 +0000

    add atcoder/abc148/abc148_a/9055367.cs

---
+++ atcoder/abc148/abc148_a/9055367.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc148/submissions/9055367
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(int.Parse(Console.ReadLine()) ^ int.Parse(Console.ReadLine()) ^ 1 ^ 2 ^ 3);
+    }
+}
