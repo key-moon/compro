f9a281f7c4feb0d66589cd7ee22e98d571abe406
crawler <kymn0116+crawler@gmail.com>
2019-12-22 09:17:06 +0000

    add atcoder/abc053/abc053_a/9052135.fs

---
+++ atcoder/abc053/abc053_a/9052135.fs
@@ -0,0 +1,6 @@
+// detail: https://atcoder.jp/contests/abc053/submissions/9052135
+[<EntryPoint>]
+let main argv =
+    if (stdin.ReadLine() |> int) < 1200 then "ABC" else "ARC"
+    |> stdout.WriteLine
+    0
