119f0ed7eeb91e83a76b4fb8af499580b3edff99
crawler <kymn0116+crawler@gmail.com>
2019-12-26 06:19:19 +0000

    add atcoder/abc070/abc070_a/9139683.fs

---
+++ atcoder/abc070/abc070_a/9139683.fs
@@ -0,0 +1,7 @@
+// detail: https://atcoder.jp/contests/abc070/submissions/9139683
+[<EntryPoint>]
+let main argv =
+    let n = stdin.ReadLine()
+    if n.[0] = n.[2] then "Yes" else "No"
+    |> stdout.WriteLine
+    0
