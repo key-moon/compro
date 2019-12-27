e38a2d7a2d94dbc101a8d91612907a9257acb57a
crawler <kymn0116+crawler@gmail.com>
2019-12-27 10:47:19 +0000

    add atcoder/arc008/arc008_3/9159091.cs

---
+++ atcoder/arc008/arc008_3/9159091.cs
@@ -0,0 +1,189 @@
+// detail: https://atcoder.jp/contests/arc008/submissions/9159091
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Reader;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var n = NextInt;
+        if (n == 1) { Console.WriteLine(0); return; }
+        var users = Enumerable.Repeat(0, n).Select(_ => new User() { Position = new Vector(NextInt, NextInt), ThrowSpeed = NextInt, RecieveSpeed = NextInt }).ToArray();
+        double[][] distance = Enumerable.Range(0, n).Select(_ => new double[n]).ToArray();
+        for (int i = 0; i < users.Length; i++)
+        {
+            var throwUser = users[i];
+            for (int j = 0; j < users.Length; j++)
+            {
+                var recieveUser = users[j];
+                var dist = (throwUser.Position - recieveUser.Position).SqrtLength;
+                var speed = Min(throwUser.ThrowSpeed, recieveUser.RecieveSpeed);
+                distance[i][j] = dist / speed;
+            }
+        }
+        double[] res = new double[n];
+        PriorityQueue<Tuple<int, double>, double> pqueue = new PriorityQueue<Tuple<int, double>, double>(x => x.Item2);
+        pqueue.Push(new Tuple<int, double>(0, 0));
+        while (pqueue.Count > 0)
+        {
+            var elem = pqueue.Pop();
+            if (res[elem.Item1] != 0) continue;
+            res[elem.Item1] = elem.Item2;
+            for (int i = 1; i < res.Length; i++)
+            {
+                if (res[i] != 0) continue;
+                var newelem = new Tuple<int, double>(i, elem.Item2 + distance[elem.Item1][i]);
+                pqueue.Push(newelem);
+            }
+        }
+        Console.WriteLine(res.Skip(1).OrderByDescending(x => x).Select((elem, count) => elem + count).Max());
+    }
+}
+
+struct User
+{
+    public Vector Position;
+    public int ThrowSpeed;
+    public int RecieveSpeed;
+}
+
+class PriorityQueue<TValue, TKey> where TKey : IComparable<TKey>
+{
+    public int Count { get; private set; }
+    private Func<TValue, TKey> KeySelector;
+    private bool Descendance;
+    private TValue[] data = new TValue[65536];
+    private TKey[] keys = new TKey[65536];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public PriorityQueue(Func<TValue, TKey> keySelector, bool descendance = false) { KeySelector = keySelector; Descendance = descendance; }
+    public TValue Top
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get { ValidateNonEmpty(); return data[1]; }
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public TValue Pop()
+    {
+        var top = Top;
+        var item = data[Count--];
+        var key = KeySelector(item);
+        int index = 1;
+        while (true)
+        {
+            if ((index << 1) >= Count)
+            {
+                if (index << 1 > Count) break;
+                if (key.CompareTo(keys[index << 1]) > 0 ^ Descendance)
+                { data[index] = data[index << 1]; keys[index] = keys[index << 1]; index <<= 1; }
+                else break;
+            }
+            else
+            {
+                var nextIndex = keys[index << 1].CompareTo(keys[(index << 1) + 1]) <= 0 ^ Descendance ? (index << 1) : (index << 1) + 1;
+                if (key.CompareTo(keys[nextIndex]) > 0 ^ Descendance)
+                { data[index] = data[nextIndex]; keys[index] = keys[nextIndex]; index = nextIndex; }
+                else break;
+            }
+        }
+        data[index] = item;
+        keys[index] = key;
+        return top;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Push(TValue item)
+    {
+        var key = KeySelector(item);
+        int index = ++Count;
+        if (data.Length == Count) Extend(data.Length * 2);
+        while ((index >> 1) != 0)
+        {
+            if (keys[index >> 1].CompareTo(key) > 0 ^ Descendance)
+            { data[index] = data[index >> 1]; keys[index] = keys[index >> 1]; index >>= 1; }
+            else break;
+        }
+        data[index] = item;
+        keys[index] = key;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Extend(int newSize)
+    {
+        TValue[] newData = new TValue[newSize];
+        TKey[] newKeys = new TKey[newSize];
+        data.CopyTo(newData, 0);
+        keys.CopyTo(newKeys, 0);
+        data = newData;
+        keys = newKeys;
+    }
+    private void ValidateNonEmpty() { if (Count == 0) throw new IndexOutOfRangeException(); }
+}
+
+struct Vector
+{
+    public double x;
+    public double y;
+    public double this[int index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get { if (index == 0) return x; else return y; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set { if (index == 0) x = value; else y = value; }
+    }
+    public double Length => x * x + y * y;
+    public double SqrtLength => Math.Sqrt(Length);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector(double x, double y) { this.x = x; this.y = y; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector RotateVector(double radian) => new Vector((double)(x * Math.Cos(radian) - y * Math.Sin(radian)), (double)(x * Math.Sin(radian) + y * Math.Cos(radian)));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public double CrossProduct(Vector a, Vector b) => a.x * b.y - a.y * b.x;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public double InnerProduct(Vector a, Vector b) => a.x * b.x + a.y * b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Vector a, Vector b) => a.x == b.x && a.y == b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Vector a, Vector b) => a.x != b.x || a.y != b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object obj) => this == (Vector)obj;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => x.GetHashCode() * 1000000007 + y.GetHashCode();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => $"({x},{y})";
+}
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = BUF_SIZE - 1;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; int sign = 1; while (Buffer[ptr] < 45) Move();
+            if (Buffer[ptr] == 45) { Move(); sign = -1; }
+            do { res = res * 10 + (Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res * sign;
+        }
+    }
+}
