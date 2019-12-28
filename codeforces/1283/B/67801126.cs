76ae66c9a027ebe2d9f282bdfce3159ee49fc159
crawler <kymn0116+crawler@gmail.com>
2019-12-28 17:10:10 +0000

    add codeforces/1283/B/67801126.cs

---
+++ codeforces/1283/B/67801126.cs
@@ -0,0 +1,33 @@
+// detail: https://codeforces.com/contest/1283/submission/67801126
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            Solve();
+        }
+    }
+    static void Solve()
+    {
+        var hw = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = hw[0];
+        var k = hw[1];
+        Console.WriteLine(Min((n / k) * k + k / 2, n));
+    }
+}
