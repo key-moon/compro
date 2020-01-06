1fd19408e232176bf83a44d2bf437f6fa623a55d
crawler <kymn0116+crawler@gmail.com>
2020-01-06 04:48:11 +0000

    add atcoder/arc045/arc045_a/9331459.cs

---
+++ atcoder/arc045/arc045_a/9331459.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/arc045/submissions/9331459
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Replace("Left", "<").Replace("Right", ">").Replace("AtCoder", "A"));
+    }
+}
