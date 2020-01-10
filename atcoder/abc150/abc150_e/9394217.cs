2e7f731bf9824f394e9304498ec9b83e4626a10c
crawler <kymn0116+crawler@gmail.com>
2020-01-10 12:31:36 +0000

    add atcoder/abc150/abc150_e/9394217.cs

---
+++ atcoder/abc150/abc150_e/9394217.cs
@@ -0,0 +1,90 @@
+// detail: https://atcoder.jp/contests/abc150/submissions/9394217
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var c = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToArray();
+        ModInt res = 0;
+        for (int i = 0; i < c.Length; i++)
+        {
+            res += (((ModInt)i + 2) / 2) * c[i];
+        }
+        res *= Power(2, 2 * n - 1);
+        Console.WriteLine(res);
+    }
+
+
+    static ModInt Power(ModInt n, long m)
+    {
+        ModInt pow = n;
+        ModInt res = 1;
+        while (m > 0)
+        {
+            if ((m & 1) == 1) res *= pow;
+            pow *= pow;
+            m >>= 1;
+        }
+        return res;
+    }
+}
+
+
+struct ModInt
+{
+    const int MOD = 1000000007;
+    const long POSITIVIZER = ((long)MOD) << 31;
+    long Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ModInt(long data) { if ((Data = data % MOD) < 0) Data += MOD; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= MOD ? res - MOD : res }; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + MOD : res }; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % MOD };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => Data.ToString();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static long GetInverse(long a)
+    {
+        long div, p = MOD, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + MOD; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + MOD; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
