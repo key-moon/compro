25b4bf94797aac9d84d354b32267c0bcbae8b8ef
crawler <kymn0116+crawler@gmail.com>
2020-01-10 12:06:01 +0000

    add atcoder/abc150/abc150_b/9384052.cs

---
+++ atcoder/abc150/abc150_b/9384052.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/abc150/submissions/9384052
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var s = Console.ReadLine();
+        Console.WriteLine(Regex.Matches(s, "ABC").Count);
+    }
+}
