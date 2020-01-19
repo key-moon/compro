8557ba968af890906362bc17b4a284a9ca40c58c
crawler <kymn0116+crawler@gmail.com>
2020-01-19 11:51:14 +0000

    add atcoder/abc152/abc152_c/9598187.cs

---
+++ atcoder/abc152/abc152_c/9598187.cs
@@ -0,0 +1,57 @@
+// detail: https://atcoder.jp/contests/abc152/submissions/9598187
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Reader;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int res = 0;
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var min = int.MaxValue;
+        foreach (var item in a)
+        {
+            if (item < min)
+            {
+                res++;
+                min = item;
+            }
+        }
+        Console.WriteLine(res);
+    }
+}
+
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = BUF_SIZE - 1;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (int)(Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
