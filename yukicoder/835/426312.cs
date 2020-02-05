337187892baa14e4df5d784d839b004ef4516c35
crawler <kymn0116+crawler@gmail.com>
2020-02-05 04:56:42 +0000

    add yukicoder/835/426312.cs

---
+++ yukicoder/835/426312.cs
@@ -0,0 +1,23 @@
+// detail: https://yukicoder.me/submissions/426312
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+using System.Threading.Tasks;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var n = long.Parse(Console.ReadLine());
+        Console.WriteLine(n + n / 2);
+    }
+}
