d0b432fcddb3db2dc0dba4b656c7871d5775b667
crawler <kymn0116+crawler@gmail.com>
2020-02-05 05:26:55 +0000

    add yukicoder/892/426325.cs

---
+++ yukicoder/892/426325.cs
@@ -0,0 +1,50 @@
+// detail: https://yukicoder.me/submissions/426325
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+using System.Threading.Tasks;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using static Reader;
+
+public static class P
+{
+    public static void Main()
+    {
+        var a1 = NextInt;
+        var b1 = NextInt;
+        var a2 = NextInt;
+        var b2 = NextInt;
+        var a3 = NextInt;
+        var b3 = NextInt;
+
+        Console.WriteLine((a1 + a2 + a3) % 2 == 0 ? ":-)" : ":-(");
+    }
+}
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = BUF_SIZE - 1;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (int)(Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
