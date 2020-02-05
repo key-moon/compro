360567e35aae95af1c21850fb67bf17ee94478b3
crawler <kymn0116+crawler@gmail.com>
2020-02-05 05:19:24 +0000

    add yukicoder/964/426322.cs

---
+++ yukicoder/964/426322.cs
@@ -0,0 +1,24 @@
+// detail: https://yukicoder.me/submissions/426322
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+using System.Threading.Tasks;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(string.Join("", Enumerable.Range(0, 10).Reverse().Take(n).SelectMany(x => Enumerable.Repeat(x, n))));
+    }
+}
