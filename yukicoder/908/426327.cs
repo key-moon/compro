b36d23c59125ad330d94a01c62794cb3b9496ac0
crawler <kymn0116+crawler@gmail.com>
2020-02-05 05:38:49 +0000

    add yukicoder/908/426327.cs

---
+++ yukicoder/908/426327.cs
@@ -0,0 +1,23 @@
+// detail: https://yukicoder.me/submissions/426327
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Math;
+using System.Threading.Tasks;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var s = Console.ReadLine();
+        Console.WriteLine(Regex.IsMatch(s, @"^\S(\s\S)*$") ? "Yes" : "No");
+    }
+}
