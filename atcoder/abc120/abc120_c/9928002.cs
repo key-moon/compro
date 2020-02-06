255fec0c7508330d1ce1fbf8d9e5c910d81da86d
crawler <kymn0116+crawler@gmail.com>
2020-02-06 12:56:03 +0000

    add atcoder/abc120/abc120_c/9928002.cs

---
+++ atcoder/abc120/abc120_c/9928002.cs
@@ -0,0 +1,45 @@
+// detail: https://atcoder.jp/contests/abc120/submissions/9928002
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using static Reader;
+
+public static class P
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(Min(s.Count(x => x == '0'), s.Count(x => x == '1')) * 2);
+    }
+}
+
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = BUF_SIZE - 1;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (int)(Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
