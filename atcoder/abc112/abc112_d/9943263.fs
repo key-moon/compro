11320c86c79942fea96ffa446efdb4bda6b292a3
crawler <kymn0116+crawler@gmail.com>
2020-02-07 14:58:33 +0000

    add atcoder/abc112/abc112_d/9943263.fs

---
+++ atcoder/abc112/abc112_d/9943263.fs
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc112/submissions/9943263
+let nm = stdin.ReadLine().Split() |> Array.map int
+let (n, m) = (nm.[0], nm.[1])
+let possibleMax = m / n;
+[|1..(m |> double |> sqrt |> ceil |> int)|]
+|> Array.collect (
+    fun x ->
+        if m % x = 0 then 
+            [|x; m / x|]
+        else
+            [||] 
+)
+|> Array.filter (fun x -> x <= possibleMax)
+|> Array.max
+|> stdout.WriteLine
