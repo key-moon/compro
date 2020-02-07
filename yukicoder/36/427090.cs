e87131142c5108688e6acae2703676f00bf30692
crawler <kymn0116+crawler@gmail.com>
2020-02-07 08:39:30 +0000

    add yukicoder/36/427090.cs

---
+++ yukicoder/36/427090.cs
@@ -0,0 +1,39 @@
+// detail: https://yukicoder.me/submissions/427090
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var n = long.Parse(Console.ReadLine());
+
+        Console.WriteLine(PrimeFactors(n).Count() >= 3 ? "YES" : "NO");
+    }
+
+    static IEnumerable<long> PrimeFactors(long n)
+    {
+        while ((n & 1) == 0)
+        {
+            n >>= 1;
+            yield return 2;
+        }
+        for (long i = 3; i * i <= n; i += 2)
+        {
+            while (n % i == 0)
+            {
+                n /= i;
+                yield return i;
+            }
+        }
+        if (n != 1) yield return n;
+    }
+}
