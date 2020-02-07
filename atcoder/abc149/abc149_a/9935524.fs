27a4199725b814112bf2628c572459ea526871b1
crawler <kymn0116+crawler@gmail.com>
2020-02-07 05:18:01 +0000

    add atcoder/abc149/abc149_a/9935524.fs

---
+++ atcoder/abc149/abc149_a/9935524.fs
@@ -0,0 +1,5 @@
+// detail: https://atcoder.jp/contests/abc149/submissions/9935524
+[<EntryPoint>]
+let main argv =
+    stdin.ReadLine().Split() |> Array.rev |> String.concat "" |> stdout.WriteLine
+    0
