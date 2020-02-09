13e256719335cd573afd63b0a6fc719d5d0d5e36
crawler <kymn0116+crawler@gmail.com>
2020-02-09 12:43:41 +0000

    add atcoder/abc154/abc154_f/9997388.cs

---
+++ atcoder/abc154/abc154_f/9997388.cs
@@ -0,0 +1,91 @@
+// detail: https://atcoder.jp/contests/abc154/submissions/9997388
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Reader;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+public static class P
+{
+    public static void Main()
+    {
+        var r1 = NextInt;
+        var c1 = NextInt;
+        var r2 = NextInt;
+        var c2 = NextInt;
+        Console.WriteLine(RangeSum(r2 + 1, c2 + 1) - RangeSum(r1, c2 + 1) - RangeSum(r2 + 1, c1) + RangeSum(r1, c1));
+    }
+    static ModInt RangeSum(int a, int b) => Factorial(a + b) / Factorial(b) / Factorial(a) - 1;
+
+    static List<ModInt> factorialMemo = new List<ModInt>() { 1 };
+    static ModInt Factorial(int x)
+    {
+        for (int i = factorialMemo.Count; i <= x; i++) factorialMemo.Add(factorialMemo.Last() * i);
+        return factorialMemo[x];
+    }
+}
+
+
+struct ModInt
+{
+    public const int Mod = 1000000007;
+    const long POSITIVIZER = ((long)Mod) << 31;
+    long Data;
+    public ModInt(long data) { if ((Data = data % Mod) < 0) Data += Mod; }
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % Mod };
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= Mod ? res - Mod : res }; }
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % Mod };
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + Mod : res }; }
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % Mod };
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % Mod };
+    public static bool operator ==(ModInt a, ModInt b) => a.Data == b.Data;
+    public static bool operator !=(ModInt a, ModInt b) => a.Data != b.Data;
+    public override string ToString() => Data.ToString();
+    public override bool Equals(object obj) => (ModInt)obj == this;
+    public override int GetHashCode() => (int)Data;
+    static long GetInverse(long a)
+    {
+        long div, p = Mod, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + Mod; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + Mod; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
+
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = BUF_SIZE - 1;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (int)(Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
