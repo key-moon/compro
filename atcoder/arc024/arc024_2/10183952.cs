d37c03a507a9fbe1939f4449c7b321bff9fc9b1c
crawler <kymn0116+crawler@gmail.com>
2020-02-17 13:51:43 +0000

    add atcoder/arc024/arc024_2/10183952.cs

---
+++ atcoder/arc024/arc024_2/10183952.cs
@@ -0,0 +1,71 @@
+// detail: https://atcoder.jp/contests/arc024/submissions/10183952
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var tree = Enumerable.Repeat(0, n).Select(_ => int.Parse(Console.ReadLine())).ToArray();
+        UnionFind uf = new UnionFind(n);
+        if (tree[0] == tree[n - 1]) uf.TryUnite(0, n - 1);
+        for (int i = 1; i < n; i++)
+            if (tree[i] == tree[i - 1]) uf.TryUnite(i, i - 1);
+        var maxSize = uf.AllRepresents.Select(x => uf.GetSize(x)).Max();
+        if (maxSize == n)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+        Console.WriteLine((maxSize + 1) / 2);
+    }
+}
+
+
+class UnionFind
+{
+    public int Size { get; private set; }
+    public int GroupCount { get; private set; }
+    public IEnumerable<int> AllRepresents => Parent.Where((x, y) => x == y);
+    int[] Sizes;
+    int[] Parent;
+    public UnionFind(int count)
+    {
+        Size = count;
+        GroupCount = count;
+        Parent = new int[count];
+        Sizes = new int[count];
+        for (int i = 0; i < count; i++) Sizes[Parent[i] = i] = 1;
+    }
+    public bool TryUnite(int x, int y)
+    {
+        int xp = Find(x);
+        int yp = Find(y);
+        if (yp == xp) return false;
+        if (Sizes[xp] < Sizes[yp]) { var tmp = xp; xp = yp; yp = tmp; }
+        GroupCount--;
+        Parent[yp] = xp;
+        Sizes[xp] += Sizes[yp];
+        return true;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int GetSize(int x) => Sizes[Find(x)];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Find(int x)
+    {
+        while (x != Parent[x]) x = (Parent[x] = Parent[Parent[x]]);
+        return x;
+    }
+}
