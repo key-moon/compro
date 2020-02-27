f74501bc69693ebcd2121cc77a678880aee14961
crawler <kymn0116+crawler@gmail.com>
2020-02-27 07:48:47 +0000

    add atcoder/arc003/arc003_1/10371821.cs

---
+++ atcoder/arc003/arc003_1/10371821.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/arc003/submissions/10371821
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(Console.ReadLine().Average(x => Max(0, 'E' - x)));
+    }
+}
