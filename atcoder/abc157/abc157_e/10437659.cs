4d5dbed86a87f42aa15db06b3b3ac3b5521e570e
crawler <kymn0116+crawler@gmail.com>
2020-03-01 12:16:21 +0000

    add atcoder/abc157/abc157_e/10437659.cs

---
+++ atcoder/abc157/abc157_e/10437659.cs
@@ -0,0 +1,104 @@
+// detail: https://atcoder.jp/contests/abc157/submissions/10437659
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var s = Console.ReadLine();
+        var q = int.Parse(Console.ReadLine());
+        SegmentTree<int> segTree = new SegmentTree<int>(s.Select(x => 1 << (x - 'a')).ToArray(), 0, (x, y) => x | y);
+        for (int i = 0; i < q; i++)
+        {
+            var query = Console.ReadLine().Split();
+            if (query[0] == "1")
+            {
+                segTree[int.Parse(query[1]) - 1] = (1 << (query[2][0] - 'a'));
+            }
+            else
+            {
+                Console.WriteLine(PopCount(segTree.Query(int.Parse(query[1]) - 1, int.Parse(query[2]) - 1)));
+            }
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static int PopCount(int n)
+    {
+        int msb = 0;
+        if (n < 0) { n &= int.MaxValue; msb = 1; }
+        n = n - ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        return (int)(((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24) + msb;
+    }
+}
+
+
+class SegmentTree<T>
+{
+    public int Size { get; private set; }
+    T Identity;
+    T[] Data;
+    Func<T, T, T> Merge;
+    int LeafCount;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public SegmentTree(int size, T identity, Func<T, T, T> merge)
+    {
+        Init(size, identity, merge);
+        for (int i = 1; i < Data.Length; i++) Data[i] = identity;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public SegmentTree(T[] elems, T identity, Func<T, T, T> merge)
+    {
+        Init(elems.Length, identity, merge);
+        elems.CopyTo(Data, LeafCount);
+        for (int i = elems.Length + LeafCount; i < Data.Length; i++) Data[i] = identity;
+        for (int i = LeafCount - 1; i >= 1; i--) Data[i] = Merge(Data[i << 1], Data[(i << 1) | 1]);
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Init(int size, T identity, Func<T, T, T> merge)
+    {
+        Size = size;
+        Identity = identity;
+        Merge = merge;
+        LeafCount = 1;
+        while (LeafCount < size) LeafCount <<= 1;
+        Data = new T[LeafCount << 1];
+    }
+
+    public T this[int index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get { return Data[LeafCount + index]; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set { Update(index, value); }
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Update(int i, T x) { Data[i += LeafCount] = x; while (0 < (i >>= 1)) Data[i] = Merge(Data[i << 1], Data[(i << 1) | 1]); }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Operate(int i, T x) { Data[i += LeafCount] = Merge(Data[i], x); while (0 < (i >>= 1)) Data[i] = Merge(Data[i << 1], Data[(i << 1) | 1]); }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Query(int l, int r)
+    {
+        T lRes = Identity, rRes = Identity;
+        for (l += LeafCount, r += LeafCount; l <= r; l = (l + 1) >> 1, r = (r - 1) >> 1)
+        {
+            if ((l & 1) == 1) lRes = Merge(lRes, Data[l]); if ((r & 1) == 0) rRes = Merge(Data[r], rRes);
+        }
+        return Merge(lRes, rRes);
+    }
+}
