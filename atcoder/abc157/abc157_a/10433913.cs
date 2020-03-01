87766b0f0d79061bf6c3f3c609ba988ee5894a2a
crawler <kymn0116+crawler@gmail.com>
2020-03-01 12:09:08 +0000

    add atcoder/abc157/abc157_a/10433913.cs

---
+++ atcoder/abc157/abc157_a/10433913.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc157/submissions/10433913
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine((int.Parse(Console.ReadLine()) + 1) / 2);
+    }
+}
