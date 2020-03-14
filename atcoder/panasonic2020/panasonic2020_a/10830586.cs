404c613450a3f7ff301699e96a356cc6de9d5e83
crawler <kymn0116+crawler@gmail.com>
2020-03-14 12:04:04 +0000

    add atcoder/panasonic2020/panasonic2020_a/10830586.cs

---
+++ atcoder/panasonic2020/panasonic2020_a/10830586.cs
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/panasonic2020/submissions/10830586
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var arr = new[] { 1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51 };
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(arr[n - 1]);
+    }
+}
