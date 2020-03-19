6a8f6f9497958da04d47d4a9d35f2da820d5c227
crawler <kymn0116+crawler@gmail.com>
2020-03-19 14:37:33 +0000

    add codeforces/1326/A/73662266.cs

---
+++ codeforces/1326/A/73662266.cs
@@ -0,0 +1,37 @@
+// detail: https://codeforces.com/contest/1326/submission/73662266
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            Solve();
+        }
+    }
+    static void Solve()
+    {
+        int n = int.Parse(Console.ReadLine());
+        if (n == 1)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+        Console.WriteLine($"2{string.Join("", Enumerable.Repeat(3, n - 1))}");
+    }
+}
