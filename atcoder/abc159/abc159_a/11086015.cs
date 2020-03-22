ff52ec792ad49f98aff58dadf92a66440acbf2b0
crawler <kymn0116+crawler@gmail.com>
2020-03-22 12:03:16 +0000

    add atcoder/abc159/abc159_a/11086015.cs

---
+++ atcoder/abc159/abc159_a/11086015.cs
@@ -0,0 +1,25 @@
+// detail: https://atcoder.jp/contests/abc159/submissions/11086015
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nm[0];
+        var m = nm[1];
+        Console.WriteLine(m * (m - 1) / 2 + n * (n - 1) / 2);
+    }
+}
