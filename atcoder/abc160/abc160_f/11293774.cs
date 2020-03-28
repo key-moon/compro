1c15d5b395ec5d8741fcb16136e63e89d1f56e8e
crawler <kymn0116+crawler@gmail.com>
2020-03-28 12:32:54 +0000

    add atcoder/abc160/abc160_f/11293774.cs

---
+++ atcoder/abc160/abc160_f/11293774.cs
@@ -0,0 +1,215 @@
+// detail: https://atcoder.jp/contests/abc160/submissions/11293774
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var edges = Enumerable.Repeat(0, n - 1).Select(_ => Console.ReadLine().Split().Select(x => int.Parse(x) - 1).ToArray()).ToArray();
+
+        ReRooting<Data> res =
+            new ReRooting<Data>(
+                n,
+                edges,
+                new Data() { Combination = 1, Count = 0 },
+                (x, y) =>
+                {
+                    var newCount = x.Count + y.Count;
+                    return new Data()
+                    {
+                        Combination = x.Combination * y.Combination * Factorial(newCount) / Factorial(x.Count) / Factorial(y.Count),
+                        Count = newCount
+                    };
+                },
+                (x, _) => new Data()
+                {
+                    Combination = x.Combination,
+                    Count = x.Count + 1
+                });
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(res.Query(i).Combination);
+        }
+    }
+
+    static List<ModInt> factorialMemo = new List<ModInt>() { 1 };
+    static ModInt Factorial(int x)
+    {
+        for (int i = factorialMemo.Count; i <= x; i++) factorialMemo.Add(factorialMemo.Last() * i);
+        return factorialMemo[x];
+    }
+}
+
+struct Data
+{
+    public ModInt Combination;
+    public int Count;
+}
+
+struct ModInt
+{
+    public const int Mod = 1000000007;
+    const long POSITIVIZER = ((long)Mod) << 31;
+    long Data;
+    public ModInt(long data) { if ((Data = data % Mod) < 0) Data += Mod; }
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % Mod };
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= Mod ? res - Mod : res }; }
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % Mod };
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + Mod : res }; }
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % Mod };
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % Mod };
+    public static bool operator ==(ModInt a, ModInt b) => a.Data == b.Data;
+    public static bool operator !=(ModInt a, ModInt b) => a.Data != b.Data;
+    public override string ToString() => Data.ToString();
+    public override bool Equals(object obj) => (ModInt)obj == this;
+    public override int GetHashCode() => (int)Data;
+    static long GetInverse(long a)
+    {
+        long div, p = Mod, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + Mod; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + Mod; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
+
+class ReRooting<T>
+{
+    public int NodeCount { get; private set; }
+
+    int[][] Adjacents;
+    int[][] IndexForAdjacent;
+
+    T[] Res;
+    T[][] DP;
+
+    T Identity;
+    Func<T, T, T> Operate;
+    Func<T, int, T> OperateNode;
+
+    public ReRooting(int nodeCount, int[][] edges, T identity, Func<T, T, T> operate, Func<T, int, T> operateNode)
+    {
+        NodeCount = nodeCount;
+
+        Identity = identity;
+        Operate = operate;
+        OperateNode = operateNode;
+
+        List<int>[] adjacents = new List<int>[nodeCount];
+        List<int>[] indexForAdjacents = new List<int>[nodeCount];
+
+        for (int i = 0; i < nodeCount; i++)
+        {
+            adjacents[i] = new List<int>();
+            indexForAdjacents[i] = new List<int>();
+        }
+        for (int i = 0; i < edges.Length; i++)
+        {
+            var edge = edges[i];
+            indexForAdjacents[edge[0]].Add(adjacents[edge[1]].Count);
+            indexForAdjacents[edge[1]].Add(adjacents[edge[0]].Count);
+            adjacents[edge[0]].Add(edge[1]);
+            adjacents[edge[1]].Add(edge[0]);
+        }
+
+        Adjacents = new int[nodeCount][];
+        IndexForAdjacent = new int[nodeCount][];
+        for (int i = 0; i < nodeCount; i++)
+        {
+            Adjacents[i] = adjacents[i].ToArray();
+            IndexForAdjacent[i] = indexForAdjacents[i].ToArray();
+        }
+
+        DP = new T[Adjacents.Length][];
+        Res = new T[Adjacents.Length];
+
+        for (int i = 0; i < Adjacents.Length; i++) DP[i] = new T[Adjacents[i].Length];
+        if (NodeCount > 1) Initialize();
+        else if (NodeCount == 1) Res[0] = OperateNode(Identity, 0);
+    }
+
+    public T Query(int node) => Res[node];
+
+    private void Initialize()
+    {
+        int[] parents = new int[NodeCount];
+        int[] order = new int[NodeCount];
+
+        #region InitOrderedTree
+        var index = 0;
+        Stack<int> stack = new Stack<int>();
+        stack.Push(0);
+        parents[0] = -1;
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+            order[index++] = node;
+            for (int i = 0; i < Adjacents[node].Length; i++)
+            {
+                var adjacent = Adjacents[node][i];
+                if (adjacent == parents[node]) continue;
+                stack.Push(adjacent);
+                parents[adjacent] = node;
+            }
+        }
+        #endregion
+
+        #region fromLeaf
+        for (int i = order.Length - 1; i >= 1; i--)
+        {
+            var node = order[i];
+            var parent = parents[node];
+
+            T accum = Identity;
+            int parentIndex = -1;
+            for (int j = 0; j < Adjacents[node].Length; j++)
+            {
+                if (Adjacents[node][j] == parent)
+                {
+                    parentIndex = j;
+                    continue;
+                }
+                accum = Operate(accum, DP[node][j]);
+            }
+            DP[parent][IndexForAdjacent[node][parentIndex]] = OperateNode(accum, node);
+        }
+        #endregion
+
+        #region toLeaf
+        for (int i = 0; i < order.Length; i++)
+        {
+            var node = order[i];
+            T accum = Identity;
+            T[] accumsFromTail = new T[Adjacents[node].Length];
+            accumsFromTail[accumsFromTail.Length - 1] = Identity;
+            for (int j = accumsFromTail.Length - 1; j >= 1; j--) accumsFromTail[j - 1] = Operate(DP[node][j], accumsFromTail[j]);
+            for (int j = 0; j < accumsFromTail.Length; j++)
+            {
+                DP[Adjacents[node][j]][IndexForAdjacent[node][j]] = OperateNode(Operate(accum, accumsFromTail[j]), node);
+                accum = Operate(accum, DP[node][j]);
+            }
+            Res[node] = OperateNode(accum, node);
+        }
+        #endregion
+    }
+}
