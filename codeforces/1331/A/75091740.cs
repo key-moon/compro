82cbb9f2d505d675bcb9f209b73e1d9455ce7b26
crawler <kymn0116+crawler@gmail.com>
2020-04-01 14:35:20 +0000

    add codeforces/1331/A/75091740.cs

---
+++ codeforces/1331/A/75091740.cs
@@ -0,0 +1,22 @@
+// detail: https://codeforces.com/contest/1331/submission/75091740
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+
+public static class P
+{
+    static void Main()
+    {
+        Console.WriteLine("No");
+    }
+}
