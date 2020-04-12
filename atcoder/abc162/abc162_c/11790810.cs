a65a6f5a76a8e4ec5f2c3390f315afc7d84a33db
crawler <kymn0116+crawler@gmail.com>
2020-04-12 12:01:53 +0000

    add atcoder/abc162/abc162_c/11790810.cs

---
+++ atcoder/abc162/abc162_c/11790810.cs
@@ -0,0 +1,45 @@
+// detail: https://atcoder.jp/contests/abc162/submissions/11790810
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        long res = 0;
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                var gcd = GCD(i, j);
+                for (int k = 1; k <= n; k++)
+                {
+                    res += GCD(gcd, k);
+                }
+            }
+        }
+        Console.WriteLine(res);
+    }
+
+
+    static long GCD(long a, long b)
+    {
+        while (true)
+        {
+            if (b == 0) return a;
+            a %= b;
+            if (a == 0) return b;
+            b %= a;
+        }
+    }
+}
