2f9ae7a12b91d0c2394923be7bdba81f84898e79
crawler <kymn0116+crawler@gmail.com>
2020-04-12 13:03:38 +0000

    add atcoder/abc162/abc162_a/11841910.cs

---
+++ atcoder/abc162/abc162_a/11841910.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc162/submissions/11841910
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+
+        Console.WriteLine(Console.ReadLine().Contains('7') ? "Yes" : "No");
+    }
+}
