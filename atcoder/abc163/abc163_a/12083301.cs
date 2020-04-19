f237ce810c6a5684b583b279a2dd05f5fca4569f
crawler <kymn0116+crawler@gmail.com>
2020-04-19 12:04:07 +0000

    add atcoder/abc163/abc163_a/12083301.cs

---
+++ atcoder/abc163/abc163_a/12083301.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc163/submissions/12083301
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n * PI * 2);
+    }
+}
