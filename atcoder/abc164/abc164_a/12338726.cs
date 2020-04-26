f1b273c24e1cad82d6f095cd36507982ec944f35
crawler <kymn0116+crawler@gmail.com>
2020-04-26 12:02:26 +0000

    add atcoder/abc164/abc164_a/12338726.cs

---
+++ atcoder/abc164/abc164_a/12338726.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc164/submissions/12338726
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        var sw = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine(sw[0] <= sw[1] ? "unsafe" : "safe");
+    }
+}
