5a3ba3ef7347a2aabe34f75bab09ac4f85cace04
crawler <kymn0116+crawler@gmail.com>
2020-05-03 12:33:24 +0000

    add atcoder/abc166/abc166_c/12740406.cs

---
+++ atcoder/abc166/abc166_c/12740406.cs
@@ -0,0 +1,43 @@
+// detail: https://atcoder.jp/contests/abc166/submissions/12740406
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+public static class P
+{
+    public static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nm[0];
+        var m = nm[1];
+        var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        List<int>[] graph = Enumerable.Repeat(0, n).Select(_ => new List<int>()).ToArray();
+        for (int i = 0; i < m; i++)
+        {
+            var st = Console.ReadLine().Split().Select(x => int.Parse(x) - 1).ToArray();
+            graph[st[0]].Add(st[1]);
+            graph[st[1]].Add(st[0]);
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            foreach (var item in graph[i])
+            {
+                if (h[i] <= h[item]) goto end;
+            }
+            res++;
+            end:;
+        }
+        Console.WriteLine(res);
+    }
+}
