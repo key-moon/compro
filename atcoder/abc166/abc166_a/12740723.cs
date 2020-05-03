30ac025e1f510cf476e207334ff14e2cfeb7431c
crawler <kymn0116+crawler@gmail.com>
2020-05-03 12:33:59 +0000

    add atcoder/abc166/abc166_a/12740723.cs

---
+++ atcoder/abc166/abc166_a/12740723.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc166/submissions/12740723
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var s =  Console.ReadLine();
+        if (s == "ABC") Console.WriteLine("ARC");
+        else Console.WriteLine( "ABC");
+    }
+}
