17c6d90d10f5d80617b04ee5dfcdcfd45fbe4092
crawler <kymn0116+crawler@gmail.com>
2020-05-09 14:39:03 +0000

    add codeforces/1352/A/79474792.cs

---
+++ codeforces/1352/A/79474792.cs
@@ -0,0 +1,68 @@
+// detail: https://codeforces.com/contest/1352/submission/79474792
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Reader;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> res = new List<int>();
+            for (int j = 10; n != 0; j *= 10)
+            {
+                if (n % j != 0) res.Add(n % j);
+                n -= n % j;
+            }
+            Console.WriteLine(res.Count);
+            Console.WriteLine(string.Join(" ", res));
+        }
+    }
+}
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = BUF_SIZE - 1;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; int sign = 1; while (Buffer[ptr] < 45) Move();
+            if (Buffer[ptr] == 45) { Move(); sign = -1; }
+            do { res = res * 10 + (Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res * sign;
+        }
+    }
+
+    public static long NextLong
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            long res = 0; int sign = 1; while (Buffer[ptr] < 45) Move();
+            if (Buffer[ptr] == 45) { Move(); sign = -1; }
+            do { res = res * 10 + (Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res * sign;
+        }
+    }
+}
