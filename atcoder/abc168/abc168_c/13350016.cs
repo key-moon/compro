34cead15dc9d190693d492edba0ca40734655109
crawler <kymn0116+crawler@gmail.com>
2020-05-17 14:15:38 +0000

    add atcoder/abc168/abc168_c/13350016.cs

---
+++ atcoder/abc168/abc168_c/13350016.cs
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/abc168/submissions/13350016
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var abhm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var a = abhm[0];
+        var b = abhm[1];
+        var h = abhm[2];
+        var m = abhm[3];
+
+        var hoge = ((h * 60.0 + m) / (12 * 60) * 11) % 1;
+        var deg = hoge * 2 * PI;
+        var cos = Cos(deg) * a;
+        var sin = Sin(deg) * a;
+        Console.WriteLine(Sqrt(Pow(b - cos, 2) + Pow(Abs(sin), 2)));
+    }
+}
