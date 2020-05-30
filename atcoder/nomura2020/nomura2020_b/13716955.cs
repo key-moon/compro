28d6ffa296d7a815c1c9be1bd1ebd1515c8f58ee
crawler <kymn0116+crawler@gmail.com>
2020-05-30 12:03:23 +0000

    add atcoder/nomura2020/nomura2020_b/13716955.cs

---
+++ atcoder/nomura2020/nomura2020_b/13716955.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/nomura2020/submissions/13716955
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Replace('?', 'D'));
+    }
+}
