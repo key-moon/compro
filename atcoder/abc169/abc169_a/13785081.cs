75e54c70584ec72fa269b3d9542bddeb424ba24f
crawler <kymn0116+crawler@gmail.com>
2020-05-31 12:03:44 +0000

    add atcoder/abc169/abc169_a/13785081.cs

---
+++ atcoder/abc169/abc169_a/13785081.cs
@@ -0,0 +1,33 @@
+// detail: https://atcoder.jp/contests/abc169/submissions/13785081
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        try
+        {
+            checked
+            {
+                var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+                var res = a.Contains(0) ? 0 : a.Aggregate(1L, (x, y) => x * y);
+                if (res > 1000000000000000000) throw new Exception();
+                Console.WriteLine(res);
+            }
+        }
+        catch
+        {
+            Console.WriteLine(-1);
+        }
+    }
+}
