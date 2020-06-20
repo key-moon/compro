c19d2cbb4e661281397dbb21d31a7f89785976d7
crawler <kymn0116+crawler@gmail.com>
2020-06-20 12:02:33 +0000

    add atcoder/agc046/agc046_a/14494913.cs

---
+++ atcoder/agc046/agc046_a/14494913.cs
@@ -0,0 +1,28 @@
+// detail: https://atcoder.jp/contests/agc046/submissions/14494913
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 360; true; i += 360)
+        {
+            if (i % n == 0)
+            {
+                Console.WriteLine(i / n);
+                return;
+            }
+        }
+    }
+}
