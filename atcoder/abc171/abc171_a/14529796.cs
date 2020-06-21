5ec63aba5aeb2a81166f301b4af3cb55a7078a91
crawler <kymn0116+crawler@gmail.com>
2020-06-21 12:01:54 +0000

    add atcoder/abc171/abc171_a/14529796.cs

---
+++ atcoder/abc171/abc171_a/14529796.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc171/submissions/14529796
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(char.IsUpper(Console.ReadLine()[0]) ? 'A' : 'a');
+    }
+}
