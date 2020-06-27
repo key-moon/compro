ea6163dda7b3664d55ba9849f24d340bc8518f23
crawler <kymn0116+crawler@gmail.com>
2020-06-27 12:05:13 +0000

    add atcoder/abc172/abc172_a/14734176.cs

---
+++ atcoder/abc172/abc172_a/14734176.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc172/submissions/14734176
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(a + a * a + a * a * a );
+    }
+}
