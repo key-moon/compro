a11521174b73ef6a5d21bc57f25295f4ca836f35
crawler <kymn0116+crawler@gmail.com>
2020-07-04 14:59:51 +0000

    add codeforces/1375/C/85939877.cs

---
+++ codeforces/1375/C/85939877.cs
@@ -0,0 +1,30 @@
+// detail: https://codeforces.com/contest/1375/submission/85939877
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            Solve();
+        }
+    }
+    static void Solve()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine(a.First() < a.Last() ? "YES" : "NO");
+    }
+}
