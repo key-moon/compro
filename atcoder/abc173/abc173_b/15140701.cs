d349d8f75364ca4ae09faa093c9dbd80d12ff769
crawler <kymn0116+crawler@gmail.com>
2020-07-11 11:59:43 +0000

    add atcoder/abc173/abc173_b/15140701.cs

---
+++ atcoder/abc173/abc173_b/15140701.cs
@@ -0,0 +1,25 @@
+// detail: https://atcoder.jp/contests/abc173/submissions/15140701
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        var a = Enumerable.Repeat(0, n).Select(_ => Console.ReadLine()).ToArray();
+        string[] h = new[] { "AC", "WA", "TLE", "RE" };
+
+        Console.WriteLine(string.Join("\n", h.Select(x => $"{x} x {a.Count(y => y == x)}")));
+    }
+}
