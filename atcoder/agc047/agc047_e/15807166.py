1e81e042b745395e43b554761919407529db861f
crawler <kymn0116+crawler@gmail.com>
2020-08-10 10:00:54 +0000

    add atcoder/agc047/agc047_e/15807166.py

---
+++ atcoder/agc047/agc047_e/15807166.py
@@ -0,0 +1,10 @@
+# detail: https://atcoder.jp/contests/agc047/submissions/15807166
+r=range
+def a(i,j,k,l="+"):print(l,i,j,k)
+print(6484)
+a(0,1,3)
+a(2,3,3,L:="<")
+for x in 0,1:
+	a(x,3,x)
+	for t in r(30):a(v:=x+4,v,v);a(v,3,6),exec('a(6,6,6);'*(29-t));a(6,x,T:=x*60+35+t,L);a(v,T,v)
+for t in r(60):a(2,2,2);[(a(34+t-s,95+s,5),a(3,5,6,L),a(2,6,2))for s in r(t)]
