f2eb131d12730433a1f0b6b9f41051e3643f0d65
crawler <kymn0116+crawler@gmail.com>
2020-08-12 10:35:14 +0000

    add codeforces/804/A/89642613.cs

---
+++ codeforces/804/A/89642613.cs
@@ -0,0 +1,21 @@
+// detail: https://codeforces.com/contest/804/submission/89642613
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n / 2 - 1 + n % 2);
+    }
+}
