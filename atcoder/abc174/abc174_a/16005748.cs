736d6a97a7341066b63f815ab79c0c273fb42cd1
crawler <kymn0116+crawler@gmail.com>
2020-08-17 12:06:16 +0000

    add atcoder/abc174/abc174_a/16005748.cs

---
+++ atcoder/abc174/abc174_a/16005748.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/abc174/submissions/16005748
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(int.Parse(Console.ReadLine()) < 30 ? "No" : "Yes");
+    }
+}
