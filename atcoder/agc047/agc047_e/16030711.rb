7093e146e079302720e1ece37ea9b88ae42ee98a
crawler <kymn0116+crawler@gmail.com>
2020-08-18 19:57:41 +0000

    add atcoder/agc047/agc047_e/16030711.rb

---
+++ atcoder/agc047/agc047_e/16030711.rb
@@ -0,0 +1,4 @@
+# detail: https://atcoder.jp/contests/agc047/submissions/16030711
+#!ruby -Knrzlib
+eval Zlib.inflate'xڭ��
+1Dţ�i�[X�C�? PPOn��Dda����03/��z���^f�	���VR�{WK���QuG�8����?���������}��Ɋ�ZC����!,���bRv!��.Jq���i�g�1�V�F�'
