76786f704ffd4e64ffca685e62664849b1e1025e
crawler <kymn0116+crawler@gmail.com>
2020-08-19 23:01:44 +0000

    add aoj/ALDS1_4_A/4774921.cs

---
+++ aoj/ALDS1_4_A/4774921.cs
@@ -0,0 +1,23 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/ALDS1_4_A/judge/4774921/C#
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var s = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var q = int.Parse(Console.ReadLine());
+        var t = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(s.Intersect(t).Count());
+    }
+}
