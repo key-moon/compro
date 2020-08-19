e92c16f18529a4caec18ffd1163502319c573642
crawler <kymn0116+crawler@gmail.com>
2020-08-19 10:02:40 +0000

    add aoj/ALDS1_1_B/4774088.cs

---
+++ aoj/ALDS1_1_B/4774088.cs
@@ -0,0 +1,31 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/ALDS1_1_B/judge/4774088/C#
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(GCD(ab[0], ab[1]));
+    }
+
+    static long GCD(long a, long b)
+    {
+        while (true)
+        {
+            if (b == 0) return a;
+            a %= b;
+            if (a == 0) return b;
+            b %= a;
+        }
+    }
+}
