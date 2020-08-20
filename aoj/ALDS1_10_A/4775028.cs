64c60f1d20f12c1ed612319bdc849f7c67489e03
crawler <kymn0116+crawler@gmail.com>
2020-08-20 00:55:29 +0000

    add aoj/ALDS1_10_A/4775028.cs

---
+++ aoj/ALDS1_10_A/4775028.cs
@@ -0,0 +1,24 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/ALDS1_10_A/judge/4775028/C#
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long[] fib = new long[n + 1];
+        fib[0] = 1;
+        if (n != 0) fib[1] = 1;
+        for (int i = 2; i < fib.Length; i++) fib[i] = fib[i - 2] + fib[i - 1];
+        Console.WriteLine(fib.Last());
+    }
+}
