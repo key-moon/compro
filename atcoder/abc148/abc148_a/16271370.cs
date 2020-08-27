ddc4b5d31366e3059ed0f3fda1259baa91704909
crawler <kymn0116+crawler@gmail.com>
2020-08-27 23:30:34 +0000

    add atcoder/abc148/abc148_a/16271370.cs

---
+++ atcoder/abc148/abc148_a/16271370.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc148/submissions/16271370
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = int.Parse(Console.ReadLine());
+        Console.WriteLine(6 - n - a);
+    }
+}
