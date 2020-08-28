07282dfeda6faee01331a50e1d3adf6e290ddc2a
crawler <kymn0116+crawler@gmail.com>
2020-08-28 09:19:11 +0000

    add atcoder/abc113/abc113_d/16279585.cs

---
+++ atcoder/abc113/abc113_d/16279585.cs
@@ -0,0 +1,86 @@
+// detail: https://atcoder.jp/contests/abc113/submissions/16279585
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var hwk = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var h = hwk[0];
+        var w = hwk[1];
+        var k = hwk[2];
+        ModInt[] place = new ModInt[w];
+        place[0] = 1;
+        for (int count = 0; count < h; count++)
+        {
+            ModInt[] nextPlace = new ModInt[w];
+            for (int i = 0; i < (1 << w - 1); i++)
+            {
+                bool prev = false;
+                ModInt[] next = place.ToArray();
+                for (int j = 0; j < w - 1; j++)
+                {
+                    if ((i >> j & 1) == 0)
+                    {
+                        prev = false;
+                        continue;
+                    }
+                    if (prev) goto end;
+                    prev = true;
+                    var tmp = next[j];
+                    next[j] = next[j + 1];
+                    next[j + 1] = tmp;
+                }
+                for (int j = 0; j < w; j++)
+                    nextPlace[j] += next[j];
+                end:;
+            }
+            place = nextPlace;
+        }
+        Console.WriteLine(place[k - 1]);
+    }
+}
+
+
+struct ModInt
+{
+    public const int Mod = 1000000007;
+    const long POSITIVIZER = ((long)Mod) << 31;
+    long Data;
+    public ModInt(long data) { if ((Data = data % Mod) < 0) Data += Mod; }
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % Mod };
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= Mod ? res - Mod : res }; }
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % Mod };
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + Mod : res }; }
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % Mod };
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % Mod };
+    public static bool operator ==(ModInt a, ModInt b) => a.Data == b.Data;
+    public static bool operator !=(ModInt a, ModInt b) => a.Data != b.Data;
+    public override string ToString() => Data.ToString();
+    public override bool Equals(object obj) => (ModInt)obj == this;
+    public override int GetHashCode() => (int)Data;
+    static long GetInverse(long a)
+    {
+        long div, p = Mod, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + Mod; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + Mod; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
