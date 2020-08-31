e23993ee3ecec6301636b5ca0d8c4a05f760c9d8
crawler <kymn0116+crawler@gmail.com>
2020-08-31 12:53:10 +0000

    add atcoder/m-solutions2020/m_solutions2020_a/16426436.cs

---
+++ atcoder/m-solutions2020/m_solutions2020_a/16426436.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/m-solutions2020/submissions/16426436
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(10 - int.Parse(Console.ReadLine()) / 200);
+    }
+}
