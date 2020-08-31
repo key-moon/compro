188c5f6684b17976cda9dc401f8451af9b5f63fa
crawler <kymn0116+crawler@gmail.com>
2020-08-31 12:46:12 +0000

    add atcoder/code-festival-2015-qualb/codefestival_2015_qualB_a/16426217.cs

---
+++ atcoder/code-festival-2015-qualb/codefestival_2015_qualB_a/16426217.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/code-festival-2015-qualb/submissions/16426217
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var s = Console.ReadLine();
+        Console.WriteLine(s + s);
+    }
+}
