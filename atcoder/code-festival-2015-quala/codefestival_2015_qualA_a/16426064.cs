81c7197e88891071b8c6a25f1bc8be7ba4e5ec84
crawler <kymn0116+crawler@gmail.com>
2020-08-31 12:40:17 +0000

    add atcoder/code-festival-2015-quala/codefestival_2015_qualA_a/16426064.cs

---
+++ atcoder/code-festival-2015-quala/codefestival_2015_qualA_a/16426064.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/code-festival-2015-quala/submissions/16426064
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Replace("2014", "2015"));
+    }
+}
