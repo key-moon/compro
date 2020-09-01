5a91b602563a9765935515f3c80a278eb6e5038d
crawler <kymn0116+crawler@gmail.com>
2020-09-01 00:44:32 +0000

    add atcoder/code-festival-2014-final/code_festival_final_a/16434342.cs

---
+++ atcoder/code-festival-2014-final/code_festival_final_a/16434342.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/code-festival-2014-final/submissions/16434342
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(50.0 / int.Parse(Console.ReadLine()));
+    }
+}
