449c21459f5831e88b1426354df65db83f5e33c0
crawler <kymn0116+crawler@gmail.com>
2020-09-06 15:02:21 +0000

    add codeforces/1404/B/92042456.cs

---
+++ codeforces/1404/B/92042456.cs
@@ -0,0 +1,187 @@
+// detail: https://codeforces.com/contest/1404/submission/92042456
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            Solve();
+        }
+        Console.Out.Flush();
+    }
+    static void Solve()
+    {
+        var nabd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nabd[0];
+        var a = nabd[1] - 1;
+        var b = nabd[2] - 1;
+        var da = nabd[3];
+        var db = nabd[4];
+
+        var edges = Enumerable.Repeat(0, n - 1).Select(_ => Console.ReadLine().Split().Select(x => int.Parse(x) - 1).ToArray()).ToArray();
+        ReRooting<int> rerooting = new ReRooting<int>(n, edges, 0, Max, (x, _) => x + 1);
+        
+        int[] distance = Enumerable.Repeat(-1, n).ToArray();
+        Stack<int> stack = new Stack<int>();
+        stack.Push(a);
+        distance[a] = 0;
+        while (stack.Count != 0)
+        {
+            var parent = stack.Pop();
+            foreach (var adj in rerooting.Adjacents[parent])
+            {
+                if (distance[adj] != -1) continue;
+                distance[adj] = distance[parent] + 1;
+                stack.Push(adj);
+            }
+        }
+
+        var d = Enumerable.Range(0, n).Select(rerooting.Query).Max() - 1;
+        if (db <= da * 2 || d <= da * 2 || distance[b] <= da)
+        {
+            Console.WriteLine("Alice");
+        }
+        else
+        {
+            Console.WriteLine("Bob");
+        }
+    }
+}
+
+
+class ReRooting<T>
+{
+    public int NodeCount { get; private set; }
+
+    public int[][] Adjacents;
+    int[][] IndexForAdjacent;
+
+    T[] Res;
+    T[][] DP;
+
+    T Identity;
+    Func<T, T, T> Operate;
+    Func<T, int, T> OperateNode;
+
+    public ReRooting(int nodeCount, int[][] edges, T identity, Func<T, T, T> operate, Func<T, int, T> operateNode)
+    {
+        NodeCount = nodeCount;
+
+        Identity = identity;
+        Operate = operate;
+        OperateNode = operateNode;
+
+        List<int>[] adjacents = new List<int>[nodeCount];
+        List<int>[] indexForAdjacents = new List<int>[nodeCount];
+
+        for (int i = 0; i < nodeCount; i++)
+        {
+            adjacents[i] = new List<int>();
+            indexForAdjacents[i] = new List<int>();
+        }
+        for (int i = 0; i < edges.Length; i++)
+        {
+            var edge = edges[i];
+            indexForAdjacents[edge[0]].Add(adjacents[edge[1]].Count);
+            indexForAdjacents[edge[1]].Add(adjacents[edge[0]].Count);
+            adjacents[edge[0]].Add(edge[1]);
+            adjacents[edge[1]].Add(edge[0]);
+        }
+
+        Adjacents = new int[nodeCount][];
+        IndexForAdjacent = new int[nodeCount][];
+        for (int i = 0; i < nodeCount; i++)
+        {
+            Adjacents[i] = adjacents[i].ToArray();
+            IndexForAdjacent[i] = indexForAdjacents[i].ToArray();
+        }
+
+        DP = new T[Adjacents.Length][];
+        Res = new T[Adjacents.Length];
+
+        for (int i = 0; i < Adjacents.Length; i++) DP[i] = new T[Adjacents[i].Length];
+        if (NodeCount > 1) Initialize();
+        else if (NodeCount == 1) Res[0] = OperateNode(Identity, 0);
+    }
+
+    public T Query(int node) => Res[node];
+
+    private void Initialize()
+    {
+        int[] parents = new int[NodeCount];
+        int[] order = new int[NodeCount];
+
+        #region InitOrderedTree
+        var index = 0;
+        Stack<int> stack = new Stack<int>();
+        stack.Push(0);
+        parents[0] = -1;
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+            order[index++] = node;
+            for (int i = 0; i < Adjacents[node].Length; i++)
+            {
+                var adjacent = Adjacents[node][i];
+                if (adjacent == parents[node]) continue;
+                stack.Push(adjacent);
+                parents[adjacent] = node;
+            }
+        }
+        #endregion
+
+        #region fromLeaf
+        for (int i = order.Length - 1; i >= 1; i--)
+        {
+            var node = order[i];
+            var parent = parents[node];
+
+            T accum = Identity;
+            int parentIndex = -1;
+            for (int j = 0; j < Adjacents[node].Length; j++)
+            {
+                if (Adjacents[node][j] == parent)
+                {
+                    parentIndex = j;
+                    continue;
+                }
+                accum = Operate(accum, DP[node][j]);
+            }
+            DP[parent][IndexForAdjacent[node][parentIndex]] = OperateNode(accum, node);
+        }
+        #endregion
+
+        #region toLeaf
+        for (int i = 0; i < order.Length; i++)
+        {
+            var node = order[i];
+            T accum = Identity;
+            T[] accumsFromTail = new T[Adjacents[node].Length];
+            accumsFromTail[accumsFromTail.Length - 1] = Identity;
+            for (int j = accumsFromTail.Length - 1; j >= 1; j--) accumsFromTail[j - 1] = Operate(DP[node][j], accumsFromTail[j]);
+            for (int j = 0; j < accumsFromTail.Length; j++)
+            {
+                DP[Adjacents[node][j]][IndexForAdjacent[node][j]] = OperateNode(Operate(accum, accumsFromTail[j]), node);
+                accum = Operate(accum, DP[node][j]);
+            }
+            Res[node] = OperateNode(accum, node);
+        }
+        #endregion
+    }
+}
