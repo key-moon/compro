067b156defc753e2dc592131e2dc21ab7b81fe30
crawler <kymn0116+crawler@gmail.com>
2020-09-07 22:49:20 +0000

    add atcoder/practice2/practice2_a/16568382.cs

---
+++ atcoder/practice2/practice2_a/16568382.cs
@@ -0,0 +1,156 @@
+// detail: https://atcoder.jp/contests/practice2/submissions/16568382
+using AtCoder;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Reader;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+        int n = NextInt;
+        int q = NextInt;
+        DSU dsu = new DSU(n);
+        for (int i = 0; i < q; i++)
+        {
+            var t = NextInt;
+            var u = NextInt;
+            var v = NextInt;
+            if (t == 0) dsu.Merge(u, v);
+            if (t == 1) Console.WriteLine(dsu.Same(u, v) ? "1" : "0");
+        }
+        Console.Out.Flush();
+    }
+}
+
+
+static class Reader
+{
+    const int BUF_SIZE = 1 << 12;
+    static Stream Stream = Console.OpenStandardInput();
+    static byte[] Buffer = new byte[BUF_SIZE];
+    static int ptr = BUF_SIZE - 1;
+    static void Move() { if (++ptr >= Buffer.Length) { Stream.Read(Buffer, 0, BUF_SIZE); ptr = 0; } }
+
+
+    public static int NextInt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            int res = 0; while (Buffer[ptr] < 48) Move();
+            do { res = res * 10 + (int)(Buffer[ptr] ^ 48); Move(); } while (48 <= Buffer[ptr]);
+            return res;
+        }
+    }
+}
+
+namespace AtCoder
+{
+    class DSU
+    {
+        public int Count;
+        private int[] ParentOrSize;
+
+        /// <summary>
+        /// <see cref="DSU"/> クラスの新しいインスタンスを、<paramref name="n"/> 頂点 0 辺の有向グラフとして初期化します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="n"/>≤10^8</para>
+        /// <para>計算量: O(<paramref name="n"/>)</para>
+        /// </remarks>
+        public DSU(int n)
+        {
+            Count = n;
+            ParentOrSize = new int[n];
+            for (int i = 0; i < ParentOrSize.Length; i++) ParentOrSize[i] = -1;
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> と頂点 <paramref name="b"/> を結ぶ辺を追加し、それらの代表元を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public int Merge(int a, int b)
+        {
+            Debug.Assert(0 <= a && a < Count);
+            Debug.Assert(0 <= b && b < Count);
+            int x = Leader(a), y = Leader(b);
+            if (x == y) return x;
+            if (-ParentOrSize[x] < ParentOrSize[y]) (x, y) = (y, x);
+            ParentOrSize[x] += ParentOrSize[y];
+            ParentOrSize[y] = x;
+            return x;
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/>, <paramref name="b"/> が連結かどうかを返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public bool Same(int a, int b)
+        {
+            Debug.Assert(0 <= a && a < Count);
+            Debug.Assert(0 <= b && b < Count);
+            return Leader(a) == Leader(b);
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> の属する連結成分の代表元を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public int Leader(int a)
+        {
+            while (true)
+            {
+                if (ParentOrSize[a] < 0) return a;
+                if (ParentOrSize[ParentOrSize[a]] < 0) return ParentOrSize[a];
+                a = (ParentOrSize[a] = ParentOrSize[ParentOrSize[a]]);
+            }
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> の属する連結成分のサイズを返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public int Size(int a)
+        {
+            Debug.Assert(0 <= a && a < Count);
+            return -ParentOrSize[Leader(a)];
+        }
+
+        /// <summary>
+        /// グラフを連結成分に分け、その情報を返します。
+        /// </summary>
+        /// <para>計算量: O(n)</para>
+        /// <returns>「一つの連結成分の頂点番号のリスト」のリスト。</returns>
+        public List<List<int>> Groups()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
