b0e7f4a5f31f49c34b6700385008ea001b670f18
crawler <kymn0116+crawler@gmail.com>
2020-10-10 12:05:14 +0000

    add atcoder/hhkb2020/hhkb2020_a/17293005.cs

---
+++ atcoder/hhkb2020/hhkb2020_a/17293005.cs
@@ -0,0 +1,27 @@
+// detail: https://atcoder.jp/contests/hhkb2020/submissions/17293005
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var s = Console.ReadLine();
+        if (s == "Y")
+        {
+            Console.WriteLine(Console.ReadLine().ToUpper());
+        }
+        else
+        {
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
