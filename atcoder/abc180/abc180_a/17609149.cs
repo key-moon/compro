a608a950db57ccf496d29d07959dc20048d7dab3
crawler <kymn0116+crawler@gmail.com>
2020-10-24 11:40:56 +0000

    add atcoder/abc180/abc180_a/17609149.cs

---
+++ atcoder/abc180/abc180_a/17609149.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc180/submissions/17609149
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var nab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(nab[0] - nab[1] + nab[2]);
+    }
+}
