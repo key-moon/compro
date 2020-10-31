e4d4b28cd6ad5739b842a0ddb6a7015966f5fb21
crawler <kymn0116+crawler@gmail.com>
2020-10-31 12:15:10 +0000

    add atcoder/arc107/arc107_c/17753067.cs

---
+++ atcoder/arc107/arc107_c/17753067.cs
@@ -0,0 +1,126 @@
+// detail: https://atcoder.jp/contests/arc107/submissions/17753067
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var nk = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nk[0];
+        int k = nk[1];
+        var mat = Enumerable.Repeat(0, n).Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+        UnionFind rowuf = new UnionFind(n);
+        UnionFind coluf = new UnionFind(n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (!Enumerable.Range(0, n).All(x => mat[i][x] + mat[j][x] <= k)) continue;
+                rowuf.TryUnite(i, j);
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (!Enumerable.Range(0, n).All(x => mat[x][i] + mat[x][j] <= k)) continue;
+                coluf.TryUnite(i, j);
+            }
+        }
+        var resrow = rowuf.AllRepresents.Select(x => Factorial(rowuf.GetSize(x))).Aggregate((ModInt)1, (x, y) => x * y);
+        var rescol = coluf.AllRepresents.Select(x => Factorial(coluf.GetSize(x))).Aggregate((ModInt)1, (x, y) => x * y);
+        Console.WriteLine(resrow * rescol);
+    }
+
+
+    static List<ModInt> factorialMemo = new List<ModInt>() { 1 };
+    static ModInt Factorial(int x)
+    {
+        for (int i = factorialMemo.Count; i <= x; i++) factorialMemo.Add(factorialMemo.Last() * i);
+        return factorialMemo[x];
+    }
+}
+
+
+struct ModInt
+{
+    public const int Mod = 998244353;
+    const long POSITIVIZER = ((long)Mod) << 31;
+    long Data;
+    public ModInt(long data) { if ((Data = data % Mod) < 0) Data += Mod; }
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % Mod };
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= Mod ? res - Mod : res }; }
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % Mod };
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + Mod : res }; }
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % Mod };
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % Mod };
+    public static bool operator ==(ModInt a, ModInt b) => a.Data == b.Data;
+    public static bool operator !=(ModInt a, ModInt b) => a.Data != b.Data;
+    public override string ToString() => Data.ToString();
+    public override bool Equals(object obj) => (ModInt)obj == this;
+    public override int GetHashCode() => (int)Data;
+    static long GetInverse(long a)
+    {
+        long div, p = Mod, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + Mod; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + Mod; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
+
+
+class UnionFind
+{
+    public int Size { get; private set; }
+    public int GroupCount { get; private set; }
+    public IEnumerable<int> AllRepresents => Parent.Where((x, y) => x == y);
+    int[] Sizes;
+    int[] Parent;
+    public UnionFind(int count)
+    {
+        Size = count;
+        GroupCount = count;
+        Parent = new int[count];
+        Sizes = new int[count];
+        for (int i = 0; i < count; i++) Sizes[Parent[i] = i] = 1;
+    }
+    public bool TryUnite(int x, int y)
+    {
+        int xp = Find(x);
+        int yp = Find(y);
+        if (yp == xp) return false;
+        if (Sizes[xp] < Sizes[yp]) { var tmp = xp; xp = yp; yp = tmp; }
+        GroupCount--;
+        Parent[yp] = xp;
+        Sizes[xp] += Sizes[yp];
+        return true;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int GetSize(int x) => Sizes[Find(x)];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Find(int x)
+    {
+        while (x != Parent[x]) x = (Parent[x] = Parent[Parent[x]]);
+        return x;
+    }
+}
