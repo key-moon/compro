8b0414ad877368ad283c3c99558e794b88ca5af5
crawler <kymn0116+crawler@gmail.com>
2020-11-01 13:05:24 +0000

    add atcoder/abc181/abc181_c/17809978.cs

---
+++ atcoder/abc181/abc181_c/17809978.cs
@@ -0,0 +1,74 @@
+// detail: https://atcoder.jp/contests/abc181/submissions/17809978
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var p = Enumerable.Repeat(0, n).Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).Select(x => new Vector(x[0], x[1])).ToArray();
+        bool b = false;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    var v1 = (p[i] - p[j]);
+                    var v2 = (p[i] - p[k]);
+                    b |= v1.x * v2.y == v1.y * v2.x;
+                }
+            }
+        }
+        Console.WriteLine(b ? "Yes" : "No");
+    }
+}
+
+
+struct Vector
+{
+    public int x;
+    public int y;
+    public int this[int index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get { if (index == 0) return x; else return y; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set { if (index == 0) x = value; else y = value; }
+    }
+    public int Length => x * x + y * y;
+    public double SqrtLength => Math.Sqrt(Length);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector(int x, int y) { this.x = x; this.y = y; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector RotateVector(double radian) => new Vector((int)(x * Math.Cos(radian) - y * Math.Sin(radian)), (int)(x * Math.Sin(radian) + y * Math.Cos(radian)));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public int CrossProduct(Vector a, Vector b) => a.x * b.y - a.y * b.x;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public int InnerProduct(Vector a, Vector b) => a.x * b.x + a.y * b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Vector a, Vector b) => a.x == b.x && a.y == b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Vector a, Vector b) => a.x != b.x || a.y != b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object obj) => this == (Vector)obj;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => x.GetHashCode() * 1000000007 + y.GetHashCode();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => $"({x},{y})";
+}
