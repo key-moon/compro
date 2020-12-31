924a722573fc7817957f91677e2e40717d9de356
crawler <kymn0116+crawler@gmail.com>
2020-12-31 14:05:19 +0000

    add yukicoder/3010/598704.cs

---
+++ yukicoder/3010/598704.cs
@@ -0,0 +1,20 @@
+// detail: https://yukicoder.me/submissions/598704
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(string.Join("\n", Directory.EnumerateFileSystemEntries("/bin").Select(Path.GetFileName).OrderBy(x => x, StringComparer.Ordinal)));
+    }
+}
