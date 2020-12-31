2cae658dfed845363642f5823e528963436ed2ef
crawler <kymn0116+crawler@gmail.com>
2020-12-31 13:33:56 +0000

    add yukicoder/5000/598687.cs

---
+++ yukicoder/5000/598687.cs
@@ -0,0 +1,2 @@
+// detail: https://yukicoder.me/submissions/598687
+using System;public static class P{public static void Main(){int n = int.Parse(Console.ReadLine());Console.WriteLine($"{n-1} 1");}}
