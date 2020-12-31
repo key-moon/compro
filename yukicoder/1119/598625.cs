3044f3d3507f0c59feaea3d8e1535f30b9405a8e
crawler <kymn0116+crawler@gmail.com>
2020-12-31 10:57:45 +0000

    add yukicoder/1119/598625.cs

---
+++ yukicoder/1119/598625.cs
@@ -0,0 +1,20 @@
+// detail: https://yukicoder.me/submissions/598625
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(a.Any(x => x % 3 == 0) ? "Yes" : "No");
+    }
+}
