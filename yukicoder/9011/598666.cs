628c13eaeb616289b29fe164993cb25f7199f6e7
crawler <kymn0116+crawler@gmail.com>
2020-12-31 13:03:56 +0000

    add yukicoder/9011/598666.cs

---
+++ yukicoder/9011/598666.cs
@@ -0,0 +1,10 @@
+// detail: https://yukicoder.me/submissions/598666
+using System;
+using System.Linq;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Sum(x => x - '0'));
+    }
+}
