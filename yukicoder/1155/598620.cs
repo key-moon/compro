2c4c896409f5daf10acdb7a79787e0f414a0a1e5
crawler <kymn0116+crawler@gmail.com>
2020-12-31 10:45:47 +0000

    add yukicoder/1155/598620.cs

---
+++ yukicoder/1155/598620.cs
@@ -0,0 +1,22 @@
+// detail: https://yukicoder.me/submissions/598620
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        if (n == 1) Console.WriteLine("Shiitakerando");
+        if (n == 2) Console.WriteLine("Otsukakokusaibijutsukan");
+        if (n == 3) Console.WriteLine("Spring-8");
+    }
+}
