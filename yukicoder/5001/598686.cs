785553529a4cc761885cca464f16163cc6c48e70
crawler <kymn0116+crawler@gmail.com>
2020-12-31 13:31:59 +0000

    add yukicoder/5001/598686.cs

---
+++ yukicoder/5001/598686.cs
@@ -0,0 +1,24 @@
+// detail: https://yukicoder.me/submissions/598686
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nm[0];
+        var m = nm[1];
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(string.Join(" ", a.Take(m)));
+    }
+}
