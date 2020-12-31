b15443173520bc18f569348b54e2e1dfecc8351a
crawler <kymn0116+crawler@gmail.com>
2020-12-31 13:54:48 +0000

    add yukicoder/3004/598698.cs

---
+++ yukicoder/3004/598698.cs
@@ -0,0 +1,25 @@
+// detail: https://yukicoder.me/submissions/598698
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            var n = int.Parse(Console.ReadLine());
+            if (n == 0) break;
+            Console.WriteLine("yukicoder"[n - 1]);
+        }
+    }
+}
