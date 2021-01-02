db1953dcf8e7030be681baa837f4564e4234c321
crawler <kymn0116+crawler@gmail.com>
2021-01-02 12:28:20 +0000

    add atcoder/abc187/abc187_a/19137068.cs

---
+++ atcoder/abc187/abc187_a/19137068.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/abc187/submissions/19137068
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split().Max(x => x.Sum(x => x - '0')));
+    }
+}
