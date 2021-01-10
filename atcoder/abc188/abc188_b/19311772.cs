d65f2cfe971d2779341ae20cdff01fb2c75cf3af
crawler <kymn0116+crawler@gmail.com>
2021-01-10 12:00:32 +0000

    add atcoder/abc188/abc188_b/19311772.cs

---
+++ atcoder/abc188/abc188_b/19311772.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/abc188/submissions/19311772
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var b = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(a.Zip(b, (x, y) => x * y).Sum() == 0 ? "Yes" : "No");
+    }
+}
