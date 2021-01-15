b6c8eef82ec312e9e15c12464530de036331da1b
crawler <kymn0116+crawler@gmail.com>
2021-01-15 10:10:53 +0000

    add yukicoder/994/603462.cs

---
+++ yukicoder/994/603462.cs
@@ -0,0 +1,22 @@
+// detail: https://yukicoder.me/submissions/603462
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var nk = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nk[0];
+        var k = nk[1];
+        Console.WriteLine(n < k ? -1 : k - 1);
+    }
+}
