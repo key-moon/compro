e4e7c86acc202a82903118871ee316b2f53b7604
crawler <kymn0116+crawler@gmail.com>
2021-01-16 15:17:16 +0000

    add yukicoder/1342/606107.cs

---
+++ yukicoder/1342/606107.cs
@@ -0,0 +1,19 @@
+// detail: https://yukicoder.me/submissions/606107
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine((19 * 43 + 28 * 47) / 27);
+    }
+}
