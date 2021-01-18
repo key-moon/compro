ad85ee2ff89256c6801dae6feb0e37afe7e76e74
crawler <kymn0116+crawler@gmail.com>
2021-01-18 10:34:17 +0000

    add codeforces/710/B/104675038.cs

---
+++ codeforces/710/B/104675038.cs
@@ -0,0 +1,22 @@
+// detail: https://codeforces.com/contest/710/submission/104675038
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+        Console.WriteLine(a[(n - 1) / 2]);
+    }
+}
