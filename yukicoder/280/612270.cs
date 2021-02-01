7064e9c799007046aa2680f546c90d048aba1809
crawler <kymn0116+crawler@gmail.com>
2021-02-01 13:58:54 +0000

    add yukicoder/280/612270.cs

---
+++ yukicoder/280/612270.cs
@@ -0,0 +1,21 @@
+// detail: https://yukicoder.me/submissions/612270
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine($"{a.Last()}/{a.First()}");
+    }
+}
