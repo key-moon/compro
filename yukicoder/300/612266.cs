b52cce0ed6ab85371192b259c3412b5433ef6c50
crawler <kymn0116+crawler@gmail.com>
2021-02-01 13:44:50 +0000

    add yukicoder/300/612266.cs

---
+++ yukicoder/300/612266.cs
@@ -0,0 +1,38 @@
+// detail: https://yukicoder.me/submissions/612266
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        long x = long.Parse(Console.ReadLine());
+        Console.WriteLine(PrimeFactors(x).GroupBy(x => x).Where(x => x.Count() % 2 == 1).Aggregate(1L, (x, y) => x * y.Key));
+    }
+
+    static IEnumerable<long> PrimeFactors(long n)
+    {
+        while ((n & 1) == 0)
+        {
+            n >>= 1;
+            yield return 2;
+        }
+        for (long i = 3; i * i <= n; i += 2)
+        {
+            while (n % i == 0)
+            {
+                n /= i;
+                yield return i;
+            }
+        }
+        if (n != 1) yield return n;
+    }
+}
