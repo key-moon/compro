51b0ce2497a9181b6167ebbf38a54359a527512e
key-moon <kymn0116@gmail.com>
2021-02-07 07:18:25 +0900

    add compro/misc/quora/2021-div1/students/submission.cpp

---
+++ misc/quora/2021-div1/students/submission.cpp
@@ -0,0 +1,211 @@
+#include <bits/stdc++.h>
+#define var auto
+using ll = long long;
+const char newl = '\n';
+template <typename T1, typename T2> inline void chmin(T1& a, T2 b) {if(a > b) a = b;}
+template <typename T1, typename T2> inline void chmax(T1& a, T2 b) {if(a < b) a = b;}
+
+using namespace std;
+
+namespace atcoder {
+
+namespace internal {
+
+template <class T> struct simple_queue {
+    std::vector<T> payload;
+    int pos = 0;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()) - pos; }
+    bool empty() const { return pos == int(payload.size()); }
+    void push(const T& t) { payload.push_back(t); }
+    T& front() { return payload[pos]; }
+    void clear() {
+        payload.clear();
+        pos = 0;
+    }
+    void pop() { pos++; }
+};
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+namespace atcoder {
+
+template <class Cap> struct mf_graph {
+  public:
+    mf_graph() : _n(0) {}
+    explicit mf_graph(int n) : _n(n), g(n) {}
+
+    int add_edge(int from, int to, Cap cap) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = int(pos.size());
+        pos.push_back({from, int(g[from].size())});
+        int from_id = int(g[from].size());
+        int to_id = int(g[to].size());
+        if (from == to) to_id++;
+        g[from].push_back(_edge{to, to_id, cap});
+        g[to].push_back(_edge{from, from_id, 0});
+        return m;
+    }
+
+    struct edge {
+        int from, to;
+        Cap cap, flow;
+    };
+
+    edge get_edge(int i) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i].first][pos[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return edge{pos[i].first, _e.to, _e.cap + _re.cap, _re.cap};
+    }
+    std::vector<edge> edges() {
+        int m = int(pos.size());
+        std::vector<edge> result;
+        for (int i = 0; i < m; i++) {
+            result.push_back(get_edge(i));
+        }
+        return result;
+    }
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto& _e = g[pos[i].first][pos[i].second];
+        auto& _re = g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) {
+        return flow(s, t, std::numeric_limits<Cap>::max());
+    }
+    Cap flow(int s, int t, Cap flow_limit) {
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+
+        std::vector<int> level(_n), iter(_n);
+        internal::simple_queue<int> que;
+
+        auto bfs = [&]() {
+            std::fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            que.clear();
+            que.push(s);
+            while (!que.empty()) {
+                int v = que.front();
+                que.pop();
+                for (auto e : g[v]) {
+                    if (e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t) return;
+                    que.push(e.to);
+                }
+            }
+        };
+        auto dfs = [&](auto self, int v, Cap up) {
+            if (v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int& i = iter[v]; i < int(g[v].size()); i++) {
+                _edge& e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d =
+                    self(self, e.to, std::min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up) return res;
+            }
+            level[v] = _n;
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            std::fill(iter.begin(), iter.end(), 0);
+            Cap f = dfs(dfs, t, flow_limit - flow);
+            if (!f) break;
+            flow += f;
+        }
+        return flow;
+    }
+
+    std::vector<bool> min_cut(int s) {
+        std::vector<bool> visited(_n);
+        internal::simple_queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int p = que.front();
+            que.pop();
+            visited[p] = true;
+            for (auto e : g[p]) {
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+  private:
+    int _n;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+    };
+    std::vector<std::pair<int, int>> pos;
+    std::vector<std::vector<_edge>> g;
+};
+
+}  // namespace atcoder
+
+
+int main() {
+  ll n, m;
+  cin >> n >> m;
+  var code = [&](ll y, ll x){
+    return x * n + y;
+  };
+  map<int, int> compressed{};
+  var get_id = [&](ll x){
+    if (!compressed.count(x)){
+      var newid = compressed.size();
+      compressed[x] = newid;
+    }
+    return compressed[x];
+  };
+  using P = pair<int, int>;
+  using PP = pair<P, P>;
+  vector<P> friends{};
+  for (int i = 0; i < m; i++){
+    int y1, x1, y2, x2;
+    cin >> y1 >> x1 >> y2 >> x2;
+    y1--; x1--; y2--; x2--;
+    if ((y1 + x1) % 2 != 0){
+      swap(y1, y2);
+      swap(x1, x2);
+    }
+    friends.emplace_back(get_id(code(y1, x1)), get_id(code(y2, x2)));
+  }
+  
+  atcoder::mf_graph<int> flow(1 + compressed.size() + 1);
+  var sink = 1 + compressed.size();
+  for (var&& [key, value] : compressed){
+    if (((key % n) + (key / n)) % 2 == 0) flow.add_edge(0, 1 + value, 1);
+    else flow.add_edge(1 + value, sink, 1);
+  }
+  for (var&& [a, b] : friends){
+    flow.add_edge(1 + a, 1 + b, n * n);
+  }
+  cout << n * n - flow.flow(0, sink, n * n) << endl;
+}
