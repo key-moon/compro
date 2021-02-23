b057f59e4875e80d0d8ab6af01f00a25c69db751
crawler <kymn0116+crawler@gmail.com>
2021-02-23 07:15:08 +0000

    add atcoder/abc049/abc049_b/20438044.cpp

---
+++ atcoder/abc049/abc049_b/20438044.cpp
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc049/submissions/20438044
+#include<bits/stdc++.h>
+using ll = long long;
+#define var auto
+const char newl = '\n';
+
+using namespace std;
+
+template<typename T1, typename T2> inline void chmin(T1& a, T2 b) { if (a > b) a = b; }
+template<typename T1, typename T2> inline void chmax(T1& a, T2 b) { if (a < b) a = b; }
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  for (int i = 0; i < h; i++) {
+    string s;
+    cin >> s;
+    cout << s << endl << s << endl;
+  }
+}
