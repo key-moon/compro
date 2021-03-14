2ea47e26a4f34db0055ca0562e1b096f5f1967c6
crawler <kymn0116+crawler@gmail.com>
2021-03-14 14:00:55 +0000

    add atcoder/arc114/arc114_a/20946847.cs

---
+++ atcoder/arc114/arc114_a/20946847.cs
@@ -0,0 +1,85 @@
+// detail: https://atcoder.jp/contests/arc114/submissions/20946847
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        long min = long.MaxValue;
+        var prime = Primes(50).ToArray();
+        for (int b = 0; b < (1 << prime.Length); b++)
+        {
+            long num = 1;
+            for (int i = 0; i < prime.Length; i++)
+            {
+                if ((b >> i & 1) != 1) continue;
+                num *= prime[i];
+            }
+            if (num < min && a.All(x => GCD(num, x) != 1)) min = num;
+        }
+        Console.WriteLine(min);
+    }
+
+    static long GCD(long a, long b)
+    {
+        while (true)
+        {
+            if (b == 0) return a;
+            a %= b;
+            if (a == 0) return b;
+            b %= a;
+        }
+    }
+
+    public static IEnumerable<int> Primes(int n)
+    {
+        if (n < 2) yield break;
+        yield return 2;
+        ulong[] bitArray = new ulong[(n + 1) / 2 / 64 + 1];
+
+        int[] smallPrimes = { 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61 };
+        foreach (var prime in smallPrimes)
+        {
+            if (n < prime) yield break;
+            yield return prime;
+
+            ulong[] mask = new ulong[prime];
+            for (int j = (prime - 3) >> 1; j < (prime << 6); j += prime)
+                mask[j >> 6] |= 1UL << j;
+
+            int maskInd = 0;
+            for (int i = 0; i < bitArray.Length; i++)
+            {
+                bitArray[i] |= mask[maskInd];
+                if (++maskInd >= prime) maskInd = 0;
+            }
+        }
+
+        int[] deBruijnIndex = { 0, 1, 59, 2, 60, 40, 54, 3, 61, 32, 49, 41, 55, 19, 35, 4, 62, 52, 30, 33, 50, 12, 14, 42, 56, 16, 27, 20, 36, 23, 44, 5, 63, 58, 39, 53, 31, 48, 18, 34, 51, 29, 11, 13, 15, 26, 22, 43, 57, 38, 47, 17, 28, 10, 25, 21, 37, 46, 9, 24, 45, 8, 7, 6 };
+        int maxInd = n / 2;
+        for (int i = 0; i < bitArray.Length; i++)
+        {
+            for (ulong bit = ~bitArray[i]; bit != 0; bit &= bit - 1)
+            {
+                int index = i << 6 | deBruijnIndex[((bit & (~bit + 1)) * 0x03F566ED27179461UL) >> 58];
+                int prime = (index << 1) + 3;
+                if (n < prime) yield break;
+                yield return prime;
+                for (int ind = index; ind <= maxInd; ind += prime)
+                    bitArray[ind >> 6] |= 1UL << ind;
+            }
+        }
+    }
+}
