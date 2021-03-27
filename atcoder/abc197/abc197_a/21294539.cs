d74b3a54c8a9aaf46bdc889d8babadd921f9a9b0
crawler <kymn0116+crawler@gmail.com>
2021-03-27 12:03:21 +0000

    add atcoder/abc197/abc197_a/21294539.cs

---
+++ atcoder/abc197/abc197_a/21294539.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc197/submissions/21294539
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s[1..] + s[..1]);
+    }
+}
