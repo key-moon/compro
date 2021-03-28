51595e73e1f044fbd40ca5357eb2df44b8ec3585
crawler <kymn0116+crawler@gmail.com>
2021-03-28 12:22:54 +0000

    add atcoder/arc116/arc116_c/21354501.cs

---
+++ atcoder/arc116/arc116_c/21354501.cs
@@ -0,0 +1,91 @@
+// detail: https://atcoder.jp/contests/arc116/submissions/21354501
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nm[0];
+        var m = nm[1];
+        //最後a長さnが何通りある
+        int maxLen = 18;
+        ModInt[][] dp = Enumerable.Repeat(0, maxLen + 1).Select(_ => new ModInt[m + 1]).ToArray();
+        Array.Fill(dp[1], 1, 1, m);
+        {
+            for (int j = 2; j <= m; j++)
+            {
+                for (int k = 1; k * k <= j; k++)
+                {
+                    if (j % k != 0) continue;
+                    for (int i = 2; i <= maxLen; i++)
+                    {
+                        dp[i][j] += dp[i - 1][k];
+                        if (k != 1 && k * k != j) dp[i][j] += dp[i - 1][j / k];
+                    }
+                }
+            }
+        }
+        ModInt res = 0;
+        for (int len = 1; len <= maxLen; len++)
+        {
+            if (n < len) continue;
+            //n-len個をlen個に分ける
+            //n-lenとlen-1個のしきり
+            var total = dp[len].Aggregate((x, y) => x + y);
+            var comb = Factorial(n - 1) / Factorial(n - len) / Factorial(len - 1);
+            res += total * comb;
+        }
+        Console.WriteLine(res);
+    }
+
+    static List<ModInt> factorialMemo = new List<ModInt>() { 1 };
+    static ModInt Factorial(int x)
+    {
+        for (int i = factorialMemo.Count; i <= x; i++) factorialMemo.Add(factorialMemo.Last() * i);
+        return factorialMemo[x];
+    }
+}
+
+struct ModInt
+{
+    public const int Mod = 998244353;
+    const long POSITIVIZER = ((long)Mod) << 31;
+    long Data;
+    public ModInt(long data) { if ((Data = data % Mod) < 0) Data += Mod; }
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % Mod };
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= Mod ? res - Mod : res }; }
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % Mod };
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + Mod : res }; }
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % Mod };
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % Mod };
+    public static bool operator ==(ModInt a, ModInt b) => a.Data == b.Data;
+    public static bool operator !=(ModInt a, ModInt b) => a.Data != b.Data;
+    public override string ToString() => Data.ToString();
+    public override bool Equals(object obj) => (ModInt)obj == this;
+    public override int GetHashCode() => (int)Data;
+    static long GetInverse(long a)
+    {
+        long div, p = Mod, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + Mod; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + Mod; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
