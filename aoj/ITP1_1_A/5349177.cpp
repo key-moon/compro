a974c6e265771f26fe81993800153f35cfa9734c
crawler <kymn0116+crawler@gmail.com>
2021-04-09 13:25:21 +0000

    add aoj/ITP1_1_A/5349177.cpp

---
+++ aoj/ITP1_1_A/5349177.cpp
@@ -0,0 +1,8 @@
+// detail: https://onlinejudge.u-aizu.ac.jp/status/users/keymoon/submissions/1/ITP1_1_A/judge/5349177/C++17
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
