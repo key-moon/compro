a1206a0bc4f35c2f77e67070dcff127e8702bebb
crawler <kymn0116+crawler@gmail.com>
2021-05-26 11:08:35 +0000

    add atcoder/abc159/abc159_c/22930267.cs

---
+++ atcoder/abc159/abc159_c/22930267.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc159/submissions/22930267
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var l = int.Parse(Console.ReadLine());
+        Console.WriteLine(Pow(l / 3.0, 3));
+    }
+}
