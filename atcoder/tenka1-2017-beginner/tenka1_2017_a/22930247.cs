ba0940d49410c543b64fb1b17135f6ef0e42f4f9
crawler <kymn0116+crawler@gmail.com>
2021-05-26 11:07:46 +0000

    add atcoder/tenka1-2017-beginner/tenka1_2017_a/22930247.cs

---
+++ atcoder/tenka1-2017-beginner/tenka1_2017_a/22930247.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/tenka1-2017-beginner/submissions/22930247
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Count(x => x == '1'));
+    }
+}
