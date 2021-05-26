4428119c0e57296688443a4c8a1006daa59bcb38
crawler <kymn0116+crawler@gmail.com>
2021-05-26 11:01:24 +0000

    add atcoder/abc090/abc090_a/22930112.cs

---
+++ atcoder/abc090/abc090_a/22930112.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/abc090/submissions/22930112
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine($"{Console.ReadLine()[0]}{Console.ReadLine()[1]}{Console.ReadLine()[2]}");
+    }
+}
