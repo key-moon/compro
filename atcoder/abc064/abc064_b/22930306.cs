d01f6616dfc2ab4f5f14908554d314257dcf3321
crawler <kymn0116+crawler@gmail.com>
2021-05-26 11:10:08 +0000

    add atcoder/abc064/abc064_b/22930306.cs

---
+++ atcoder/abc064/abc064_b/22930306.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc064/submissions/22930306
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(a.Max() - a.Min());
+    }
+}
