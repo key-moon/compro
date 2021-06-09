81f091227c3496e432f815cf3cd777bcc9e63d11
crawler <kymn0116+crawler@gmail.com>
2021-06-09 10:39:11 +0000

    add atcoder/abc145/abc145_c/23311309.cs

---
+++ atcoder/abc145/abc145_c/23311309.cs
@@ -0,0 +1,89 @@
+// detail: https://atcoder.jp/contests/abc145/submissions/23311309
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        var pos = Enumerable.Repeat(0, n).Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).Select(x => new Vector(x[0], x[1])).ToArray();
+        var res = Permutations(Enumerable.Range(0, n).ToArray()).Average(item => item.Zip(item.Skip(1), (x, y) => (pos[x] - pos[y]).SqrtLength).Sum());
+        Console.WriteLine(res);
+    }
+
+    static IEnumerable<T[]> Permutations<T>(T[] array) where T : IComparable<T>
+    {
+        int index = 0;
+        yield return array;
+        while (true)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                if (array[i - 1].CompareTo(array[i]) >= 0) continue;
+                int j = Array.FindLastIndex(array, x => array[i - 1].CompareTo(x) < 0);
+                T tmp = array[i - 1]; array[i - 1] = array[j]; array[j] = tmp;
+                Array.Reverse(array, i, array.Length - i);
+                yield return array;
+                goto end;
+            }
+            Array.Reverse(array, index, array.Length);
+            yield break;
+            end:;
+        }
+    }
+}
+
+
+struct Vector
+{
+    public double x;
+    public double y;
+    public double this[int index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get { if (index == 0) return x; else return y; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set { if (index == 0) x = value; else y = value; }
+    }
+    public double Length => x * x + y * y;
+    public double SqrtLength => Math.Sqrt(Length);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector(double x, double y) { this.x = x; this.y = y; }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector RotateVector(double radian) => new Vector((double)(x * Math.Cos(radian) - y * Math.Sin(radian)), (double)(x * Math.Sin(radian) + y * Math.Cos(radian)));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public double CrossProduct(Vector a, Vector b) => a.x * b.y - a.y * b.x;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public double InnerProduct(Vector a, Vector b) => a.x * b.x + a.y * b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator *(Vector a, double op) => new Vector(a.x * op, a.y * op);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector operator /(Vector a, double op) => new Vector(a.x / op, a.y / op);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Vector a, Vector b) => a.x == b.x && a.y == b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Vector a, Vector b) => a.x != b.x || a.y != b.y;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object obj) => this == (Vector)obj;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => x.GetHashCode() * 1000000007 + y.GetHashCode();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => $"({x},{y})";
+}
