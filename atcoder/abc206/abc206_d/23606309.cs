5de8f31aa3dc0ba84febb2aaf42cc8aeb2ca1565
crawler <kymn0116+crawler@gmail.com>
2021-06-19 12:47:47 +0000

    add atcoder/abc206/abc206_d/23606309.cs

---
+++ atcoder/abc206/abc206_d/23606309.cs
@@ -0,0 +1,66 @@
+// detail: https://atcoder.jp/contests/abc206/submissions/23606309
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        long n = int.Parse(Console.ReadLine());
+        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        UnionFind uf = new UnionFind(200001);
+        for (int i = 0; i < a.Length; i++)
+        {
+            uf.TryUnite(a[i], a[^(i + 1)]);
+        }
+        Console.WriteLine(a.Distinct().Count() - a.Select(uf.Find).Distinct().Count());
+    }
+}
+
+
+class UnionFind
+{
+    public int Size { get; private set; }
+    public int GroupCount { get; private set; }
+    public IEnumerable<int> AllRepresents => Parent.Where((x, y) => x == y);
+    int[] Sizes;
+    int[] Parent;
+    public UnionFind(int count)
+    {
+        Size = count;
+        GroupCount = count;
+        Parent = new int[count];
+        Sizes = new int[count];
+        for (int i = 0; i < count; i++) Sizes[Parent[i] = i] = 1;
+    }
+    public bool TryUnite(int x, int y)
+    {
+        int xp = Find(x);
+        int yp = Find(y);
+        if (yp == xp) return false;
+        if (Sizes[xp] < Sizes[yp]) { var tmp = xp; xp = yp; yp = tmp; }
+        GroupCount--;
+        Parent[yp] = xp;
+        Sizes[xp] += Sizes[yp];
+        return true;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int GetSize(int x) => Sizes[Find(x)];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Find(int x)
+    {
+        while (x != Parent[x]) x = (Parent[x] = Parent[Parent[x]]);
+        return x;
+    }
+}
