9121bea88276163a0e7480f32ab94d1db62da447
crawler <kymn0116+crawler@gmail.com>
2021-06-30 10:32:08 +0000

    add atcoder/abc006/abc006_1/23877859.cs

---
+++ atcoder/abc006/abc006_1/23877859.cs
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/abc006/submissions/23877859
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(n % 3 == 0 ? "YES" : "NO");
+    }
+}
+
