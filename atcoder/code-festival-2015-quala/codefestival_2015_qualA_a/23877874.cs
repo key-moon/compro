bc5157912ba7da8735ae81cc420df0bb07144f58
crawler <kymn0116+crawler@gmail.com>
2021-06-30 10:33:10 +0000

    add atcoder/code-festival-2015-quala/codefestival_2015_qualA_a/23877874.cs

---
+++ atcoder/code-festival-2015-quala/codefestival_2015_qualA_a/23877874.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/code-festival-2015-quala/submissions/23877874
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine()[..^1] + '5');
+    }
+}
+
