25a5feae80ad769c1a6700f243eb1c35b459b3e4
crawler <kymn0116+crawler@gmail.com>
2021-07-04 13:29:02 +0000

    add atcoder/abc208/abc208_a/23986958.cs

---
+++ atcoder/abc208/abc208_a/23986958.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc208/submissions/23986958
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var a = ab[0];
+        var b = ab[1];
+        Console.WriteLine(a <= b && b <= a * 6 ? "Yes" : "No");
+    }
+}
