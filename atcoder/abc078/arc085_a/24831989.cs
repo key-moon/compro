130599a787bc937fe57d38f59778256777e6cd61
crawler <kymn0116+crawler@gmail.com>
2021-08-07 13:15:55 +0000

    add atcoder/abc078/arc085_a/24831989.cs

---
+++ atcoder/abc078/arc085_a/24831989.cs
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc078/submissions/24831989
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nm[0];
+        var m = nm[1];
+        Console.WriteLine(((n - m) * 100 + m * 1900) * (1 << m));
+    }
+}
