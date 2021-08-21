15f04e6c3b3f7f4ba2916d2eafdd78df556cd972
crawler <kymn0116+crawler@gmail.com>
2021-08-21 12:09:15 +0000

    add atcoder/abc215/abc215_b/25207166.cs

---
+++ atcoder/abc215/abc215_b/25207166.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc215/submissions/25207166
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(n, 2).Length - 1);
+    }
+}
