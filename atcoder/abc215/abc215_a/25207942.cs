05564f5d54aa55b43430885560b0230b1321a9d9
crawler <kymn0116+crawler@gmail.com>
2021-08-21 12:10:02 +0000

    add atcoder/abc215/abc215_a/25207942.cs

---
+++ atcoder/abc215/abc215_a/25207942.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc215/submissions/25207942
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s == "Hello,World!" ? "AC" : "WA");
+    }
+}
