76ba042caeb11b30cfdf87e4fce3191bca7d9760
crawler <kymn0116+crawler@gmail.com>
2021-09-04 12:03:15 +0000

    add atcoder/abc217/abc217_a/25560990.cs

---
+++ atcoder/abc217/abc217_a/25560990.cs
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc217/submissions/25560990
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        var st = Console.ReadLine().Split();
+
+        Console.WriteLine(st[0].CompareTo(st[1]) < 0 ? "Yes" : "No");
+    }
+}
