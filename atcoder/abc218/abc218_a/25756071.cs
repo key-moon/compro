de5a34d998905ff7af68549cab87639ac7eb1b89
crawler <kymn0116+crawler@gmail.com>
2021-09-11 12:09:28 +0000

    add atcoder/abc218/abc218_a/25756071.cs

---
+++ atcoder/abc218/abc218_a/25756071.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc218/submissions/25756071
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(Console.ReadLine()[n - 1] == 'o' ? "Yes" : "No");
+    }
+}
