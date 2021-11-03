77b496f466fa96146e7ddfcd8c8fef995803aa62
crawler <kymn0116+crawler@gmail.com>
2021-11-03 12:29:54 +0000

    add atcoder/future-contest-2022-qual/future_contest_2022_qual_a/27008199.cs

---
+++ atcoder/future-contest-2022-qual/future_contest_2022_qual_a/27008199.cs
@@ -0,0 +1,34 @@
+// detail: https://atcoder.jp/contests/future-contest-2022-qual/submissions/27008199
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+public static class P
+{
+    public static void Main()
+    {
+        var nmkr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        for (int i = 0; i < nmkr[0]; i++)
+        {
+            Console.ReadLine();
+        }
+        for (int i = 0; i < nmkr[3]; i++)
+        {
+            Console.ReadLine();
+        }
+        Console.WriteLine("1 1 1"); Console.ReadLine();
+        while (true)
+        {
+            Console.WriteLine("0");
+            if (Console.ReadLine() == "-1") break;
+        }
+    }
+}
