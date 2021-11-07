3ba19854aecf82eb57dd4a17075b6c9de190e1d7
crawler <kymn0116+crawler@gmail.com>
2021-11-07 12:22:39 +0000

    add atcoder/abc226/abc226_e/27090434.cs

---
+++ atcoder/abc226/abc226_e/27090434.cs
@@ -0,0 +1,152 @@
+// detail: https://atcoder.jp/contests/abc226/submissions/27090434
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+public static class P
+{
+    public static void Main()
+    {
+        var nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var n = nm[0];
+        var m = nm[1];
+        UnionFind<int> uf = new UnionFind<int>(n, (x, y) => x + y, 0);
+        int[] degrees = new int[n];
+        for (int i = 0; i < m; i++)
+        {
+            var st = Console.ReadLine().Split().Select(x => int.Parse(x) - 1).ToArray();
+            degrees[st[0]]++;
+            degrees[st[1]]++;
+            uf.TryUnite(st[0], st[1]);
+            uf[st[0]]++;
+        }
+        foreach (var rep in uf.AllRepresents)
+        {
+            if (uf[rep] != uf.GetSize(rep))
+            {
+                Console.WriteLine(0);
+                return;
+            }
+        }
+        Console.WriteLine(Power(2, uf.GroupCount));
+    }
+
+    static ModInt Power(ModInt n, long m)
+    {
+        ModInt pow = n;
+        ModInt res = 1;
+        while (m > 0)
+        {
+            if ((m & 1) == 1) res *= pow;
+            pow *= pow;
+            m >>= 1;
+        }
+        return res;
+    }
+}
+
+
+struct ModInt
+{
+    public const int Mod = 998244353;
+    const long POSITIVIZER = ((long)Mod) << 31;
+    long Data;
+    public ModInt(long data) { if ((Data = data % Mod) < 0) Data += Mod; }
+    public static implicit operator long(ModInt modInt) => modInt.Data;
+    public static implicit operator ModInt(long val) => new ModInt(val);
+    public static ModInt operator +(ModInt a, int b) => new ModInt() { Data = (a.Data + b + POSITIVIZER) % Mod };
+    public static ModInt operator +(ModInt a, long b) => new ModInt(a.Data + b);
+    public static ModInt operator +(ModInt a, ModInt b) { long res = a.Data + b.Data; return new ModInt() { Data = res >= Mod ? res - Mod : res }; }
+    public static ModInt operator -(ModInt a, int b) => new ModInt() { Data = (a.Data - b + POSITIVIZER) % Mod };
+    public static ModInt operator -(ModInt a, long b) => new ModInt(a.Data - b);
+    public static ModInt operator -(ModInt a, ModInt b) { long res = a.Data - b.Data; return new ModInt() { Data = res < 0 ? res + Mod : res }; }
+    public static ModInt operator *(ModInt a, int b) => new ModInt(a.Data * b);
+    public static ModInt operator *(ModInt a, long b) => a * new ModInt(b);
+    public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % Mod };
+    public static ModInt operator /(ModInt a, ModInt b) => new ModInt() { Data = a.Data * GetInverse(b) % Mod };
+    public static bool operator ==(ModInt a, ModInt b) => a.Data == b.Data;
+    public static bool operator !=(ModInt a, ModInt b) => a.Data != b.Data;
+    public override string ToString() => Data.ToString();
+    public override bool Equals(object obj) => (ModInt)obj == this;
+    public override int GetHashCode() => (int)Data;
+    static long GetInverse(long a)
+    {
+        long div, p = Mod, x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+        while (true)
+        {
+            if (p == 1) return x2 + Mod; div = a / p; x1 -= x2 * div; y1 -= y2 * div; a %= p;
+            if (a == 1) return x1 + Mod; div = p / a; x2 -= x1 * div; y2 -= y1 * div; p %= a;
+        }
+    }
+}
+
+
+class UnionFind<T>
+{
+    public readonly int Size;
+    public int GroupCount { get; private set; }
+    public IEnumerable<int> AllRepresents => Parent.Where((x, y) => x == y);
+    int[] Sizes;
+    int[] Parent;
+    T[] Data;
+    Func<T, T, T> MergeData;
+    public UnionFind(int count, Func<T, T, T> mergeData, T def)
+    {
+        Size = count;
+        GroupCount = count;
+        Parent = new int[count];
+        Sizes = new int[count];
+        Data = new T[count];
+        MergeData = mergeData;
+        for (int i = 0; i < count; i++)
+        {
+            Parent[i] = i;
+            Sizes[i] = 1;
+            Data[i] = def;
+        }
+    }
+    public T this[int index]
+    {
+        get { return Data[Find(index)]; }
+        set { Data[Find(index)] = value; }
+    }
+
+    public bool TryUnite(int x, int y)
+    {
+        int xp = Find(x);
+        int yp = Find(y);
+        if (yp == xp) return false;
+        if (Sizes[xp] < Sizes[yp])
+        {
+            var tmp = xp;
+            xp = yp;
+            yp = tmp;
+        }
+        GroupCount--;
+        Parent[yp] = xp;
+        Data[xp] = MergeData(Data[xp], Data[yp]);
+        Sizes[xp] += Sizes[yp];
+        return true;
+    }
+    public int GetSize(int x) => Sizes[Find(x)];
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Find(int x)
+    {
+        while (x != Parent[x])
+        {
+            Parent[x] = Parent[Parent[x]];
+            x = Parent[x];
+        }
+        return x;
+    }
+}
