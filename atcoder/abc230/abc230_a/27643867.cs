ec077a8a5ebdf646eab9c5e8ba70112f9a8ee7c9
crawler <kymn0116+crawler@gmail.com>
2021-12-03 12:01:58 +0000

    add atcoder/abc230/abc230_a/27643867.cs

---
+++ atcoder/abc230/abc230_a/27643867.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/abc230/submissions/27643867
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"AGC{(42 <= n ? n + 1 : n):000}");
+    }
+}
