a7382eb1b9bf64bf3c8a1abbc47c2b88a19a5fdf
crawler <kymn0116+crawler@gmail.com>
2021-12-24 13:53:21 +0000

    add atcoder/xmascon21/xmascon21_f/28096736.rb

---
+++ atcoder/xmascon21/xmascon21_f/28096736.rb
@@ -0,0 +1,3 @@
+# detail: https://atcoder.jp/contests/xmascon21/submissions/28096736
+#!ruby -Knrzlib
+puts Zlib.inflate'x�eO� z[������v�f �+��;Â�f�+�ل�ש��]Uv) ����Y�;{R��_���#|YV*t:�8]��{�B�����.����jQ'
