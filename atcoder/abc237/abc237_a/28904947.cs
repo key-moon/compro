3399d98791af23adff0f9a4adda3570e62ab8b8e
crawler <kymn0116+crawler@gmail.com>
2022-01-30 12:06:06 +0000

    add atcoder/abc237/abc237_a/28904947.cs

---
+++ atcoder/abc237/abc237_a/28904947.cs
@@ -0,0 +1,27 @@
+// detail: https://atcoder.jp/contests/abc237/submissions/28904947
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        try
+        {
+            int.Parse(Console.ReadLine());
+            Console.WriteLine("Yes");
+        }
+        catch
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
