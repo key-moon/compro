cdee1391f7b9c94486419c3b1a8d9aaf2d877b84
crawler <kymn0116+crawler@gmail.com>
2022-04-16 12:09:46 +0000

    add atcoder/abc248/abc248_a/31014196.cs

---
+++ atcoder/abc248/abc248_a/31014196.cs
@@ -0,0 +1,19 @@
+// detail: https://atcoder.jp/contests/abc248/submissions/31014196
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+public static class P
+{
+    public static void Main()
+    {
+        Console.WriteLine("0123456789".Except(Console.ReadLine()).First());
+    }
+}
